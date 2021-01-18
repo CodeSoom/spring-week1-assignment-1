6de10394152cc0fd52ea40c87315a9082c940585
seungin yang <rhfpdk92@naver.com>
2021-01-18 23:03:27 +0900

    Add get method

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,16 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress addr = new InetSocketAddress(8000);
+        HttpServer httpServer = HttpServer.create(addr, 0);
+        httpServer.createContext("/",new TodoHttpHandler());
+        httpServer.start();
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/TodoHttpHandler.java
@@ -0,0 +1,78 @@
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TodoHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+    private ObjectMapper mapper = new ObjectMapper();
+    private int idx;
+    public TodoHttpHandler() {
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("hello");
+        tasks.add(task);
+        Task task2 = new Task();
+        task2.setId(2L);
+        task2.setTitle("world");
+        tasks.add(task2);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        String content = "";
+
+        System.out.println(method + " " + path);
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        if (method.equals("GET")) {
+            if (hasNumberParameter(path))
+                content = taskToJson(idx - 1);
+             else
+                content = taskToJson();
+        }
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+    private boolean hasNumberParameter(String path){
+        if(path.startsWith("/tasks/")){
+            String[] split = path.split("/");
+            if (split.length == 3) {
+                idx=Integer.parseInt(split[2]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String taskToJson() throws IOException {
+        OutputStream outputstream = new ByteArrayOutputStream();
+        mapper.writeValue(outputstream, tasks);
+        return outputstream.toString();
+    }
+
+    private String taskToJson(long idx) throws IOException {
+        OutputStream outputstream = new ByteArrayOutputStream();
+        mapper.writeValue(outputstream, tasks.get((int) idx));
+        return outputstream.toString();
+    }
+}
