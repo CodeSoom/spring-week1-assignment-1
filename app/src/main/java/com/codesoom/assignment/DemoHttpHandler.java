d2e18f7c802d14095df373d8a559831c630542a8
juunini <juuni.ni.i@gmail.com>
2021-01-18 17:04:13 +0900

    Set demo http server

---
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -0,0 +1,15 @@
+package com.codesoom.assignment;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class DemoHttpHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+
+        System.out.println(method);
+    }
+}
