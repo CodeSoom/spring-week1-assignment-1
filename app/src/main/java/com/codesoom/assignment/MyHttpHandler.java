4c3c4915af20846a614f2bf56c89cd29ba8f3013
etff <wish2rich@gmail.com>
2021-01-18 22:35:31 +0900

    Set Project init
    Implement Base Http Methods

---
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -0,0 +1,65 @@
+package com.codesoom.assignment;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(method + " " + path);
+        if (!body.isBlank()) {
+            Task task = toTask(body);
+            tasks.add(task);
+        }
+
+        String content = "Hello, world";
+
+        if (method.equals("GET") && path.equals("/tasks")) {
+            content = tasksToJSON();
+        }
+
+        if (method.equals("POST") && path.equals("/tasks")) {
+            content = "create task";
+        }
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = exchange.getResponseBody();
+
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+
+    }
+
+    private Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+
+    private String tasksToJSON() throws IOException {
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+        return outputStream.toString();
+    }
+}
