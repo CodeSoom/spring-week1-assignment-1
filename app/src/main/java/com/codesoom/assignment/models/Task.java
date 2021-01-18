f2d2373e3843d560365bf216541e2b611883fc04
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-18 23:20:11 +0900

    Add 'Task' default construct for Jackson

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -4,6 +4,10 @@ public class Task {
     private long id;
     private String title;
 
+    public Task() {
+
+    }
+
     public Task(long id, String title) {
         this.id = id;
         this.title = title;
