a0288a2576825d8d627d0c78506b9b506e408cc2
thkim <thkim2017@gmail.com>
2021-01-18 21:10:28 +0900

    Create a httpServer Plus set port address

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,17 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+
+        InetSocketAddress address = new InetSocketAddress(8000);
+        HttpServer httpServer = HttpServer.create(address, 0);
     }
 }
