4e7901c2c7e5c9024524136711122b85030f3d00
JamieShin0201 <jamieshin1450@gmail.com>
2021-01-18 22:15:05 +0900

    Create http server

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,22 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
+        InetSocketAddress address = new InetSocketAddress(8000);
+
+        HttpServer httpServer = null;
+        try {
+            httpServer = HttpServer.create(address, 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        httpServer.start();
     }
+
 }
