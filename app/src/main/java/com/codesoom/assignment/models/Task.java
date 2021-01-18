ba67ae3467adf8ebd7e1b4a05a64eceddd7d70f6
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-18 22:45:56 +0900

    Add 'Task' class

---
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -0,0 +1,31 @@
+package com.codesoom.assignment.models;
+
+public class Task {
+    private long id;
+    private String title;
+
+    public Task(long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
