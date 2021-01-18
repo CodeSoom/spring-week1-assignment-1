006f7ee00d44b91c8213876864c8e5f73d606fbe
juunini <juuni.ni.i@gmail.com>
2021-01-18 20:34:52 +0900

    Add path validation

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -8,24 +8,43 @@ import java.io.OutputStream;
 import java.net.URI;
 
 public class DemoHttpHandler implements HttpHandler {
+    private boolean isValidPath(String path) {
+        return path.startsWith("/tasks");
+    }
+
+    private Long extractID(String path) {
+        String[] split = path.split("/");
+        if (split.length != 2) {
+            return null;
+        }
+        return Long.parseLong(split[1]);
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
         URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
         System.out.println(method + " " + uri.getPath());
 
         OutputStream outputStream = exchange.getResponseBody();
         String content = "";
 
-        switch (method) {
-            case "GET" -> content = "GET";
-            case "POST" -> content = "POST";
-            case "PUT" -> content = "PUT";
-            case "PATCH" -> content = "PATCH";
-            case "DELETE" -> content = "DELETE";
+        if (isValidPath(path)) {
+            switch (method) {
+                case "GET" -> content = "GET";
+                case "POST" -> content = "POST";
+                case "PUT" -> content = "PUT";
+                case "PATCH" -> content = "PATCH";
+                case "DELETE" -> content = "DELETE";
+            }
+
+            exchange.sendResponseHeaders(200, content.length());
+        } else {
+            content = "not found";
+            exchange.sendResponseHeaders(404, content.length());
         }
 
-        exchange.sendResponseHeaders(200, content.length());
         outputStream.write(content.getBytes());
 
         outputStream.flush();
