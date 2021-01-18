62c05e6950f24203a22781283dc5dd934e299c74
developerOlive <chloehiho@gmail.com>
2021-01-19 05:34:01 +0900

    feat : http 서버 생성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,18 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+
+            InetSocketAddress serverAddress = new InetSocketAddress(8000);
+            HttpServer httpServer = HttpServer.create(serverAddress,0);
+
+
     }
 }
