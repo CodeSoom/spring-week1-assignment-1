64167b38b22f20e1850da3313574afd533fe22b7
etff <wish2rich@gmail.com>
2021-01-19 01:41:40 +0900

    Add put method
    implement put method to change task title

--- app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -9,12 +9,14 @@ import java.io.*;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class MyHttpHandler implements HttpHandler {
     private List<Task> tasks = new ArrayList<>();
     private ObjectMapper objectMapper = new ObjectMapper();
-    private Long count = 0L;
+    private Pattern pattern = Pattern.compile("/tasks/([^/]+)");
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -28,23 +30,45 @@ public class MyHttpHandler implements HttpHandler {
                 .collect(Collectors.joining("\n"));
 
         System.out.println(method + " " + path);
-        if (!body.isBlank()) {
-            Task task = toTask(body);
-            task.setId(IdGenerator.generate());
-            tasks.add(task);
-        }
+
 
         String content = "Hello, world";
 
         if (method.equals("GET") && path.equals("/tasks")) {
             content = tasksToJSON();
+            exchange.sendResponseHeaders(200, content.getBytes().length);
         }
 
         if (method.equals("POST") && path.equals("/tasks")) {
             content = "create a new task";
+            if (!body.isBlank()) {
+                Task task = toTask(body);
+                task.setId(IdGenerator.generate());
+                tasks.add(task);
+                exchange.sendResponseHeaders(201, content.getBytes().length);
+            }
         }
 
-        exchange.sendResponseHeaders(200, content.getBytes().length);
+        if (method.equals("PUT") && pattern.matcher(path).matches()) {
+            content = "update a task";
+            String[] paths = path.split("/");
+            Long finalPathVariable = Long.parseUnsignedLong(paths[2]);
+
+            if (!body.isBlank()) {
+                System.out.println("found Body");
+                Optional<Task> foundTask = tasks.stream()
+                        .filter(t -> t.getId() == finalPathVariable)
+                        .findFirst();
+                if (foundTask.isPresent()) {
+                    foundTask.get().setTitle(extractValue(body));
+                    exchange.sendResponseHeaders(200, content.getBytes().length);
+                } else {
+                    System.out.println("not found task" + foundTask);
+                    exchange.sendResponseHeaders(400, 0);
+                }
+            }
+        }
+
 
         OutputStream outputStream = exchange.getResponseBody();
 
@@ -54,9 +78,15 @@ public class MyHttpHandler implements HttpHandler {
 
     }
 
+    private String extractValue(String content) throws JsonProcessingException {
+        Task task = objectMapper.readValue(content, Task.class);
+        return task.getTitle();
+    }
+
     private Task toTask(String content) throws JsonProcessingException {
         return objectMapper.readValue(content, Task.class);
     }
+
 
     private String tasksToJSON() throws IOException {
 
