304821c80df5010ad5d663f8008f2050eaff5169
developerOlive <chloehiho@gmail.com>
2021-01-19 05:48:06 +0900

    feat : http서버 실행

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -12,9 +12,10 @@ public class App {
     public static void main(String[] args) {
 
         try {
-            
+
             InetSocketAddress serverAddress = new InetSocketAddress(8000);
             HttpServer httpServer = HttpServer.create(serverAddress,0);
+            httpServer.start();
 
         } catch (IOException e) {
             e.printStackTrace();
