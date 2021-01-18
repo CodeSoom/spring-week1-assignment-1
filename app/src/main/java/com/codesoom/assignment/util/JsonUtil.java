f82a521a8b30532d25b71ac293ce0829deb53306
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-18 23:17:33 +0900

    Add 'JsonUtil' class - Serialize/Deserialize 'Task' object

---
+++ app/src/main/java/com/codesoom/assignment/util/JsonUtil.java
@@ -0,0 +1,19 @@
+package com.codesoom.assignment.util;
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public class JsonUtil {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    public static Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+}
