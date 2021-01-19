49592f033d8e4d9a45d362be35ba0a2978758198
thkim <thkim2017@gmail.com>
2021-01-20 02:14:17 +0900

    Create a constructor of DemoHttpHandler It sets an id and title of a task, plus add it into a List tasks

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -1,16 +1,29 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DemoHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+
+    public DemoHttpHandler() {
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("\"나는 이미 행복한 부자다\"라고 되뇌기");
+
+        tasks.add(task);
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+
         String method = exchange.getRequestMethod();
 
         URI uri = exchange.getRequestURI();
