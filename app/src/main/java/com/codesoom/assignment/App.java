86a12b149a92103f657e89f7ec22ce85700affd2
jennifer <jennifer@novonetworks.com>
2021-01-19 13:10:49 +0900

    Add model, HttpHandler
    Add Task model, TaskHttpHandler

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,27 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello, this is TODO App server...";
     }
 
     public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+
+        try {
+            InetSocketAddress address = new InetSocketAddress("localhost", 8000);
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new TaskHttpHandler();
+            httpServer.createContext("/tasks", handler);
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/TaskHttpHandler.java
@@ -0,0 +1,37 @@
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.models.Task;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TaskHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(method + " " + path);
+
+        String content = "Hello";
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+}
