0052631ab245ffbccb7e577cf0c2e1c8c2fa8ea4
Gyu <melon9751@naver.com>
2021-01-19 20:55:29 +0900

    Add OutputStream and ResponseHeaders
    OuputStream needs to finish handle method. localhost:8000 returns
    RemoteDisconnected('Remote end closed connection without response')).
    Return Headers with status code.

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -4,11 +4,17 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class DemoHttpHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
         System.out.println(method);
+
+        exchange.sendResponseHeaders(200,0);
+
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.close();
     }
 }
