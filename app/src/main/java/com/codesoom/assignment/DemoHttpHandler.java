6f14f98e1f5243287a72928b464b921731932e0e
Gyu <melon9751@naver.com>
2021-01-19 22:44:35 +0900

    Use ObjectMapper to convert from object to Json
    Make a Task class object in constructor. ObjectMapper makes possible
    convert Task object to Json data type.

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -1,13 +1,28 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DemoHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+
+    public DemoHttpHandler() {
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("Do nothing...");
+
+        tasks.add(task);
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
@@ -18,7 +33,7 @@ public class DemoHttpHandler implements HttpHandler {
         String content = "Hello World";
 
         if(method.equals("GET") && path.equals("/tasks")) {
-            content = "No Tasks";
+            content = tasksToJson();
         }
 
         if(method.equals("POST") && path.equals("/task")) {
@@ -31,5 +46,13 @@ public class DemoHttpHandler implements HttpHandler {
         outputStream.write(content.getBytes());
         outputStream.flush();
         outputStream.close();
+    }
+
+    private String tasksToJson() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+        return outputStream.toString();
     }
 }
