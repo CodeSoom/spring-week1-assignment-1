3a9d52aa1b5b2e7192ea41fbd3b7b46c99e84895
Gyu <melon9751@naver.com>
2021-01-19 20:25:29 +0900

    Add context path
    $http http://localhost:8000 returns 404 Not Found. Add context path.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -10,6 +10,7 @@ public class App {
     public static void main(String[] args) throws IOException {
         InetSocketAddress inetSocketAddress = new InetSocketAddress(8000);
         HttpServer httpServer = HttpServer.create(inetSocketAddress, 0);
+        httpServer.createContext("/");
         httpServer.start();
     }
 }
