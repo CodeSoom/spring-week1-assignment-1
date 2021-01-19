e6492dd8f95ce1b121a8a56d43285752af8b2c03
wenodev <logicalstark1105@gmail.com>
2021-01-19 20:20:03 +0900

    Create HttpServer

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,26 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * 1. Create HttpServer
+ */
+
+
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
+        try {
+            InetSocketAddress address = new InetSocketAddress(8000);
+            HttpServer server = HttpServer.create(address, 0);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
