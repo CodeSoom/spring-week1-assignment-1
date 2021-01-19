132d0ec61a6144d93f5b80a6b0002dcd849a248a
seungin yang <rhfpdk92@naver.com>
2021-01-20 00:57:28 +0900

    Change port to constant

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,16 +1,17 @@
 package com.codesoom.assignment;
 
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class App {
+    static final int PORT = 8000;
+
     public static void main(String[] args) throws IOException {
-        InetSocketAddress addr = new InetSocketAddress(8000);
-        HttpServer httpServer = HttpServer.create(addr, 0);
-        httpServer.createContext("/",new TodoHttpHandler());
+        InetSocketAddress address = new InetSocketAddress(PORT);
+        HttpServer httpServer = HttpServer.create(address, 0);
+        httpServer.createContext("/", new TodoHttpHandler());
         httpServer.start();
     }
 }
