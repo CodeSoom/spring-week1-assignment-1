9ae8305b9ba6d46c33dcf7a385c3bfe35da55e35
developerOlive <chloehiho@gmail.com>
2021-01-19 16:16:54 +0900

    Feat : MyHttpHandler 클래스 - Task 객체를 담을 arrayList 생성

--- app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -1,12 +1,18 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyHttpHandler implements HttpHandler {
+
+    private List<Task> tasks = new ArrayList<>();
+    
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
