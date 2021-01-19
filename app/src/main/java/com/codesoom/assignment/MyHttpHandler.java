63f39de660d4bb0557e79eeccf0f7e4919cd9a00
developerOlive <chloehiho@gmail.com>
2021-01-19 09:29:41 +0900

    Feat : MyHttpHandler 클래스 - request로 어떤 메서드가 들어오는지 확인

--- app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -2,13 +2,16 @@ package com.codesoom.assignment;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.IOException;
 
 public class MyHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+
+        // request로 어떤 메서드가 들어오는지 확인
+        String method = exchange.getRequestMethod();
+        System.out.println("method : " + method);
 
     }
 }
