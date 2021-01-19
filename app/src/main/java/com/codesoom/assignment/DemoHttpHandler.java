5bcd0bf07bf06a7fccb41d22493f3f1b0c717568
thkim <thkim2017@gmail.com>
2021-01-19 22:44:42 +0900

    Add a variable named method Plus create an instance of OutputStream named responseBody

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -4,11 +4,18 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class DemoHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         System.out.println("Handler is working");
+
+        String method = exchange.getRequestMethod();
+        System.out.println("Handler just requested a " + method + " method");
+
+        OutputStream responseBody = exchange.getResponseBody();
+        responseBody.close();
     }
 }
