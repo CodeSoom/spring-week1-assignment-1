3ea3499f0f6543857d2bef96613b49517c02e55c
newoo <newoo4297@naver.com>
2021-01-19 21:31:10 +0900

    Update response content about request to add new task

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -3,11 +3,14 @@ package com.codesoom.assignment;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.codesoom.assignment.JSONParser.taskToJSON;
+import static com.codesoom.assignment.JSONParser.toTask;
 
 public class DemoHttpHandler implements HttpHandler {
     private List<Task> tasks = new ArrayList<>();
@@ -18,9 +21,20 @@ public class DemoHttpHandler implements HttpHandler {
         URI uri = exchange.getRequestURI();
         String path = uri.getPath();
 
-        System.out.println(method + " " + path);
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
 
         String content = "";
+
+        System.out.println(method + " " + path);
+        if (!body.isBlank()) {
+            System.out.println(body);
+
+            Task task = toTask(body, Long.valueOf(tasks.size() + 1));
+            tasks.add(task);
+        }
 
         if (method.equals("GET") && path.equals("/tasks")) {
             content = "ToDo 목록 얻기";
@@ -31,7 +45,7 @@ public class DemoHttpHandler implements HttpHandler {
         }
 
         if (method.equals("POST") && path.equals("/tasks")) {
-            content = "ToDo 생성하기";
+            content = taskToJSON(tasks.get(tasks.size() - 1));
         }
 
         if ((method.equals("PUT") || method.equals("PATCH")) && path.matches("/tasks/*[0-9]*")) {
