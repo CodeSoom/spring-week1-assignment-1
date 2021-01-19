d8fcfcb6450ec3fa590a0962c4167b9558d044f1
juunini <juuni.ni.i@gmail.com>
2021-01-19 17:21:16 +0900

    Add jackson parse Task test

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.models;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +9,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TaskTest {
     @Test
     void makeTask() {
-        long id = 1;
-        String title = "sample";
+        final long id = 1;
+        final String title = "sample";
         Task task = new Task(id, title);
 
         assertEquals(id, task.id());
         assertEquals(title, task.title());
+    }
+
+    @Test
+    void parseTaskByJackson() throws JsonProcessingException {
+        final long id = 1;
+        final String title = "sample";
+        final String taskStr = String.format("{\"id\": %d, \"title\": \"%s\"}", id, title);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Task task = mapper.readValue(taskStr, Task.class);
+
+        assertEquals(1, task.id());
+        assertEquals("sample", task.title());
     }
 }
