17bbc81446634fb14e9421b5a28a91ab77c005cf
developerOlive <chloehiho@gmail.com>
2021-01-19 09:34:22 +0900

    Feat : MyHttpHandler 클래스 - response 처리
    request에 대한 response 처리 진행

--- app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class MyHttpHandler implements HttpHandler {
 
@@ -12,6 +13,10 @@ public class MyHttpHandler implements HttpHandler {
         // request로 어떤 메서드가 들어오는지 확인
         String method = exchange.getRequestMethod();
         System.out.println("method : " + method);
+
+        // response 처리
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.close(); // 호출해서 사용했던 시스템 자원을 풀어줌
 
     }
 }
