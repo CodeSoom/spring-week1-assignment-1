e7e32ea72e47d0dfd768381718b6894e24815a2d
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-19 11:38:20 +0900

    Change task management method - from list to map

--- app/src/main/java/com/codesoom/assignment/service/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/service/TaskService.java
@@ -2,51 +2,49 @@ package com.codesoom.assignment.service;
 
 import com.codesoom.assignment.models.Task;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class TaskService {
-    private List<Task> tasks = new ArrayList<>();
+    private Map<Long, Task> tasks = new HashMap<>();
 
     public Task getTask(long id) {
-        return findTaskById(id).orElseThrow(() -> new IllegalArgumentException("Failed to find task (ID: " + id + ")"));
+        return findTaskById(id);
     }
 
     public List<Task> getTasks() {
-        return this.tasks;
+        return new ArrayList<>(tasks.values());
     }
 
     public Task createNewTask(String title) {
-        Task task = new Task(generateTaskId(), title);
-        tasks.add(task);
+        Task task = new Task(generateNewTaskId(), title);
+        tasks.put(task.getId(), task);
         System.out.println("Completed to create task - " + task.toString());
         return task;
     }
 
     public void updateTask(long id, String newTitle) {
-        Task task = findTaskById(id).orElseThrow(() -> new IllegalArgumentException("Failed to find task (ID: " + id + ")"));
+        Task task = findTaskById(id);
         task.setTitle(newTitle);
         System.out.println("Completed to update task - " + task.toString());
     }
 
     public void deleteTask(long id) {
-        Task task = findTaskById(id).orElseThrow(() -> new IllegalArgumentException("Failed to find task (ID: " + id + ")"));
-        tasks.remove(task);
+        Task task = findTaskById(id);
+        tasks.remove(task.getId());
         System.out.println("Completed to delete task - " + task.toString());
     }
 
-    private Optional<Task> findTaskById(long id) {
-        return tasks.stream().filter(e -> e.getId() == id).findFirst();
+    private Task findTaskById(long id) {
+        return Optional.ofNullable(tasks.get(id)).orElseThrow(() -> new IllegalArgumentException("Failed to find task (ID: " + id + ")"));
     }
 
-    private long generateTaskId() {
+    private long generateNewTaskId() {
         long id = 1;
 
         if (!tasks.isEmpty()) {
-            id = tasks.stream().mapToLong(Task::getId).max().getAsLong() + 1;
+            //Task 삭제를 대비하여 가장 최근 task의 ID를 기준으로 생성
+            id = tasks.keySet().stream().mapToLong(Long::longValue).max().getAsLong() + 1;
         }
-
         return id;
     }
 }
