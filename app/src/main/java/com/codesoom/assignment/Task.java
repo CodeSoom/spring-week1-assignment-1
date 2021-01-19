a28824bc59bf6cedbc9ad4e3600fc13c7fbcdcbf
newoo <newoo4297@naver.com>
2021-01-19 15:44:39 +0900

    Delete Task constructor code

--- app/src/main/java/com/codesoom/assignment/Task.java
+++ app/src/main/java/com/codesoom/assignment/Task.java
@@ -4,11 +4,6 @@ public class Task {
     private Long id;
     private String title;
 
-    public Task(Long id, String title) {
-        this.id = id;
-        this.title = title;
-    }
-
     public Long getId() {
         return id;
     }
