fb0ec997f477e439fbd635e4b14811da1cd8602c
thkim <thkim2017@gmail.com>
2021-01-20 02:03:39 +0900

    Detach the port number As a static final variable

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -7,13 +7,11 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final int PORT = 8000;
 
     public static void main(String[] args) {
         try {
-            InetSocketAddress address = new InetSocketAddress(8000);
+            InetSocketAddress address = new InetSocketAddress(PORT);
             HttpServer httpServer = HttpServer.create(address, 0);
 
             HttpHandler handler = new DemoHttpHandler();
