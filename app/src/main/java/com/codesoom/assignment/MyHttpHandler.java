593c45218083b1577f04887b7cab8ffaf6e8c80d
developerOlive <chloehiho@gmail.com>
2021-01-19 20:43:12 +0900

    Feat : taskToJSON() 메서드 생성
    taskToJSON() 메서드 내용을 구성하기 위해 ObjectorMapper 객체 생성,
    outputStream 객체 생성,
    objectMapper의 writeValue() 메서드를 사용하여 JSON 형태로 변환

--- app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -1,8 +1,10 @@
 package com.codesoom.assignment;
 
 import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
@@ -10,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyHttpHandler implements HttpHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     private List<Task> tasks = new ArrayList<>();
 
@@ -37,14 +41,31 @@ public class MyHttpHandler implements HttpHandler {
         // connectionError가 발생하지 않도록 HTTP 상태코드 처리
         // content : 성공적으로 응답 했을 경우, 확인을 위해 화면에 띄워줄 메세지
         String content = "Hello, world!";
+
+        if (method.equals("GET") && path.equals("/tasks")){
+            content = tasksToJSON();
+        }
+
         exchange.sendResponseHeaders(200, content.getBytes().length);
 
 
         // response 처리
-        OutputStream outputStream = exchange.getResponseBody();
+        OutputStream outputStream = exchange.getResponseBody(); // getResponseBody() : Response를 byte 배열로 반환
         outputStream.write(content.getBytes());  // 매개값으로 주어진 바이트 배열의 모든 바이트를 출력 스트림으로 보냄
         outputStream.flush(); // 버퍼에 남아있는 데이터를 모두 출력시키고 버퍼를 비움
         outputStream.close(); // 호출해서 사용했던 시스템 자원을 풀어줌
 
+    }
+
+    private String tasksToJSON() throws IOException {
+
+        // ByteArrayOutputStream() : 바이트 배열 출력 시 사용되는 스트림
+        OutputStream outputStream = new ByteArrayOutputStream();
+
+        // Java Object to JSON
+        objectMapper.writeValue(outputStream, tasks);
+
+        // String 형식의 content 변수에 담기 위해 String으로 변환하여 return
+        return outputStream.toString();
     }
 }
