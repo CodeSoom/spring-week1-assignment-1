c3377af6782d014f92ae2e18002cb463b75bbdac
juunini <juuni.ni.i@gmail.com>
2021-01-20 17:26:38 +0900

    Modify task manager doc

--- app/src/main/java/com/codesoom/assignment/models/TaskManager.java
+++ app/src/main/java/com/codesoom/assignment/models/TaskManager.java
@@ -8,24 +8,27 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Task 의 관리를 위해 만들었습니다.
+ * The class {@code TaskManager} is managing tasks.
  */
 public class TaskManager {
     private static long index = 0;
     private static final HashMap<Long, Task> TASKS = new HashMap<>();
 
     /**
-     * findAll 의 역할을 합니다.
-     * @return 전체 Task 를 List 로 반환합니다.
+     * Find all tasks.
+     *
+     * @return all tasks.
      */
     public static List<Task> find() {
         return new ArrayList<>(TASKS.values());
     }
 
     /**
-     * findOne 의 역할을 합니다.
-     * @param id 찾고자 하는 Task id
-     * @return 존재 시 Task 반환, 없으면 null 반환
+     * Find one task.
+     *
+     * @param id target id.
+     * @return target task.
+     * @throws NotExistsIDException if id is not exist.
      */
     public static Task find(long id) throws NotExistsIDException {
         Task task = TASKS.get(id);
@@ -37,9 +40,10 @@ public class TaskManager {
     }
 
     /**
-     * Task 를 받아서 insert 합니다. id가 존재시 Exception 을 반환합니다.
-     * @param task 입력하고자 하는 Task 를 입력합니다.
-     * @throws AlreadyExistsIDException 이미 id가 존재 할 경우
+     * Insert received task.
+     *
+     * @param task want to insert.
+     * @throws AlreadyExistsIDException if id is already exist.
      */
     public static void insert(Task task) throws AlreadyExistsIDException {
         if (isExist(task.id())) {
@@ -49,9 +53,11 @@ public class TaskManager {
     }
 
     /**
-     * title 을 입력받아 Task 를 생성합니다.
-     * @param title 입력하고자 하는 title 을 입력합니다.
-     * @return id는 auto increase 하여 생성한 Task 를 반환합니다.
+     * Insert task.
+     * id insert to auto increment value.
+     *
+     * @param title task title.
+     * @return inserted task.
      */
     public static Task insert(String title) {
         index += 1;
@@ -64,9 +70,10 @@ public class TaskManager {
     }
 
     /**
-     * Task 를 입력받아 변경합니다.
-     * @param task 변경할 Task 를 입력합니다.
-     * @throws NotExistsIDException id가 존재하지 않을 시
+     * Modify task.
+     *
+     * @param task want to modify task object.
+     * @throws NotExistsIDException if id is not exist.
      */
     public static void modify(Task task) throws NotExistsIDException {
         if (!isExist(task.id())) {
@@ -76,11 +83,12 @@ public class TaskManager {
     }
 
     /**
-     * id와 title 을 각각 입력받아 변경합니다.
-     * @param id 대상 id
-     * @param title 변경할 title
-     * @return 변경된 Task 를 반환합니다.
-     * @throws NotExistsIDException id가 존재하지 않을 시
+     * Modify task.
+     *
+     * @param id target id
+     * @param title want to change content.
+     * @return modified task.
+     * @throws NotExistsIDException if id is not exist.
      */
     public static Task modify(long id, String title) throws NotExistsIDException {
         if (!isExist(id)) {
@@ -93,9 +101,10 @@ public class TaskManager {
     }
 
     /**
-     * Task 를 삭제합니다.
-     * @param id 삭제할 Task id
-     * @throws NotExistsIDException id가 존재하지 않을 시
+     * Delete task.
+     *
+     * @param id target id.
+     * @throws NotExistsIDException if id is not exist.
      */
     public static void delete(long id) throws NotExistsIDException {
         if (!isExist(id)) {
@@ -104,15 +113,19 @@ public class TaskManager {
         TASKS.remove(id);
     }
 
+    /**
+     * Remove all tasks and initialization {@code index}
+     */
     public static void clear() {
         TASKS.clear();
         index = 0;
     }
 
     /**
-     * id가 존재하는지 확인합니다.
-     * @param id 확인하고자 하는 id
-     * @return 존재 여부
+     * Check id is exist.
+     *
+     * @param id target id.
+     * @return true if id is exists.
      */
     private static boolean isExist(long id) {
         return TASKS.get(id) != null;
