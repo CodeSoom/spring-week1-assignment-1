95c171bc9b03af404fa53485c54783a51c4dc705
Las <haho013039@gmail.com>
2021-01-20 20:25:32 +0900

    Use TaskApplicationService
    리팩터링을 진행하면서 실제 TaskApplicationService를 사용하도록 수정하였습니다. WebHandler에서는 TaskApplicationService를 생성하는 시점에 주입받아서 사용하도록 하였습니다.

--- app/src/main/java/com/codesoom/assignment/web/WebHandler.java
+++ app/src/main/java/com/codesoom/assignment/web/WebHandler.java
@@ -1,26 +1,53 @@
 package com.codesoom.assignment.web;
 
+import com.codesoom.assignment.application.TaskApplicationService;
+import com.codesoom.assignment.application.TaskJsonTransfer;
+import com.codesoom.assignment.domain.NotFoundTask;
+import com.codesoom.assignment.domain.Task;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.stream.Collectors;
 
 public class WebHandler implements HttpHandler {
+    TaskApplicationService taskApplicationService;
+    TaskJsonTransfer transfer;
+
+    public WebHandler(TaskApplicationService taskApplicationService) {
+        this.taskApplicationService = taskApplicationService;
+        this.transfer = new TaskJsonTransfer();
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         if (exchange.getRequestURI().getPath().equals("/tasks")) {
-            String content = "[]";
+            String content = transfer.taskListToJson(taskApplicationService.getAllTasks());
             setJsonToResponseBody(exchange, content, 200);
-        }
-        else if (exchange.getRequestURI().getPath().equals("/task")){
-            String content = "{\"id\":1,\"title\":\"Play Game\"}";
-            setJsonToResponseBody(exchange, content, 201);
-        }
-        else {
+        } else if (exchange.getRequestURI().getPath().equals("/task")) {
+            String requestBody = new BufferedReader(
+                    new InputStreamReader(exchange.getRequestBody()))
+                    .lines()
+                    .collect(Collectors.joining(""));
+            Task requestTask = transfer.jsonStringToTask(requestBody);
+
+            try {
+                Long taskId = taskApplicationService.createTask(requestTask.getTitle());
+                Task createdTask = taskApplicationService.findTask(taskId);
+
+                String content = transfer.taskToJson(createdTask);
+                setJsonToResponseBody(exchange, content, 201);
+            } catch (NotFoundTask notFoundTask) {
+                notFoundTask.printStackTrace();
+            }
+        } else {
             exchange.sendResponseHeaders(200, 0);
         }
     }
+
     private void setJsonToResponseBody(HttpExchange exchange, String content, int statusCode) throws IOException {
         exchange.getResponseHeaders().set("Content-Type", "application/json");
         exchange.sendResponseHeaders(statusCode, content.getBytes().length);
--- app/src/main/java/com/codesoom/assignment/web/WebServer.java
+++ app/src/main/java/com/codesoom/assignment/web/WebServer.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.web;
 
+import com.codesoom.assignment.application.TaskApplicationService;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
@@ -8,10 +9,14 @@ import java.net.InetSocketAddress;
 
 public class WebServer {
     HttpServer httpServer;
+    TaskApplicationService taskApplicationService;
+
     public WebServer(int port) throws IOException {
+        taskApplicationService = new TaskApplicationService();
+
         InetSocketAddress address = new InetSocketAddress("localhost", port);
         httpServer = HttpServer.create(address, 0);
-        httpServer.createContext("/", new WebHandler());
+        httpServer.createContext("/", new WebHandler(taskApplicationService));
     }
 
     public void start() {
