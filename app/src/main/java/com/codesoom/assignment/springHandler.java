81805c1cbe41eb80bb5497ae0e3bf7675cfe8d3b
mikekang47 <gyeong0308@gmail.com>
2021-01-20 23:06:30 +0900

    Add uri variable and show path Add .flush and .write method

--- app/src/main/java/com/codesoom/assignment/springHandler.java
+++ app/src/main/java/com/codesoom/assignment/springHandler.java
@@ -5,16 +5,27 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 public class springHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
         String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
 
-        System.out.println(method);
+        System.out.println(method + " " + path);
+
+        String content = "Hello, world!";
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
 
         OutputStream outputStream = exchange.getResponseBody();
+
+        outputStream.write(content.getBytes());
+        outputStream.flush();
         outputStream.close();
 
     }
