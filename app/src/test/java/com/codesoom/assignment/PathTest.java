e102873af2a8e7ec7a6a125090755b6e7e5ca6be
wenodev <logicalstark1105@gmail.com>
2021-01-20 23:24:31 +0900

    테스트코드 주석 작성

--- app/src/test/java/com/codesoom/assignment/PathTest.java
+++ app/src/test/java/com/codesoom/assignment/PathTest.java
@@ -14,12 +14,18 @@ public class PathTest {
         path = "/tasks/1";
     }
 
+    /*
+    목표 : '/'를 기준으로 문자열을 자르고 id값을 얻는다.
+    알아내고 싶은 것 :
+    [1] 올바르게 문자열을 분리했는가?
+    [2] id값을 올바르게 얻었는가?
+    */
     @Test
     void test_id값얻기(){
         String[] array = path.split("/");
         Long id = Long.valueOf(array[2]);
-        assertEquals(array[1], "tasks");
-        assertEquals(id, 1L);
+        assertEquals(array[1], "tasks"); // 알아내고 싶은 것 [1]
+        assertEquals(id, 1L); // 알아내고 싶은 것 [2]
     }
 
 }
