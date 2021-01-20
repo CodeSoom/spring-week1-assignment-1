73caf0cb4cff17eceb19f10b891547bb65e81bc3
Las <haho013039@gmail.com>
2021-01-20 19:19:33 +0900

    seperate server create and open

--- app/src/test/java/com/codesoom/assignment/web/WebServerTest.java
+++ app/src/test/java/com/codesoom/assignment/web/WebServerTest.java
@@ -5,6 +5,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,15 +17,20 @@ public class WebServerTest {
 
     @BeforeEach
     void openServer() {
-        server = new WebServer(8000);
+        assertDoesNotThrow(() -> {
+            server = new WebServer(8000);
+        });
+        server.start();
     }
+
     @AfterEach
     void closeServer() {
         server.close();
     }
+
     @Test
-    void getRoot(){
-        HttpUriRequest request = new HttpGet( "http://localhost:8000");
+    void getRoot() {
+        HttpUriRequest request = new HttpGet("http://localhost:8000");
         assertDoesNotThrow(() -> {
             HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
 
