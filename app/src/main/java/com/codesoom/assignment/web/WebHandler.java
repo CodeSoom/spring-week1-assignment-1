446a689e8d23c6bd75a35fe19757671fa260c320
Las <haho013039@gmail.com>
2021-01-20 19:54:19 +0900

    tasks를 가져올 때 비어있는 경우 구현

--- app/src/main/java/com/codesoom/assignment/web/WebHandler.java
+++ app/src/main/java/com/codesoom/assignment/web/WebHandler.java
@@ -4,13 +4,23 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class WebHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         if (exchange.getRequestURI().getPath().equals("/tasks")) {
+            String content = "[]";
+
             exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, content.getBytes().length);
+
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(content.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } else {
+            exchange.sendResponseHeaders(200, 0);
         }
-        exchange.sendResponseHeaders(200, 0);
     }
 }
--- app/src/test/java/com/codesoom/assignment/web/WebServerTest.java
+++ app/src/test/java/com/codesoom/assignment/web/WebServerTest.java
@@ -5,9 +5,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,14 +39,20 @@ public class WebServerTest {
             assertEquals(200, httpResponse.getStatusLine().getStatusCode());
         });
     }
+
     @Test
     void getAllEmptyTask() {
         HttpUriRequest request = new HttpGet("http://localhost:8000/tasks");
         assertDoesNotThrow(() -> {
             HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+            String responseBody = new BufferedReader(
+                new InputStreamReader(httpResponse.getEntity().getContent()))
+                .lines()
+                .collect(Collectors.joining(""));
 
             assertEquals(200, httpResponse.getStatusLine().getStatusCode());
             assertNotNull(httpResponse.getEntity().getContentType());
+            assertEquals("[]", responseBody);
         });
     }
 }
