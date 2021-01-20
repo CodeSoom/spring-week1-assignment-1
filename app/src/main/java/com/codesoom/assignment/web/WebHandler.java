75432f60831bd60893dfd26f3d1e22fe18751148
Las <haho013039@gmail.com>
2021-01-20 19:40:02 +0900

    set content type to /tasks

--- app/src/main/java/com/codesoom/assignment/web/WebHandler.java
+++ app/src/main/java/com/codesoom/assignment/web/WebHandler.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 public class WebHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        if (exchange.getRequestURI().getPath().equals("/tasks")) {
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+        }
         exchange.sendResponseHeaders(200, 0);
     }
 }
