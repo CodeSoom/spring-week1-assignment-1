fee193ff97076c1ca65a30b1d09e071c9b9fb7dc
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-01-21 15:40:48 +0900

    Add 'HttpStatusCode' class

---
+++ app/src/main/java/com/codesoom/assignment/web/models/HttpStatusCode.java
@@ -0,0 +1,13 @@
+package com.codesoom.assignment.web.models;
+
+public class HttpStatusCode {
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+    public static final int NO_CONTENT = 204;
+
+    public static final int INVALID_PARAMETER = 400;
+    public static final int NOT_FOUND = 404;
+    public static final int METHOD_NOT_ALLOWED = 405;
+
+    public static final int INTERNAL_ERROR = 500;
+}
