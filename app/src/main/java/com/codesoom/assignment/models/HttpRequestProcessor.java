52e6eac32ebd13782e88954d6f3debfca9ba0320
JamieShin0201 <jamieshin1450@gmail.com>
2021-01-21 14:15:23 +0900

    Fix processTask method to default method

--- app/src/main/java/com/codesoom/assignment/models/HttpRequestProcessor.java
+++ app/src/main/java/com/codesoom/assignment/models/HttpRequestProcessor.java
@@ -3,5 +3,7 @@ package com.codesoom.assignment.models;
 import com.codesoom.assignment.service.TaskService;
 
 public interface HttpRequestProcessor {
-    HttpResponse processTasks(HttpRequest httpRequest, TaskService taskService);
+    default HttpResponse processTasks(HttpRequest httpRequest, TaskService taskService) {
+        return null;
+    }
 }
