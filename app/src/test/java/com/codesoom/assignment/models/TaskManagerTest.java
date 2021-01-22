6ca4a8d25f517191ca2859fe0dd52f24b316ea19
juunini <juuni.ni.i@gmail.com>
2021-01-22 14:58:51 +0900

    Fix TaskMangerTest and add doc

--- app/src/test/java/com/codesoom/assignment/models/TaskManagerTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskManagerTest.java
@@ -13,8 +13,13 @@ public class TaskManagerTest {
         TaskManager.clear();
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.find}</p>
+     * <p>상황 : 존재하지 않는 ID를 조회.</p>
+     * <p>기대 : {@code NotExistsIDException} 을 던짐.</p>
+     */
     @Test
-    void findIfNotExist() {
+    void findNotExistID() {
         try {
             TaskManager.find(100);
         } catch (NotExistsIDException ignored) {
@@ -23,25 +28,47 @@ public class TaskManagerTest {
         fail();
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.insert}</p>
+     * <p>상황 : 변수를 입력해 insert.</p>
+     * <p>기대 : 자동 생성된 ID를 가진 Task 가 만들어짐.</p>
+     */
     @Test
-    void successInsertTitle() {
-        Task task = TaskManager.insert("sample");
+    void insertTaskByVariables() {
+        final long id = 1;
+        final String title = "sample";
 
-        assertEquals("sample", task.title());
-        assertEquals(1, task.id());
+        Task task = TaskManager.insert(title);
+
+        assertEquals(title, task.title());
+        assertEquals(id, task.id());
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.insert}</p>
+     * <p>상황 : Task Object 를 입력해 insert.</p>
+     * <p>기대 : 입력된 Task Object 가 그대로 입력됨을 확인.</p>
+     */
     @Test
-    void successInsertTask() throws Exception {
-        TaskManager.insert(new Task(2, "sample2"));
-        Task task = TaskManager.find(2);
+    void insertTaskByTaskObject() {
+        final long id = 2;
+        final String title = "sample2";
 
-        assertEquals("sample2", task.title());
-        assertEquals(2, task.id());
+        TaskManager.insert(new Task(id, title));
+        Task task = TaskManager.find(id);
+
+        assertEquals(title, task.title());
+        assertEquals(id, task.id());
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.insert}</p>
+     * <p>상황 : 이미 존재하는 ID를 가진 Task Object 를 insert.</p>
+     * <p>기대 : {@code AlreadyExistsIDException}를 던짐.</p>
+     */
     @Test
-    void failInsert() {
+    void insertAlreadyExistID() {
+        // insert task. id: 1, title: "sample".
         TaskManager.insert("sample");
 
         try {
@@ -52,18 +79,51 @@ public class TaskManagerTest {
         fail();
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.modify}</p>
+     * <p>상황 : Task Object 를 입력받아 기존의 Task 를 modify.</p>
+     * <p>기대 : 성공적으로 수정됨.</p>
+     */
     @Test
-    void successModify() throws Exception {
+    void modifyTaskByTaskObject() {
+        // insert task. id: 1, title: "sample".
         TaskManager.insert("sample");
-        TaskManager.modify(new Task(1, "sample1"));
 
-        Task task = TaskManager.find(1);
+        final long id = 1;
+        final String title = "modified sample";
 
-        assertEquals("sample1", task.title());
+        TaskManager.modify(new Task(id, title));
+
+        Task task = TaskManager.find(id);
+
+        assertEquals(title, task.title());
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.modify}</p>
+     * <p>상황 : 각각의 변수를 입력받아 modify.</p>
+     * <p>기대 : 성공적으로 수정됨.</p>
+     */
     @Test
-    void failModify() {
+    void modifyTaskByVariables() {
+        // insert task. id: 1, title: "sample".
+        TaskManager.insert("sample");
+
+        final long id = 1;
+        final String title = "modified sample";
+
+        Task task = TaskManager.modify(id, title);
+
+        assertEquals(title, task.title());
+    }
+
+    /**
+     * <p>메소드 : {@code TaskManager.modify}</p>
+     * <p>상황 : 존재하지 않는 ID의 Task 를 modify.</p>
+     * <p>기대 : {@code NotExistsIDException}를 뱉음.</p>
+     */
+    @Test
+    void modifyNotExistID() {
         try {
             TaskManager.modify(new Task(100, "not exist"));
         } catch (NotExistsIDException ignored) {
@@ -72,21 +132,34 @@ public class TaskManagerTest {
         fail();
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.delete}</p>
+     * <p>상황 : 존재하는 ID를 delete.</p>
+     * <p>기대 : 삭제된 ID를 조회하려 하면 {@code NotExistsIDException}를 뱉음.</p>
+     */
     @Test
-    void successDelete() throws Exception {
+    void deleteTask() {
+        // insert task. id: 1, title: "sample".
         TaskManager.insert("sample");
-        TaskManager.delete(1);
+
+        final long id = 1;
+        TaskManager.delete(id);
 
         try {
-            TaskManager.find(1);
+            TaskManager.find(id);
         } catch (NotExistsIDException ignored) {
             return;
         }
         fail();
     }
 
+    /**
+     * <p>메소드 : {@code TaskManager.delete}</p>
+     * <p>상황 : 존재하지 않는 ID를 delete.</p>
+     * <p>기대 : {@code NotExistsIDException}를 뱉음.</p>
+     */
     @Test
-    void failDelete() {
+    void deleteNotExistID() {
         try {
             TaskManager.delete(100);
         } catch (NotExistsIDException ignored) {
