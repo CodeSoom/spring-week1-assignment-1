7c9acb2537356f82a2a0188d47367890d27b9c83
mikekang47 <gyeong0308@gmail.com>
2021-01-22 14:34:54 +0900

    Add httpStatus = 200 and add POST method Change content when call GET method

--- app/src/main/java/com/codesoom/assignment/SpringHandler.java
+++ app/src/main/java/com/codesoom/assignment/SpringHandler.java
@@ -2,13 +2,13 @@ package com.codesoom.assignment;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
 public class SpringHandler implements HttpHandler {
+    static final int httpStatus = 200;
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
@@ -20,7 +20,16 @@ public class SpringHandler implements HttpHandler {
 
         String content = "매일 매일 달리지기 위한 첫걸음 시작하기!";
 
-        exchange.sendResponseHeaders(200, content.getBytes().length);
+        if(method.equals("GET") && path.equals("/tasks")) {
+            content = "[{\"id\":1, \"title\":\"Do nothing\"}]";
+        }
+
+        if(method.equals("POST") && path.equals("/tasks")) {
+            content = "Create a new task.";
+        }
+
+
+        exchange.sendResponseHeaders(httpStatus, content.getBytes().length);
 
         OutputStream outputStream = exchange.getResponseBody();
 
