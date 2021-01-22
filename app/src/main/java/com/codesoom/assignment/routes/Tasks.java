33b12903e6f7496c502a47e278dcb129b3842bb3
juunini <juuni.ni.i@gmail.com>
2021-01-22 15:55:29 +0900

    Add Tasks routes

---
+++ app/src/main/java/com/codesoom/assignment/routes/Tasks.java
@@ -0,0 +1,25 @@
+package com.codesoom.assignment.routes;
+
+import com.codesoom.assignment.models.Task;
+
+public class Tasks {
+    public String get(Long id) {
+
+    }
+
+    public String post(Task task) {
+
+    }
+
+    public String put(Task task) {
+
+    }
+
+    public String patch(long id, String title) {
+
+    }
+
+    public void delete(long id) {
+
+    }
+}
