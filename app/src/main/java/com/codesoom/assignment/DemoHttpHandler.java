8dc0a4cbe063359002aed711f7ac1c93ffa2efb0
newoo <newoo4297@naver.com>
2021-01-23 08:31:54 +0900

    Change the declaration position of a variable so that it is close to the usage

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -17,19 +17,17 @@ public class DemoHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String method = exchange.getRequestMethod();
-        URI uri = exchange.getRequestURI();
-        String path = uri.getPath();
-
         InputStream inputStream = exchange.getRequestBody();
         String body = new BufferedReader(new InputStreamReader(inputStream))
                 .lines()
                 .collect(Collectors.joining("\n"));
 
-        System.out.println(method + " " + path);
-
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
         String content = "";
         int statusCode = Constant.HttpStatusCode.OK;
+
+        System.out.println(method + " " + path);
 
         try {
             content = new ResponseHandler().handle(method, path, tasks, body);
@@ -43,17 +41,14 @@ public class DemoHttpHandler implements HttpHandler {
                     statusCode = Constant.HttpStatusCode.NO_CONTENT;
                     break;
             }
-
         } catch (ResponseHandlingException e) {
             e.printDescription();
-            content = "";
             statusCode = Constant.HttpStatusCode.NOT_FOUND;
         }
 
         exchange.sendResponseHeaders(statusCode, content.getBytes().length);
 
         OutputStream outputStream = exchange.getResponseBody();
-
         outputStream.write(content.getBytes());
         outputStream.flush();
         outputStream.close();
--- app/src/main/java/com/codesoom/assignment/JSONParser.java
+++ app/src/main/java/com/codesoom/assignment/JSONParser.java
@@ -1,6 +1,5 @@
 package com.codesoom.assignment;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.ByteArrayOutputStream;
