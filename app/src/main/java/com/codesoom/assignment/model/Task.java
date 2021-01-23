0b0f5d7279b4e9d2f3a8d067eea0188ecdc069a6
newoo <newoo4297@naver.com>
2021-01-23 16:07:21 +0900

    Do overriding toString() method of task class

--- app/src/main/java/com/codesoom/assignment/model/Task.java
+++ app/src/main/java/com/codesoom/assignment/model/Task.java
@@ -22,4 +22,9 @@ public class Task {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    @Override
+    public String toString() {
+        return "Task -> id: " + id + ", title: " + title;
+    }
 }
