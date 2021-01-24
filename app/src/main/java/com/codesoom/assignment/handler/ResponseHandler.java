57bad08d02eeaaeb1067fa196fe1b2ddda8c82d6
newoo <newoo4297@naver.com>
2021-01-24 15:14:07 +0900

    Create TaskHandler to handle actions about task list

--- app/src/main/java/com/codesoom/assignment/handler/ResponseHandler.java
+++ app/src/main/java/com/codesoom/assignment/handler/ResponseHandler.java
@@ -3,20 +3,18 @@ package com.codesoom.assignment.handler;
 import com.codesoom.assignment.HttpMethod;
 import com.codesoom.assignment.HttpStatusCode;
 import com.codesoom.assignment.ResponseHandlingException;
-import com.codesoom.assignment.helper.TaskParser;
 import com.codesoom.assignment.model.Task;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Handle responses content by requests.
  * TODO: if possible, change switch statement to hashmap.
  */
 public class ResponseHandler {
-    TaskParser taskParser = new TaskParser();
+    TaskHandler taskHandler = new TaskHandler();
 
     public String handle(String method, String path, List<Task> tasks, String body) throws IOException, ResponseHandlingException {
         if (path.equals("/") && HttpMethod.valueOf(method).equals(HttpMethod.HEAD)) {
@@ -34,45 +32,23 @@ public class ResponseHandler {
             case GET:
                 // fetch task list
                 if (path.equals("/tasks")) {
-                    return taskParser.tasksToJSON(tasks);
+                    return taskHandler.handleFetch(tasks);
                 }
 
                 // fetch a task
-                Task selectedTask = taskParser.getTask(tasks, taskId);
-                // TODO: getTask에서 아래의 로직을 처리할 수 있도록 함
-                if (selectedTask.getId() == null) {
-                    throw new ResponseHandlingException(HttpStatusCode.NOT_FOUND);
-                }
-
-                return taskParser.taskToJSON(selectedTask);
+                return taskHandler.handleFetch(tasks, taskId);
 
             case POST:
                 if (body.isBlank()) { break; }
-
-                Task newTask = taskParser.toTask(body, (long) (tasks.size() + 1));
-                tasks.add(newTask);
-                return taskParser.taskToJSON(tasks.get(tasks.size() - 1));
+                return taskHandler.handleAdd(tasks, body);
 
             case PUT:
             case PATCH:
                 if (body.isBlank()) { break; }
-
-                Task editableTask = taskParser.getTask(tasks, taskId);
-                // TODO: getTask에서 아래의 로직을 처리할 수 있도록 함
-                if (editableTask.getId() == null) {
-                    throw new ResponseHandlingException(HttpStatusCode.NOT_FOUND);
-                }
-
-                tasks.set(tasks.indexOf(editableTask), taskParser.toTask(body, editableTask.getId()));
-                return taskParser.taskToJSON(taskParser.getTask(tasks, taskId));
+                return taskHandler.handleUpdate(tasks, taskId, body);
 
             case DELETE:
-                if (taskParser.getTask(tasks, taskId).getId() == null) {
-                    throw new ResponseHandlingException(HttpStatusCode.NOT_FOUND);
-                }
-
-                tasks.removeIf(task -> Objects.equals(taskId, task.getId()));
-                return "";
+                return taskHandler.handleRemove(tasks, taskId);
 
             default:
                 throw new ResponseHandlingException(HttpStatusCode.METHOD_NOT_ALLOWED);
