d21d9e25af90522d9a79209d8ca31467d14d4d60
Flying-Fisherman <ghdud7037@gmail.com>
2021-05-04 21:46:52 +0900

    Create method GET, POST and some modify
    수업 간 강의 내용을 토대로 GET, POST 메서드를 구현하였으며, 각 task에 대한 response 번호 부여 및 출력내용, POST시 들어갈 ID넘버링에 대한 개선을 하였습니다.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,31 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+        // 서버 생성 및 주소, 포트 설정
+        InetSocketAddress address = new InetSocketAddress("localhost", 8000);
+        HttpServer server = HttpServer.create(address, 0);
+
+        // Handler 생성
+        HttpHandler handler = new ServerHttpHandler();
+
+        // Path 지정
+        server.createContext("/", handler);
+
+        // 웹 서버 구동
+        server.start();
+
     }
 }
