5ba836d556cf35ef2a49341730468647b9091c64
Changsu Im <imcxsu@gmail.com>
2021-05-05 04:21:11 +0900

    Add jackson dependency

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -1,22 +1,53 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DemoHttpHandler implements HttpHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<Task> tasks = new ArrayList<>();
+
+    public DemoHttpHandler() {
+        Task task1 = new Task();
+        task1.setId(1L);
+        task1.setTitle("Do nothing...");
+        tasks.add(task1);
+
+        Task task2 = new Task();
+        task2.setId(2L);
+        task2.setTitle("Do anything...");
+        tasks.add(task2);
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         final String method = exchange.getRequestMethod();
         final String path = exchange.getRequestURI().getPath();
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
         System.out.println(method + " " + path);
+
+        if (!body.isBlank()) {
+            Task task = toTask(body);
+            tasks.add(task);
+        }
 
         String content = "Hello, World!";
 
         if (method.equals("GET") && path.equals("/tasks")) {
-            content = "We have no task.";
+            content = tasksToJSON(tasks);
         }
 
         if (method.equals("POST") && path.equals("/tasks")) {
@@ -30,5 +61,15 @@ public class DemoHttpHandler implements HttpHandler {
         outputStream.flush();
         outputStream.close();
         // exchange.close();
+    }
+
+    private Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+
+    private String tasksToJSON(List<Task> tasks) throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+        return outputStream.toString();
     }
 }
