ef957e816371607f8e92aee8ad22aba201b38b9f
youdongkwan <dkyou7@naver.com>
2021-05-07 17:41:37 +0900

    1주차 과제 제출 1. GET 방식 ("/tasks") 	- /tasks or /tasks/ 둘 다 같은 역할을 수행하도록 작업 2. POST방식 ("/tasks") 	- body를 가져와서 list.add 방식으로 구현 	- title만 적으면 id값이 자동으로 +1 되도록 구현 3. PUT / PATCH ("/tasks/{num}") 	- 리스트 해당 인덱스를 찾아 title을 수정가능하도록 구현 4. DELETE 	- 리스트 해당 인덱스를 찾아 제거

---
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -0,0 +1,30 @@
+package com.codesoom.assignment.models;
+
+public class Task {
+    private String title;
+    private Long id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
