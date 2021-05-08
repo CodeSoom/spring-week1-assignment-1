339b479dec54853275cc27f94d4f023b36e74b84
yeonhwa <yeonhwa6717@gmail.com>
2021-05-08 15:57:53 +0900

    Get request path
    Add getRequestURI
    Get path form requestURI
    Log request path

--- app/src/main/java/com/codesoom/assignment/TodoRestApiHandler.java
+++ app/src/main/java/com/codesoom/assignment/TodoRestApiHandler.java
@@ -4,13 +4,18 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.net.URI;
 
 public class TodoRestApiHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
+        URI url = exchange.getRequestURI();
+        String path = url.getPath();
 
         System.out.println((method));
+        System.out.println((path));
+
     }
 }
