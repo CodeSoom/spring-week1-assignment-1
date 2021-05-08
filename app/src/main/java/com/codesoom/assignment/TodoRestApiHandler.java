196f79d753596760ee84d1843984aefd14201807
yeonhwa <yeonhwa6717@gmail.com>
2021-05-08 23:04:04 +0900

    Create GET /tasks reset api
    Set objectMapper.
    Add case "GET" method and "/tasks" path
    Create tasksToJSON.

--- app/src/main/java/com/codesoom/assignment/TodoRestApiHandler.java
+++ app/src/main/java/com/codesoom/assignment/TodoRestApiHandler.java
@@ -1,9 +1,11 @@
 package com.codesoom.assignment;
 
 import com.codesoom.assignment.model.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
@@ -11,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TodoRestApiHandler implements HttpHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
     private List<Task> tasks = new ArrayList<>();
 
     @Override
@@ -21,6 +24,10 @@ public class TodoRestApiHandler implements HttpHandler {
 
         String content = "Todo List";
 
+        if(method.equals("GET") && path.equals("/tasks")) {
+            content = tasksToJSON();
+        }
+
         exchange.sendResponseHeaders(200, content.getBytes().length);
 
         OutputStream outputStream = exchange.getResponseBody();
@@ -28,5 +35,12 @@ public class TodoRestApiHandler implements HttpHandler {
         outputStream.flush();
 
         outputStream.close();
+    }
+
+    private String tasksToJSON() throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
     }
 }
