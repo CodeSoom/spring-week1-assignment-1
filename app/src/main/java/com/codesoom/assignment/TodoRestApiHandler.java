9ef5333aa7debefc6ef764f3f6a195b5c5e06287
yeonhwa <yeonhwa6717@gmail.com>
2021-05-09 00:00:16 +0900

    Create GET /tasks/{id} reset api
    Add tasks.
    Add case "GET" method and path contains "/tasks"
    Create taskToJSON.

--- app/src/main/java/com/codesoom/assignment/TodoRestApiHandler.java
+++ app/src/main/java/com/codesoom/assignment/TodoRestApiHandler.java
@@ -15,7 +15,17 @@ import java.util.List;
 public class TodoRestApiHandler implements HttpHandler {
     private ObjectMapper objectMapper = new ObjectMapper();
     private List<Task> tasks = new ArrayList<>();
+    public TodoRestApiHandler() {
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("Read Book");
+        Task task2 = new Task();
+        task2.setId(2L);
+        task2.setTitle("gkgkgkgkgk");
+        tasks.add(task);
+        tasks.add(task2);
 
+    }
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
@@ -24,8 +34,20 @@ public class TodoRestApiHandler implements HttpHandler {
 
         String content = "Todo List";
 
+
         if(method.equals("GET") && path.equals("/tasks")) {
             content = tasksToJSON();
+        }
+
+        if(method.equals("GET") && path.contains("/tasks/")) {
+            String[] splitPath = path.split("/");
+            String id = splitPath[splitPath.length - 1];
+
+            for (Task task : tasks) {
+                if (task.getId().toString().equals(id)) {
+                    content = taskToJSON(task);
+                }
+            }
         }
 
         exchange.sendResponseHeaders(200, content.getBytes().length);
@@ -40,6 +62,13 @@ public class TodoRestApiHandler implements HttpHandler {
     private String tasksToJSON() throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
+    }
+
+    private String taskToJSON(Task task) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, task);
 
         return outputStream.toString();
     }
