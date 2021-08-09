75e78bf25680b2072fdce209302d2cc02c77224d
HyoungUkJJang <guddnr1451@gmail.com>
2021-08-10 01:57:25 +0900

    코드리뷰 피드백 반영(08/09)
    1. 커밋메시지 신경써서 작성하기
    2. 매직넘버란 ?
    3. import *사용하지 않기
    4. HttpServer.create 메소드 0의 의미
    5. equals NPE 방지하는 방법
    6. Http 상태코드 200의미

--- app/src/main/java/com/codesoom/assignment/CrudHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/CrudHttpHandler.java
@@ -6,7 +6,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.ByteArrayOutputStream;
+
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,13 +36,18 @@ public class CrudHttpHandler implements HttpHandler {
 
         String response = "[]";
 
-        if(method.equals("GET") && path.equals("/task")) {
+
+        /**
+         * method에 null이 들어온다면 비교주체자가 null이 되버리기 때문에 equals를 실행할 수 없어 NPE가 발생할 가능성이 생기는 것 같습니다.
+         * 반대로 "GET".equals(method) 로 변경하면 비교하는 주체가 null이 발생할 일이 없어지기 때문에 NPE 방지가 되는 원리 같습니다.
+         */
+        if("GET".equals(method) && path.equals("/task")) {
             if(!tasks.isEmpty()) {
                 response = toTaskJson();
             }
             exchange.sendResponseHeaders(200,response.getBytes().length);
         }
-        else if(method.equals("POST") && path.equals("/task")) {
+        else if("POST".equals(method) && path.equals("/task")) {
 
             InputStream inputStream = exchange.getRequestBody();
             String content = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
@@ -49,6 +60,10 @@ public class CrudHttpHandler implements HttpHandler {
             exchange.sendResponseHeaders(201,response.getBytes().length);
         }
         else{
+            /**
+             * 서버가 요청을 완료했다는 뜻을 담고 있습니다.
+             * https://datatracker.ietf.org/doc/html/rfc7231#section-6.3.1
+             */
             exchange.sendResponseHeaders(200, 0);
         }
 
