b956af2c19fdb26172789c43948294f13c556081
daeungkim <daeungkim93@gmail.com>
2021-08-10 06:49:40 +0900

    Task 생성자 추가, toJsonOrNull 메서드 추가
    Task 생성자 추가
    -> Http요청을 받아 새로운 Task생성시 setter 보다 생성자를 통하여
    -> Task의 멤버 변수 값을 설정하는것이 더 적적하다고 판단
    Task 개체 Json형식의 문자열로 변환하는 메서드(toJsonOrNull) 추가
    -> 클라이언트가 Task 개체의 값을 요청하는경우
    -> Json 형식의 문자열로 변환하여 전달 해야하므로
    toJsonOrNull메서드 Task 개체가 가지고 있도록 설정
    -> toString처럼 개체의 값을 Json형식의 문자열로 변환하는 메서드의 경우
    -> 개체 내부에 있어야 한다고 판단
    JSON형식의 문자열 변환 실패 에러메시지 추가
    -> toJsonOrNull 메서드가 Task 내부에 있으므로
    -> 에러 메시지 또한 Task 내부에 있어야 한다고 판단

--- app/src/main/java/com/codesoom/assignment/modles/Task.java
+++ app/src/main/java/com/codesoom/assignment/modles/Task.java
@@ -1,11 +1,24 @@
 package com.codesoom.assignment.modles;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public final class Task {
+    public static final String TO_JSON_FAIL_ERROR = "Json conversion fail.";
+
     private Long id;
     private String titie;
 
+    public Task(final Long id, final String titie) {
+        this.id = id;
+        this.titie = titie;
+    }
+
     public Long getId() {
-        return id;
+        return this.id;
     }
 
     public void setId(Long id) {
@@ -13,10 +26,22 @@ public final class Task {
     }
 
     public String getTitie() {
-        return titie;
+        return this.titie;
     }
 
     public void setTitie(String titie) {
         this.titie = titie;
+    }
+
+    public String toJsonOrNull() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final OutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            objectMapper.writeValue(outputStream, this);
+            return outputStream.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
