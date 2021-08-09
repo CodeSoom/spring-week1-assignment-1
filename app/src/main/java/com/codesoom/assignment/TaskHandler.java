aa8c4651a7f1456f52c6dd56097619c7a2d6865a
daeungkim <daeungkim93@gmail.com>
2021-08-10 07:36:04 +0900

    sendResponse 메서드 추가
    sendResponse 메서드 추가
    -> HttpResponse를 전달하는 코드들이 중복되므로 메서드화 하였음

--- app/src/main/java/com/codesoom/assignment/TaskHandler.java
+++ app/src/main/java/com/codesoom/assignment/TaskHandler.java
@@ -4,28 +4,45 @@ import com.codesoom.assignment.modles.Task;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class TaskHandler implements HttpHandler {
     private final List<Task> tasks = new ArrayList<>();
 
+    private void sendResponse(final HttpExchange exchange, final int statusCode, final String content) throws IOException {
+        final OutputStream outputStream = exchange.getResponseBody();
+        exchange.sendResponseHeaders(statusCode, content.getBytes().length);
+        outputStream.write(content.getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
+    }
+
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(final HttpExchange exchange) throws IOException {
         System.out.println(exchange.getRequestURI().getPath());
         System.out.println(exchange.getRequestMethod());
 
-        final String content = "Hello world";
+        final String requestBody = new BufferedReader((new InputStreamReader(exchange.getRequestBody())))
+                .lines().collect(Collectors.joining(System.lineSeparator()));
 
-        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, content.getBytes().length);
+        final Task task = Task.toTaskOrNull(requestBody);
+        if (task == null) {
+            sendResponse(exchange, HttpURLConnection.HTTP_INTERNAL_ERROR, Task.TO_TASK_FAIL);
+            return;
+        }
 
-        OutputStream outputStream = exchange.getResponseBody();
-        outputStream.write(content.getBytes(StandardCharsets.UTF_8));
-        outputStream.close();
+        System.out.println(task.toString());
 
+        final String content = task.toJsonOrNull();
+        if (content == null) {
+            sendResponse(exchange, HttpURLConnection.HTTP_INTERNAL_ERROR, Task.TO_JSON_FAIL);
+            return;
+        }
+
+        sendResponse(exchange, HttpURLConnection.HTTP_OK, content);
     }
 }
