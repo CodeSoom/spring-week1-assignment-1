ce1689f99062d400f38a935fdd6e29e33f48a321
wjy5446 <wjy5446@gmail.com>
2021-08-10 22:41:21 +0900

    feat) Task 기본 생성자 추가

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,14 +1,13 @@
 package com.codesoom.assignment.models;
 
 public class Task {
-    private String title;
     private Long id;
+    private String title;
 
-    public String getTitle() {
-        return title;
-    }
+    public Task() {}
 
-    public void setTitle(String title) {
+    public Task(Long id, String title) {
+        this.id = id;
         this.title = title;
     }
 
@@ -18,6 +17,14 @@ public class Task {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String toString() {
