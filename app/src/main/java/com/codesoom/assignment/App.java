55c76351a800e4f884c917bf601e997f8c19640c
Sungeheon <elon4856@gmail.com>
2021-08-10 20:21:54 +0900

    httpserver를 만드세요
    코드숨 수업내용을 주로 참조하여서 만듬.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,30 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+
+        System.out.println(new App().getGreeting());
+
+        try {
+            Integer portNum = 8000;
+            InetSocketAddress address =new InetSocketAddress(( portNum));
+            HttpServer httpServer = HttpServer.create(address,0);
+            HttpHandler handler = new DemoHttpHandler();
+            httpServer.createContext("/",handler);
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
