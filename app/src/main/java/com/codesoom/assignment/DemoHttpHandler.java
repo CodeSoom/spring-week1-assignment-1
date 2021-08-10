4e11b19cf1a0ce9ae0cc65237315a52419485793
Sungeheon <elon4856@gmail.com>
2021-08-10 20:26:58 +0900

    httphandler를 만드세요.
    코드숨 수업내용을 참조하였음.

---
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -0,0 +1,83 @@
+package com.codesoom.assignment;
+
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DemoHttpHandler implements HttpHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private List<Task> tasks = new ArrayList<>();
+
+    public DemoHttpHandler(){
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("do nothing");
+        tasks.add(task);
+    }
+
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
+
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(method + " "+ path);
+        if(!body.isBlank()){
+
+            Task task = toTask(body);
+            tasks.add(task);
+        };
+
+        String content = "Hello codesoom";
+
+        if (method.equals("GET") && path.equals("/tasks")) {
+            content = tasksToJson();
+        }
+
+        if (method.equals("POST") && path.equals("/tasks")) {
+            content = "Create a new task";
+        }
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+
+    }
+
+    private Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+
+    private String tasksToJson() throws IOException{
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
+    }
+
+
+}
