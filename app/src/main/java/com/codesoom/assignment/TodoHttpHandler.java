e845f418e2c03442c7f7b48b8908c93865e5c7d1
injun <ison@sfu.ca>
2021-08-10 13:26:43 +0900

    TodoHttpHandler 메서드에 따른 분기
    점심시간 종료오 인한 미완성

--- app/src/main/java/com/codesoom/assignment/TodoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/TodoHttpHandler.java
@@ -1,30 +1,76 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class TodoHttpHandler implements HttpHandler {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<Task> tasks = new ArrayList<>();
+
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String PUT = "PUT";
+    private static final String PATCH = "PATCH";
+    private static final String DELETE = "DELETE";
+    private static final String BASIC_URI = "/tasks";
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
         URI uri = exchange.getRequestURI();
         String path = uri.getPath();
-
+        String content = "";
         InputStream inputStream = exchange.getRequestBody();
         String body = new BufferedReader(new InputStreamReader(inputStream))
                 .lines()
                 .collect(Collectors.joining("\n"));
 
         if(!body.isBlank()) {
+            Task task = toTask(body);
+            tasks.add(task);
+        }
+
+        // TODO: 나중에 mehotd들 enum으로 변경해보기
+        if (method.equals(GET)) {
+            if (path.equals(BASIC_URI)) {
+                content = tasksToJSON();
+            }
+        } else if (method.equals(POST)) {
+
+        } else if (method.equals(PUT)) {
+
+        } else if (method.equals(PATCH)) {
+
+        } else if (method.equals(DELETE)) {
 
         }
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+
+    private String tasksToJSON() throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
     }
 }
