bb10c194ad700bf111d6b149e455d34ce4d7b789
nankim <nan.kim@kurlycorp.com>
2021-08-12 17:36:35 +0900

    Remove a magic number
    port number를 상수로 선언하고, 선언한 상수를 파라미터에 넣어주도록 변경했습니다.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class App {
+    private static final Integer PORT_NUMBER = 8000;
+
     public static void main(String[] args) {
         try {
-            InetSocketAddress address = new InetSocketAddress(8000);
+            InetSocketAddress address = new InetSocketAddress(PORT_NUMBER);
             HttpServer httpServer = HttpServer.create(address, 0);
 
             HttpHandler handler = new TodoHttpHandler();
