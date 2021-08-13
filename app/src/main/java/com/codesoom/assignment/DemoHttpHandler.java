45bf9a06638cc84b39192e406acb49f7ae166c6b
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2021-08-13 20:12:10 +0900

    함수로 추출

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -25,6 +25,11 @@ public class DemoHttpHandler implements HttpHandler {
 
     private HashMap<Long, Task> tasks = new HashMap<>();
     private long id = 0L;
+    private int httpStatusCode = 500;
+    private int length = 0;
+    private String content = null;
+    String[] pathElements = null;
+    String body = null;
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
@@ -39,74 +44,37 @@ public class DemoHttpHandler implements HttpHandler {
         String path = uri.getPath();
 
         InputStream inputStream = httpExchange.getRequestBody();
-        String body = new BufferedReader(new InputStreamReader(inputStream))
+        body = new BufferedReader(new InputStreamReader(inputStream))
                 .lines()
                 .collect(Collectors.joining("\n"));
 
         System.out.println(method + " " + path + " " + body);
-        String[] pathElements = null;
+        pathElements = null;
         if (path != null) {
             pathElements = path.split("/");
         }
 
-        String content = "Hello, world!";
+
+
+
+        content = "Hello, world!";
         boolean hasIdVariable = false;
         if(pathElements != null && pathElements.length > 2) {
             hasIdVariable = true;
         }
 
-        int httpStatusCode = 500;
-        int length = 0;
+        httpStatusCode = 500;
+        length = 0;
         if ("GET".equals(method) && Objects.equals(path, "/tasks")) { //전체 조회
-            content = tasksToJSON();
-            System.out.println("/tasks");
-            httpStatusCode = HttpStatusCode.Success.getStatusCode();
+            getTasks();
         } else if ("POST".equals(method) && Objects.equals(path, "/tasks")) { //task 생성
-            if (!body.isEmpty()) {
-                Task task = toTask(body);
-                long tempId = id+1;
-                task.setId(tempId);
-                tasks.put(tempId, task);
-                ++id;
-                content = taskToJSON(id);
-                httpStatusCode = HttpStatusCode.Created.getStatusCode();
-            } else {
-                httpStatusCode = HttpStatusCode.NoContent.getStatusCode();
-            }
+            generateTask();
         } else if ("GET".equals(method) && (hasIdVariable)) { //특정 task 조회
-
-            id = Long.parseLong(pathElements[pathElements.length - 1]);
-
-            if (tasks.get(id) != null) {
-                content = taskToJSON(id);
-                httpStatusCode = HttpStatusCode.Success.getStatusCode();
-                length = content.getBytes().length;
-            } else {
-                httpStatusCode = HttpStatusCode.NotFound.getStatusCode();
-            }
+            getTask();
         } else if (("PATCH".equals(method) || "PUT".equals(method)) && (hasIdVariable)) { // 특정 task 수정
-            id = Long.parseLong(pathElements[pathElements.length - 1]);
-            if (!body.isEmpty() && tasks.get(id) != null) {
-                Task task = toTask(body);
-                task.setId(id);
-                tasks.put(id, task);
-                content = taskToJSON(id);
-                httpStatusCode = HttpStatusCode.Success.getStatusCode();
-                length = content.getBytes().length;
-            } else {
-                httpStatusCode = HttpStatusCode.NotFound.getStatusCode();
-            }
+            modifyTask();
         } else if ("DELETE".equals(method) && (hasIdVariable)) { //특정 task 삭제
-            id = Long.parseLong(pathElements[pathElements.length - 1]);
-            if (tasks.get(id) != null) {
-                tasks.remove(id);
-                content = "task deleted.";
-                httpStatusCode = HttpStatusCode.NoContent.getStatusCode();
-                length = content.getBytes().length;
-            } else {
-                content = "존재하지 않음";
-                httpStatusCode = HttpStatusCode.NotFound.getStatusCode();
-            }
+            deleteTask();
         }
         httpExchange.sendResponseHeaders(httpStatusCode, length);
 
@@ -116,6 +84,65 @@ public class DemoHttpHandler implements HttpHandler {
         outputStream.flush();
         outputStream.close();
 
+    }
+
+    private void modifyTask() throws IOException {
+        id = Long.parseLong(pathElements[pathElements.length - 1]);
+        if (!body.isEmpty() && tasks.get(id) != null) {
+            Task task = toTask(body);
+            task.setId(id);
+            tasks.put(id, task);
+            content = taskToJSON(id);
+            httpStatusCode = HttpStatusCode.Success.getStatusCode();
+            length = content.getBytes().length;
+        } else {
+            httpStatusCode = HttpStatusCode.NotFound.getStatusCode();
+        }
+    }
+
+    private void deleteTask() {
+        id = Long.parseLong(pathElements[pathElements.length - 1]);
+        if (tasks.get(id) != null) {
+            tasks.remove(id);
+            content = "task deleted.";
+            httpStatusCode = HttpStatusCode.NoContent.getStatusCode();
+            length = content.getBytes().length;
+        } else {
+            content = "존재하지 않음";
+            httpStatusCode = HttpStatusCode.NotFound.getStatusCode();
+        }
+    }
+
+    private void generateTask() throws IOException {
+        if (!body.isEmpty()) {
+            Task task = toTask(body);
+            long tempId = id+1;
+            task.setId(tempId);
+            tasks.put(tempId, task);
+            ++id;
+            content = taskToJSON(id);
+            httpStatusCode = HttpStatusCode.Created.getStatusCode();
+        } else {
+            httpStatusCode = HttpStatusCode.NoContent.getStatusCode();
+        }
+    }
+
+    private void getTasks() throws IOException {
+        content = tasksToJSON();
+        System.out.println("/tasks");
+        httpStatusCode = HttpStatusCode.Success.getStatusCode();
+    }
+
+    private void getTask() throws IOException {
+        id = Long.parseLong(pathElements[pathElements.length - 1]);
+
+        if (tasks.get(id) != null) {
+            content = taskToJSON(id);
+            httpStatusCode = HttpStatusCode.Success.getStatusCode();
+            length = content.getBytes().length;
+        } else {
+            httpStatusCode = HttpStatusCode.NotFound.getStatusCode();
+        }
     }
 
     private Task toTask(String content) throws JsonProcessingException {
