ce7f9d852427193772b4eb6faaecb9c50df71a4c
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-08-13 19:21:31 +0900

    feat: Server App Run Add - No Handler

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,25 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final int port = 8000;
 
     public static void main(String[] args) {
+        try {
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+            HttpServer httpServer = HttpServer.create(inetSocketAddress, 0);
+            httpServer.createContext("/");
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getGreeting() {
+        return "Hello World!";
     }
 }
