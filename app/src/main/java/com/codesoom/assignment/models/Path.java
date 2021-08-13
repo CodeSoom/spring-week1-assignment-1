7fd8fa11d1a287ce54cdec0edb9be75e6f667254
wjy5446 <wjy5446@gmail.com>
2021-08-14 04:32:12 +0900

    feat) path 관련 함수를 관심사 분리하기 위해 Path 클래스 생성

--- app/src/main/java/com/codesoom/assignment/models/Path.java
+++ app/src/main/java/com/codesoom/assignment/models/Path.java
@@ -1,2 +1,37 @@
-package com.codesoom.assignment.models;public class Path {
+package com.codesoom.assignment.models;
+
+import java.net.URI;
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Path {
+    String path;
+
+    public Path(URI uri) {
+        this.path = uri.getPath();
+    }
+
+    public boolean has(String sub) {
+        String regex = "/" + sub + "/?$";
+        return Pattern.matches(regex, this.path);
+    }
+
+    public boolean hasIdOf(String sub) {
+        String regex = "/" + sub + "/[0-9]+/?$";
+        return Pattern.matches(regex, this.path);
+    }
+
+    public Long getIdOf(String sub) throws NoSuchElementException {
+        String regex = "/" + sub + "/([0-9]+)/?$";
+        System.out.println(regex);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(this.path);
+
+        if(!matcher.find()) {
+            throw new NoSuchElementException("Not Found id");
+        }
+
+        return Long.parseLong(matcher.group(1));
+    }
 }
