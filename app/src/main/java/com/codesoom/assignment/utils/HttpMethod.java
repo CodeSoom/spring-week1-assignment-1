eed639999ccc249c05a378c02eb1a55c4e7f5e21
daeungkim <daeungkim93@gmail.com>
2021-08-15 18:33:22 +0900

    HttpMethod enum javadoc 추가
    HttpMethod enum javadoc 추가

--- app/src/main/java/com/codesoom/assignment/utils/HttpMethod.java
+++ app/src/main/java/com/codesoom/assignment/utils/HttpMethod.java
@@ -1,9 +1,9 @@
 package com.codesoom.assignment.utils;
 
-/*
-https://datatracker.ietf.org/doc/html/rfc2616
+/**
+ * HTTP 프로토콜 메소드 정의
+ * @link https://datatracker.ietf.org/doc/html/rfc2616
  */
-
 public enum HttpMethod {
     GET,
     POST,
