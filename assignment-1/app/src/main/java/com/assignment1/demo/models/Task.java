b4a30e2158d4b6c282ec741d8237a6d11f4ea160
bloomspes <bloomspes@gmail.com>
2021-08-15 17:03:51 +0900

    ToDo REST API 과제 구현하기 - Jackson API 연동 - ToJSON 생성자 구현 - InputStream, OutputStream, BufferedReader 메소드 구현 - ToTask 생성자 구현 중

---
+++ assignment-1/app/src/main/java/com/assignment1/demo/models/Task.java
@@ -0,0 +1,22 @@
+package com.assignment1.demo.models;
+
+public class Task {
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
