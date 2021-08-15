786f7108d9195afffdbe1146e0da4b603b90d242
daeungkim <daeungkim93@gmail.com>
2021-08-15 18:05:22 +0900

    코드 개행
    코드 개행
    -> 가독성을 위해

--- app/src/main/java/com/codesoom/assignment/controllers/ExceptionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ExceptionController.java
@@ -6,14 +6,20 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 public class ExceptionController extends Controller {
     public void handleInvalidMethod(
             final HttpExchange exchange, final String path, final String[] allowedMethods
     ) throws IOException {
         final StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(TaskHandler.HANDLER_PATH).append(path).append(" can only handle ");
-        Arrays.stream(allowedMethods).forEach(method ->stringBuilder.append(method).append(" "));
+        final Consumer<String> methodAppender = method -> stringBuilder.append(method)
+                        .append(" ");
+        stringBuilder.append(TaskHandler.HANDLER_PATH)
+                    .append(path)
+                    .append(" can only handle ");
+        Arrays.stream(allowedMethods)
+                .forEach(methodAppender);
         stringBuilder.append("methods.");
         sendResponse(exchange, HttpURLConnection.HTTP_BAD_METHOD, stringBuilder.toString());
     }
