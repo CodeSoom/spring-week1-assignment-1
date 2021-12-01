82b4f39aa3045389fdb24cc84bd9f3153f07c776
Kwakcena <rhkrgudwh@gmail.com>
2021-12-02 00:13:51 +0900

    handler에서 method, path를 출력하라
    TodoHttpHandler에서 요청받은 method와 path를 출력합니다.

--- app/src/main/java/com/codesoom/assignment/models/TodoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/models/TodoHttpHandler.java
@@ -4,11 +4,16 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.net.URI;
 
 public class TodoHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
 
+        System.out.println(method + " " + path);
     }
 }
