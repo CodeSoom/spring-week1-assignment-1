831d7ea1e88e677e586bb465daaf58096212aab6
Kwakcena <rhkrgudwh@gmail.com>
2021-12-02 01:18:06 +0900

    Task modle을 만들어라
    Task class를 만들었습니다.

---
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.models;
+
+public class Task {
+    private Long id;
+
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
