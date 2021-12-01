8507095ba594c9da4ecfb047c0a3d15d0459dac8
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-01 20:15:54 +0900

    초기화

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,24 +1,12 @@
 package com.codesoom.assignment;
 
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws IOException {
-        InetSocketAddress address = new InetSocketAddress(8000);
-        HttpServer httpServer = HttpServer.create(address, 0);
-
-        HttpHandler handler = new TaskHandler();
-        httpServer.createContext("/", handler);
-
-        httpServer.start();
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
     }
 
 }
