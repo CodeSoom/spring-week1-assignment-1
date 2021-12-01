9f7b5b89f529cf056a42dcc38ea467dfec1ad672
Kwakcena <rhkrgudwh@gmail.com>
2021-12-01 23:52:40 +0900

    http server를 만들고 실행하라
    8001번 포트를 사용하는 간단한 httpServer를 만들었습니다.
    현재는 context에 handler가 등록되어있지 않아서 500 server error를
    응답합니다.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,25 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        try {
+            InetSocketAddress address = new InetSocketAddress(8001);
+            HttpServer httpServer = HttpServer.create(address, 0);
+            httpServer.createContext("/");
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(new App().getGreeting());
     }
 }
