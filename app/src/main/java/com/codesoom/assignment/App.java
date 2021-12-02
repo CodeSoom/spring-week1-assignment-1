9f53eb4b7aeee8f94fa4b3a8dd90b5a6fe1ab24c
koo6357 <koo6357@naver.com>
2021-12-02 18:46:05 +0900

    [spring week1] 소켓 주소 및 httpServer 생성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,22 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // ip는 와일드카드 이고 port 가 8000 인 소켓 주소를 작성한다.
+        InetSocketAddress address = new InetSocketAddress(8000);
+
+        // 위 address 로 httpServer 를 생성한다.
+        // backlog -> backlog the socket backlog. If this value is less than or equal to zero, then a system default value is used
+        HttpServer httpServer = HttpServer.create(address, 0);
+        
     }
 }
