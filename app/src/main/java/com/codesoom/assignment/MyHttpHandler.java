1e0be077333bb5cf0790e5b7fd41fea4a963d4cc
Seungchul Jang <jangsc0000@gmail.com>
2021-12-02 19:48:09 +0900

    엔드포인트 추가 - /tasks GET - /tasks 엔드포인트의 GET method 추가 - /tasks GET 은 저장된 모든 task를 json포맷으로 반환

--- app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -1,33 +1,65 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.*;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class MyHttpHandler implements HttpHandler {
+    List<Task> tasks = new ArrayList<>();
+
+    public MyHttpHandler() {
+        Task task = new Task();
+        task.setId(1);
+        task.setTitle("test");
+        tasks.add(task);
+        Task task2 = new Task();
+        task2.setId(2);
+        task2.setTitle("test2");
+        tasks.add(task2);
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
         URI requestURI = exchange.getRequestURI();
+        String path = requestURI.toString();
         InputStream inputStream = exchange.getRequestBody();
         String requestBody = new BufferedReader(new InputStreamReader(inputStream))
                 .lines()
                 .collect(Collectors.joining("\n"));
 
-        System.out.println(method + " - " + requestURI);
+        System.out.println(method + " - " + path);
         if (!requestBody.isBlank()) {
             System.out.println(requestBody);
         }
 
-        String content = "Hi, World!";
+
+        String content = "";
+
+        if (method.equals("GET") && path.equals("/tasks")) {
+            content = tasksToJson();
+        }
+
         exchange.sendResponseHeaders(200, content.getBytes().length);
 
         OutputStream responseBody = exchange.getResponseBody();
         responseBody.write(content.getBytes());
         responseBody.close();
+    }
+
+    private String tasksToJson() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
     }
 }
