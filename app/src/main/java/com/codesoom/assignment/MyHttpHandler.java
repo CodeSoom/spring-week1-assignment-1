17363db1fe29d20f8104595266e05a1cc276bd91
jangsc0000 <jangsc0000@gmail.com>
2021-12-04 07:51:58 +0900

    리팩토링 - 각 엔드포인트의 동작들을 createTask, readTask, readTasks 메소드로 분리 - TaskManager를 만들어 List tasks 관리

--- app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHttpHandler.java
@@ -1,110 +1,101 @@
 package com.codesoom.assignment;
 
 import com.codesoom.assignment.models.Task;
-import com.codesoom.assignment.HttpStatusCode;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.*;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class MyHttpHandler implements HttpHandler {
-    List<Task> tasks = new ArrayList<>();
+    TaskManager taskManager = new TaskManager();
     ObjectMapper objectMapper = new ObjectMapper();
-
-    public MyHttpHandler() {
-    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
-        URI requestURI = exchange.getRequestURI();
-
-
-        String path = requestURI.toString();
-        InputStream inputStream = exchange.getRequestBody();
-        String requestBody = new BufferedReader(new InputStreamReader(inputStream))
+        String path = exchange.getRequestURI().toString();
+        String body = new BufferedReader(new InputStreamReader(exchange.getRequestBody()))
                 .lines()
                 .collect(Collectors.joining("\n"));
 
+        printRequestInfo(method, path, body);
+
+        if (method.equals("GET") && path.equals("/tasks")) {
+            readTasks(exchange);
+        }
+
+        if (method.equals("POST") && path.equals("/tasks")) {
+            createTask(exchange, body);
+        }
+
+        if (method.equals("GET") && path.startsWith("/tasks/")) {
+            readTask(exchange, path);
+        }
+
+        String content = "";
+        sendResponse(exchange, HttpStatusCode.NOT_FOUND, content);
+    }
+
+
+    private void createTask(HttpExchange exchange, String body) throws IOException {
+        String content = "";
+        try {
+            Task task = toTask(body);
+            taskManager.insertTask(task);
+            content = taskToJson(task);
+            sendResponse(exchange, HttpStatusCode.CREATED, content);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            sendResponse(exchange, HttpStatusCode.BAD_REQUEST, content);
+        }
+    }
+
+    private void readTask(HttpExchange exchange, String path) throws IOException {
+        String idStr = path.substring(path.lastIndexOf("/") + 1);
+        String content = "";
+        long id = Long.parseLong(idStr);
+        try {
+            Task task = taskManager.readTask(id);
+            content = taskToJson(task);
+            sendResponse(exchange, HttpStatusCode.OK, content);
+        }
+        catch (NoSuchElementException e){
+            e.printStackTrace();
+            sendResponse(exchange, HttpStatusCode.NOT_FOUND, content);
+        }
+    }
+
+    private void readTasks(HttpExchange exchange) throws IOException {
+        String content = tasksToJson();
+        sendResponse(exchange, HttpStatusCode.OK, content);
+    }
+
+    private void printRequestInfo(String method, String path, String requestBody) {
         System.out.println(method + " - " + path);
         if (!requestBody.isBlank()) {
             System.out.println(requestBody);
         }
+    }
 
-
-        String content = "";
-        HttpStatusCode httpStatusCode = HttpStatusCode.NOT_FOUND;
-
-        if (method.equals("GET") && path.equals("/tasks")) {
-            content = tasksToJson();
-            httpStatusCode = HttpStatusCode.OK;
-        }
-
-        if (method.equals("POST") && path.equals("/tasks")) {
-            try {
-                Task task = toTask(requestBody);
-
-                long newId = tasks.size() == 0 ? 1 : tasks.get(tasks.size() - 1).getId() + 1;
-                task.setId(newId);
-                tasks.add(task);
-
-                content = taskToJson(task);
-
-                httpStatusCode = HttpStatusCode.CREATED;
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-                httpStatusCode = HttpStatusCode.BAD_REQUEST;
-            }
-        }
-
-        if (method.equals("GET") && path.startsWith("/tasks/")) {
-            try {
-                String idStr = path.substring(path.lastIndexOf("/") + 1);
-                System.out.println(idStr);
-                long id = Long.parseLong(idStr);
-
-                for (Task task : tasks) {
-                    if (task.getId() == id) {
-                        content = taskToJson(task);
-                        httpStatusCode = HttpStatusCode.OK;
-                        break;
-                    }
-                }
-                if (httpStatusCode != HttpStatusCode.OK)
-                    httpStatusCode = HttpStatusCode.NOT_FOUND;
-
-            } catch (IndexOutOfBoundsException e) {
-                e.printStackTrace();
-                httpStatusCode = HttpStatusCode.NOT_FOUND;
-            } catch (IOException e) {
-                e.printStackTrace();
-                httpStatusCode = HttpStatusCode.INTERNAL_SERVER_ERROR;
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                httpStatusCode = HttpStatusCode.NOT_FOUND;
-            }
-        }
-
+    private void sendResponse(HttpExchange exchange, HttpStatusCode httpStatusCode, String content) throws IOException {
         exchange.sendResponseHeaders(httpStatusCode.getValue(), content.getBytes().length);
         OutputStream responseBody = exchange.getResponseBody();
         responseBody.write(content.getBytes());
         responseBody.close();
     }
 
-    private Task toTask(String requestBody) throws JsonProcessingException {
-        return objectMapper.readValue(requestBody, Task.class);
+    private Task toTask(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, Task.class);
     }
 
     private String tasksToJson() throws IOException {
 
         OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream, tasks);
+        objectMapper.writeValue(outputStream, taskManager.tasks);
 
         return outputStream.toString();
     }
