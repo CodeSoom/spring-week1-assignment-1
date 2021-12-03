38ba1d3a6ec219409c1348752e61735e98ffd8cb
ririti <xodnzlzl@naver.com>
2021-12-03 21:14:28 +0900

    final, BACKLOG 상수 선언 import * 해제하 각항목에 대해 명시(import문 확장)

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -6,17 +6,21 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
+    //8000이랑 BACKLOG를 상수로 선언
+    static final int PORT = 8000;
+    static final int BACKLOG = 0;
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 
         try {
-            InetSocketAddress address = new InetSocketAddress(8000);
-            HttpServer httpServer = HttpServer.create(address, 0);
+            InetSocketAddress address = new InetSocketAddress(PORT);
+            HttpServer httpServer = HttpServer.create(address, BACKLOG);
             HttpHandler handler = new DemoHttpHandler();
             httpServer.createContext("/", handler);
             httpServer.start();
@@ -24,4 +28,5 @@ public class App {
             e.printStackTrace();
         }
     }
+
 }
