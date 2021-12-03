7f47bdf0db216e7cbd1bf7084390e202f4ce7af0
jangsc0000 <jangsc0000@gmail.com>
2021-12-04 02:49:42 +0900

    enum HttpStatus 주석 추가 - RFC2616 section 10의 링크 추가

--- app/src/main/java/com/codesoom/assignment/HttpStatusCode.java
+++ app/src/main/java/com/codesoom/assignment/HttpStatusCode.java
@@ -1,6 +1,12 @@
 package com.codesoom.assignment;
 
+/*
+    Written based on RFC2616 section 10
+    https://datatracker.ietf.org/doc/html/rfc2616#section-10
+*/
+
 public enum HttpStatusCode {
+
     OK(200),
     CREATED(201),
     BAD_REQUEST(400),
