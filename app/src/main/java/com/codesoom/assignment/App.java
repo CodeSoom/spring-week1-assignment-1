5a06a30376a51a908a8b936d3dfc4d928dedf82e
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-04 09:44:54 +0900

    App.java에서 변하지 않는 상수값의 이름을 대문자로 표기 + static 키워드 추가

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -11,14 +11,15 @@ public class App {
         return "Hello World!";
     }
 
+    static final int PORT = 8000; //포트번호
+    static final int BACKLOG = 0; //클라이언트가 요청이 오면 대기열에서 기다리는 갯수
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 
         try {
-            final int port = 8000; //포트번호
-            final int backlog = 0; //클라이언트가 요청이 오면 대기열에서 기다리는 갯수
-            InetSocketAddress address = new InetSocketAddress(port); //서버 정보
-            HttpServer httpServer = HttpServer.create(address, backlog);
+            InetSocketAddress address = new InetSocketAddress(PORT); //서버 정보
+            HttpServer httpServer = HttpServer.create(address, BACKLOG);
 
             String path = "/"; //클라이언트의 요청을 받아들일 수 있는 최소한의 path
             HttpHandler handler = new DemoHttpHandler(); //클라이언트 요청 처리하는 클래스
