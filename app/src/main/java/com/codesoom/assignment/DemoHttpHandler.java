50e28735f0706c47dab94d99772ae828b7e4ed0c
ririti <xodnzlzl@naver.com>
2021-12-04 21:20:30 +0900

    final, BACKLOG 상수 선언 import * 해제하 각항목에 대해 명시(import문 확장)

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -23,16 +23,7 @@ public class DemoHttpHandler implements HttpHandler {
 
 
     private List<Task> tasks = new ArrayList<>();
-
-//    public DemoHttpHandler() {
-//        Task task = new Task();
-//        task.setId(1L);
-//        task.setTitle("DO nothing...");
-//
-//        tasks.add(task);
-//
-//    }
-
+    private Long newId = 0L;
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -41,44 +32,98 @@ public class DemoHttpHandler implements HttpHandler {
         // 2. Path - "/", "/tasks", "/tasks/1", ...
         // 3. Headers, Body(Content)
 
+        //변하지 않는 값
         final String method = exchange.getRequestMethod();
         final URI uri = exchange.getRequestURI();
         final String path = uri.getPath();
 
-        InputStream inputStream = exchange.getRequestBody();
-        String body = new BufferedReader(new InputStreamReader(inputStream))
-                .lines()
-                .collect(Collectors.joining("\n"));
-
         System.out.println(method + " " + path);
-        if (!body.isBlank()) {
-            Task task = toTask(body);
-            tasks.add(task);
+
+
+        if(path.equals("/tasks")) {
+            handleCollection(exchange, method);
+            return;
         }
 
-        String content = "Hello, world!";
-
-        if (method.equals("GET") && path.equals("/tasks")) {
-            content = tasksToJason();
+        if(path.startsWith("/tasks/")) { //startWith에 대해 공부
+            Long id = Long.parseLong(path.substring("/tasks/".length()));
+            handleItem(exchange, method, id);
+            return;
         }
 
-        if (method.equals("POST") && path.equals("/tasks")) {
-            content = toPost();
+        send(exchange, 200, "Hello, world!");
+    }
+
+    private void handleCollection(HttpExchange exchange, String method)
+            throws IOException {
+        if (method.equals("GET") ) {
+            handleList(exchange);
         }
 
-//        if (method.equals("GET") && path.equals("/tasks/")) {
-//            content = toSearch();
-//        }
+        if (method.equals("POST")) {
+            handleCreate(exchange);
+        }
+    }
 
-        if (method.equals("PUT") && path.equals("/tasks/")) {
-            content = "Update title";
+    private void handleItem(HttpExchange exchange, String method, Long id)
+            throws IOException {
+        Task task = findTask(id);
+
+        if(task == null){ //값이 없을때 404코드로 리턴한다.
+            send(exchange, 404, "");
+            return;
         }
 
-        if (method.equals("DELETE") && path.equals("/tasks/")) {
-            content = "DELETE task";
+        if (method.equals("GET") ) {
+            handleDetail(exchange, task);
         }
 
-        exchange.sendResponseHeaders(200, content.getBytes().length);
+        if (method.equals("PUT") || method.equals("PATH")) {//PUT : 전체 PATH : 일부분
+            handleUpdate(exchange, task);
+        }
+
+        if (method.equals("DELETE") ) {
+            handleDelete(exchange, task);
+        }
+    }
+
+    private void handleList(HttpExchange exchange) throws IOException {
+        send(exchange, 200, toJSON(tasks));
+    }
+
+    private void handleDetail(HttpExchange exchange, Task task) throws IOException {
+        send(exchange, 200, toJSON(task));
+    }
+
+    private void handleCreate(HttpExchange exchange) throws IOException {
+        String body = getBody(exchange);//getBody를 만들어주어 밖에서 가져온다
+
+        Task task = toTask(body);
+        task.setId(generateId());
+        tasks.add(task);
+
+        send(exchange, 201, toJSON(task));
+    }
+
+    private void handleUpdate(HttpExchange exchange, Task task) throws IOException {
+        String body = getBody(exchange);//getBody를 만들어주어 밖에서 가져온다
+        Task source = toTask(body);
+
+        task.setTitle(source.getTitle());
+
+        send(exchange, 200, toJSON(task));
+    }
+
+    private void handleDelete(HttpExchange exchange, Task task) throws IOException {
+        tasks.remove(task);
+
+        send(exchange, 200, "");
+    }
+
+
+    private void send(HttpExchange exchange, int statusCode, String content)
+            throws IOException {
+        exchange.sendResponseHeaders(statusCode, content.getBytes().length);
 
         OutputStream outputStream = exchange.getResponseBody();
 
@@ -87,38 +132,41 @@ public class DemoHttpHandler implements HttpHandler {
         outputStream.close();
     }
 
+    private String getBody(HttpExchange exchange) {
+        InputStream inputStream = exchange.getRequestBody();
+        return new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+    }
+
+    //get을 사용시 그대로 나오며 값이 없다면 excption을 준다
+    private Task findTask(Long id) {
+        return tasks.stream()
+                .filter(task -> task.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+        //      .findFirst().get();
+    }
+
     private Task toTask(String content) throws JsonProcessingException {
         return objectMapper.readValue(content, Task.class);
 
     }
+    private String tasksTOJSON() throws IOException {
+        return toJSON(tasks);
+    }
 
-//    private Task toSearch(String content) throws JsonProcessingException {
-//        return objectMapper.readValue(content, tasks.get(id));
-//
-//    }
 
-    private String tasksToJason() throws IOException {
+    private String toJSON(Object object) throws IOException { //task로 넣어줫지만 object로 넣어줘도 문제없다
         OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream, tasks);
+        objectMapper.writeValue(outputStream, object); //witeValue가 리플렉션을 이용해서 알아서 맞춰준다
 
         return outputStream.toString();
     }
 
-    private String toPost() throws IOException {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream, tasks);
-
-        return outputStream.toString();
+    private Long generateId() {
+        newId += 1;
+        return newId;
     }
-
-    private String toDelete() throws IOException {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream, tasks);
-
-        return outputStream.toString();
-    }
-//    private String searchTask(){
-//        objectMapper.writeValue(outputStream, );
-//    }
 
 }
