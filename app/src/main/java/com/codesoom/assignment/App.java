8091026523693d77b79bd0fcd435dbb306a00d03
davidyang2149 <davidyang2149@gmail.com>
2022-03-21 06:50:34 +0900

    아무것도 동작하지 않는 HttpServer를 제작합니다
    - 일등으로 PR을 올리기 위해 아무것도 동작하지 않는 HttpServer를 제작해봅니다.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,22 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        InetSocketAddress address = new InetSocketAddress(8000);
+        try {
+            HttpServer httpServer = HttpServer.create(address, 0);
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
