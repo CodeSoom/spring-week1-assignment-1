8bc103458700a742866d75bfed29ca02036d5d19
SUBIN606 <gaemilee606@gmail.com>
2022-03-21 22:59:54 +0900

    feat: HttpServer 실행

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,23 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+
+    private static int PORT = 8000;
 
     public static void main(String[] args) {
+        try {
+
+            InetSocketAddress address = new InetSocketAddress(PORT);
+            HttpServer httpServer = HttpServer.create(address, 0);
+            httpServer.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
