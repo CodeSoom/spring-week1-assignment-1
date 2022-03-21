8bc10c307affb2c8f8ecaa73bd6606870164e06c
Jeon Gyedong <jeongyedong@gmail.com>
2022-03-21 22:32:32 +0900

    Print "Hello world!"

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,11 @@
 package com.codesoom.assignment;
 
 public class App {
-    public String getGreeting() {
+    public static String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        System.out.println(getGreeting());
     }
 }
