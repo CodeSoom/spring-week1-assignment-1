7fd2c268f36c0f46930f25664038ed8ae594a7e9
Jeon Gyedong <jeongyedong@gmail.com>
2022-03-22 22:24:03 +0900

    CLN: static 키워드 제거

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,11 +1,11 @@
 package com.codesoom.assignment;
 
 public class App {
-    public static String getGreeting() {
+    public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(getGreeting());
+        System.out.println(new App().getGreeting());
     }
 }
