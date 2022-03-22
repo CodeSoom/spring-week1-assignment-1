6eaff06bb2ead1498d57078da838a780388510cc
n00nietzsche <n00nietzsche@gmail.com>
2022-03-23 01:44:51 +0900

    remove useless codes
    remove greeting code and its test.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,6 @@
 package com.codesoom.assignment;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
     }
 }
