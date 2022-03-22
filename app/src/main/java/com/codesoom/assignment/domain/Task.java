d6f4deec8836bbe90be274338ac42b138bad2628
Lee Byoung Seong <dev.bslee@gmail.com>
2022-03-22 21:00:11 +0900

    [Week1] 빌더패턴 제거

--- app/src/main/java/com/codesoom/assignment/domain/Task.java
+++ app/src/main/java/com/codesoom/assignment/domain/Task.java
@@ -2,7 +2,6 @@ package com.codesoom.assignment.domain;
 
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDateTime;
 
@@ -13,60 +12,22 @@ public class Task {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    private Task(TaskBuilder taskBuilder) {
-        if (StringUtils.isNotEmpty(taskBuilder.getTaskId())) {
-            modifyAction(taskBuilder);
-        } else {
-            createdAction(taskBuilder);
-        }
+    private Task(String taskId, String title, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.taskId = taskId;
+        this.title = title;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
-    private void modifyAction(TaskBuilder taskBuilder) {
-        this.taskId = taskBuilder.getTaskId();
-        this.title = taskBuilder.getTitle();
+    public static Task registerTask(String title) {
+        var taskId = RandomStringUtils.randomAlphabetic(8);
+        var task = new Task(taskId, title, LocalDateTime.now(), LocalDateTime.now());
+        return task;
+    }
+
+    public Task updateTitle(String title) {
+        this.title = title;
         this.updatedAt = LocalDateTime.now();
-    }
-
-    private void createdAction(TaskBuilder taskBuilder) {
-        this.taskId = getTaskId();
-        this.title = taskBuilder.getTitle();
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-
-    private String getTaskId() {
-        return RandomStringUtils.randomAlphabetic(8);
-    }
-
-    public static class TaskBuilder {
-        private final String title;
-        private String taskId;
-        private LocalDateTime updatedAt;
-
-        public TaskBuilder(String title) {
-            this.title = title;
-        }
-
-        private void updateTask(String taskId) {
-            this.taskId = taskId;
-            updatedAt = LocalDateTime.now();
-        }
-
-        public String getTaskId() {
-            return taskId;
-        }
-
-        public LocalDateTime getUpdatedAt() {
-            return updatedAt;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public Task build() {
-            return new Task(this);
-        }
+        return this;
     }
 }
