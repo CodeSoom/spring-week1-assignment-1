57404d31af13d5023d038e71abac38a2a61f55af
shineoov <shineoov88@gmail.com>
2022-03-23 01:04:21 +0900

    Task API 메소드 순서 변경

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -64,36 +64,6 @@ public class DemoHttpHandler implements HttpHandler {
         }
     }
 
-    private void response(final HttpExchange exchange, final HttpStatusCode statusCode) throws IOException {
-
-        long responseLength = statusCode.equals(NO_CONTENT) ? NO_CONTENT_RESPONSE_LENGTH : EMPTY_RESPONSE_LENGTH;
-
-        exchange.sendResponseHeaders(statusCode.getCode(), responseLength);
-
-        OutputStream responseOutputStream = exchange.getResponseBody();
-        responseOutputStream.close();
-    }
-
-    private void response(final HttpExchange exchange,
-                          final HttpStatusCode statusCode,
-                          final String content) throws IOException {
-
-        exchange.sendResponseHeaders(statusCode.getCode(), content.getBytes().length);
-
-        OutputStream responseOutputStream = exchange.getResponseBody();
-        responseOutputStream.write(content.getBytes());
-        responseOutputStream.flush();
-        responseOutputStream.close();
-    }
-
-    private String getRequestBody(final HttpExchange exchange) {
-
-        InputStream inputStream = exchange.getRequestBody();
-        return new BufferedReader(new InputStreamReader(inputStream))
-                .lines()
-                .collect(Collectors.joining("\n"));
-    }
-
     private void saveTask(final HttpExchange exchange) throws IOException {
 
         String requestBody = getRequestBody(exchange);
@@ -197,6 +167,36 @@ public class DemoHttpHandler implements HttpHandler {
 
         taskList.remove(findTask);
         response(exchange, NO_CONTENT);
+    }
+
+    private void response(final HttpExchange exchange, final HttpStatusCode statusCode) throws IOException {
+
+        long responseLength = statusCode.equals(NO_CONTENT) ? NO_CONTENT_RESPONSE_LENGTH : EMPTY_RESPONSE_LENGTH;
+
+        exchange.sendResponseHeaders(statusCode.getCode(), responseLength);
+
+        OutputStream responseOutputStream = exchange.getResponseBody();
+        responseOutputStream.close();
+    }
+
+    private void response(final HttpExchange exchange,
+                          final HttpStatusCode statusCode,
+                          final String content) throws IOException {
+
+        exchange.sendResponseHeaders(statusCode.getCode(), content.getBytes().length);
+
+        OutputStream responseOutputStream = exchange.getResponseBody();
+        responseOutputStream.write(content.getBytes());
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+
+    private String getRequestBody(final HttpExchange exchange) {
+
+        InputStream inputStream = exchange.getRequestBody();
+        return new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
     }
 
     private Long getTaskId(final String[] pathArr) throws NumberFormatException {
