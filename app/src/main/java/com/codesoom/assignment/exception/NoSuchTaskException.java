2350d74601d9d336a0a6fac51fb99a336db2f5f2
n00nietzsche <n00nietzsche@gmail.com>
2022-03-24 00:03:50 +0900

    ID로 Task를 검색했는데 Task 를 찾지 못했을 때 사용할 예외 추가

---
+++ app/src/main/java/com/codesoom/assignment/exception/NoSuchTaskException.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.exception;
+
+public class NoSuchTaskException extends RuntimeException {
+    public NoSuchTaskException(String message) {
+        super(message);
+    }
+}
