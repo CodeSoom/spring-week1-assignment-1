ff965988711e7ae80390d82b59cdd2f25497f6ee
n00nietzsche <n00nietzsche@gmail.com>
2022-03-23 20:51:32 +0900

    잘못된 Task JSON을 입력한 경우 나타낼 예외 메세지 추가

---
+++ app/src/main/java/com/codesoom/assignment/exception/WrongTaskJsonException.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.exception;
+
+public class WrongTaskJsonException extends RuntimeException{
+    public WrongTaskJsonException(String message) {
+        super(message);
+    }
+}
