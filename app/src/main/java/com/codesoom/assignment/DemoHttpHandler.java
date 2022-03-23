9e17262e165dbffce6eeef7832c8f0999eebe7e3
5Hanui <gkdud644@naver.com>
2022-03-23 23:04:27 +0900

    Http 요청 처리하기

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -1,14 +1,32 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpHandler;
 
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DemoHttpHandler implements HttpHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private List<Task> tasks = new ArrayList<>();
+
+    public DemoHttpHandler() {
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("Do nothing...");
+
+        tasks.add(task);
+
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -19,12 +37,21 @@ public class DemoHttpHandler implements HttpHandler {
         URI uri = exchange.getRequestURI();
         String path = uri.getPath();
 
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("/n"));
+
         System.out.println(method + " " + path);
+        if(!body.isBlank()) {
+            Task task = toTask(body);
+            tasks.add(task);
+        }
 
         String content = "Hello, world!";
 
         if(method.equals("GET") && path.equals("/tasks")) {
-            content = "We have no task.";
+            content = tasksToJson();
         }
 
         exchange.sendResponseHeaders(200, content.getBytes().length);
@@ -34,5 +61,17 @@ public class DemoHttpHandler implements HttpHandler {
         outputStream.write(content.getBytes());
         outputStream.flush();
         outputStream.close();
+    }
+
+    private Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+
+    private String tasksToJson() throws IOException {
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
     }
 }
