c3ce8194a98dd9b28c30093faedea18d2feddfaf
minhee.son <minhee4735@gmail.com>
2022-03-23 20:40:55 +0900

    refactor `import *` 처럼 전체 import 사용하지 않고 필요한 라이브러리만 import 하도록 수정

--- app/src/main/java/com/codesoom/assignment/handlers/TodoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/handlers/TodoHttpHandler.java
@@ -8,7 +8,12 @@ import com.codesoom.assignment.utils.Logger;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
