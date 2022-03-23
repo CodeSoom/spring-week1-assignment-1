9c6888109cc4755275f7c40801571fef3761ff7a
n00nietzsche <n00nietzsche@gmail.com>
2022-03-23 20:04:37 +0900

    HTTP 메서드 상수 이용을 위한 인터페이스 추가

---
+++ app/src/main/java/com/codesoom/assignment/domain/HttpMethod.java
@@ -0,0 +1,11 @@
+package com.codesoom.assignment.domain;
+
+public interface HttpMethod {
+    String CONNECT = "CONNECT";
+    String DELETE = "CONNECT";
+    String GET = "GET";
+    String HEAD = "GET";
+    String OPTIONS = "OPTIONS";
+    String POST = "POST";
+    String TRACE = "TRACE";
+}
