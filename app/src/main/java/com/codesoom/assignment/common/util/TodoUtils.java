41de01f5248c9ce4464c67ac232df6445f44285f
Lee Byoung Seong <dev.bslee@gmail.com>
2022-03-24 20:15:12 +0900

    [Week1] Todo REST api 만들기 [수행 사항] 1. method, httpStatusCode enum 객체 생성했습니다. 2. HttpResponse 객체에 Serializable 추가했습니다. 3. HttpResponse 객체를 bype array로 만들기 위해 유틸 함수 생성했습니다 4. pathVariable 동적으로 얻기 위한 메소드를 생성했습니다
    [내일 수행사항]
    1. 응답시 body에 데이터가 실리지 않는 원인 분석

--- app/src/main/java/com/codesoom/assignment/common/util/TodoUtils.java
+++ app/src/main/java/com/codesoom/assignment/common/util/TodoUtils.java
@@ -1,31 +1,48 @@
 package com.codesoom.assignment.common.util;
 
+import com.codesoom.assignment.common.logger.Log;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class TodoUtils {
-    public static Map<String,Object> transferStringToJson(String string){
-        ObjectMapper objectMapper =new ObjectMapper();
-        Map<String,Object> transferStringToMap=new HashMap<>();
+    private static final Logger logger = Log.getInstance().getLog(TodoUtils.class);
+
+    public static Map<String, Object> transferStringToJson(String string) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> transferStringToMap = new HashMap<String, Object>();
         try {
-            transferStringToMap=objectMapper.readValue(string,Map.class);
-        }catch (IOException e){
-            System.out.printf(" transferStringToJson -> {}",e.getMessage());
+            transferStringToMap = objectMapper.readValue(string, Map.class);
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
         }
         return transferStringToMap;
     }
 
-    public static String transferJsonToString(Object json){
-        ObjectMapper objectMapper =new ObjectMapper();
+    public static String transferJsonToString(Object json) {
+        ObjectMapper objectMapper = new ObjectMapper();
         String transferJsonToString = null;
         try {
-            transferJsonToString=objectMapper.writeValueAsString(json);
-        }catch (IOException e){
-            System.out.printf(" transferJsonToString -> {}",e.getMessage());
+            transferJsonToString = objectMapper.writeValueAsString(json);
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
         }
         return transferJsonToString;
+    }
+
+    public static byte[] convertToBytes(Object object) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(bos)) {
+            out.writeObject(object);
+            return bos.toByteArray();
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
+        }
+        return null;
     }
 }
--- app/src/main/java/com/codesoom/assignment/server/TodoServerHandler.java
+++ app/src/main/java/com/codesoom/assignment/server/TodoServerHandler.java
@@ -1,9 +1,12 @@
 package com.codesoom.assignment.server;
 
+import com.codesoom.assignment.common.logger.Log;
+import com.codesoom.assignment.common.request.HttpMethod;
 import com.codesoom.assignment.common.request.HttpRequest;
 import com.codesoom.assignment.common.response.HttpResponse;
+import com.codesoom.assignment.common.util.TodoUtils;
 import com.codesoom.assignment.config.AppConfig;
-import com.codesoom.assignment.domain.mapping.*;
+import com.codesoom.assignment.domain.mapping.HttpMapping;
 import com.codesoom.assignment.domain.todo.TodoService;
 import com.codesoom.assignment.infrastructure.mapping.*;
 import com.sun.net.httpserver.HttpExchange;
@@ -13,27 +16,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class TodoServerHandler implements HttpHandler {
     public final AppConfig appConfig;
+    private final Logger logger = Log.getInstance().getLog(TodoServerHandler.class);
+
 
     public TodoServerHandler(AppConfig appConfig) {
         this.appConfig = appConfig;
     }
 
-    /*
-        ToDo request 로그 찍기
-     */
     @Override
     public void handle(HttpExchange exchange) {
         String path = exchange.getRequestURI().getPath();
-        String method = exchange.getRequestMethod().toLowerCase(Locale.ROOT);
+        String method = exchange.getRequestMethod();
         String requestBody = new BufferedReader(
                 new InputStreamReader(exchange.getRequestBody())
         ).lines().collect(Collectors.joining("\n"));
@@ -56,22 +58,22 @@ public class TodoServerHandler implements HttpHandler {
 
     private Map<String, Function<TodoService, HttpMapping>> getHttpMethodToConstructorMap() {
         Map<String, Function<TodoService, HttpMapping>> httpMethodToConstructorMap = new HashMap<>();
-        httpMethodToConstructorMap.put("get", GetHttpMapping::new);
-        httpMethodToConstructorMap.put("post", PostHttpMapping::new);
-        httpMethodToConstructorMap.put("put", PutHttpMapping::new);
-        httpMethodToConstructorMap.put("delete", DeleteHttpMapping::new);
-        httpMethodToConstructorMap.put("patch", PatchHttpMapping::new);
+        httpMethodToConstructorMap.put(HttpMethod.GET.getMethod(), GetHttpMapping::new);
+        httpMethodToConstructorMap.put(HttpMethod.POST.getMethod(), PostHttpMapping::new);
+        httpMethodToConstructorMap.put(HttpMethod.PUT.getMethod(), PutHttpMapping::new);
+        httpMethodToConstructorMap.put(HttpMethod.DELETE.getMethod(), DeleteHttpMapping::new);
+        httpMethodToConstructorMap.put(HttpMethod.PATCH.getMethod(), PatchHttpMapping::new);
         return httpMethodToConstructorMap;
     }
 
     private void writeHttpResponse(HttpExchange httpExchange, HttpResponse httpResponse) {
         try (OutputStream outputStream = httpExchange.getResponseBody()) {
-            byte[] body = httpResponse.getBody().getBytes();
+            byte[] body = TodoUtils.convertToBytes(httpResponse);
             httpExchange.sendResponseHeaders(httpResponse.getStatusCode(), body.length);
             outputStream.write(body);
             outputStream.flush();
         } catch (IOException e) {
-            System.out.printf("in handle -> {}", e.getMessage());
+            logger.severe(e.getMessage());
         }
     }
 
