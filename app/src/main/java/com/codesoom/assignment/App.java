3b561226fe18b835945de2c1ab91a3620d560cee
Jeon Gyedong <jeongyedong@gmail.com>
2022-03-25 21:09:47 +0900

    HttpServer 생성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,5 +1,10 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -7,5 +12,14 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        try {
+            InetSocketAddress address = new InetSocketAddress("localhost", 8000);
+            HttpServer httpServer = HttpServer.create(address, 0);
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
