0ec5ad8d9a686d864a6f752f8257e008b75b0531
Jeon Gyedong <jeongyedong@gmail.com>
2022-03-26 08:31:42 +0900

    CommonHandler 추가

---
+++ app/src/main/java/com/codesoom/assignment/CommonHandler.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Objects;
+
+public class CommonHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        System.out.println(method + " " + path);
+    }
+
+    public void outputResponse(HttpExchange exchange, String content) throws IOException {
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+}
