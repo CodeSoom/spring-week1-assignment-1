865c04428b5f726f5b84191b0b5f830f699a002a
Jeon Gyedong <jeongyedong@gmail.com>
2022-03-26 07:44:04 +0900

    Task 모델 추가

---
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -0,0 +1,22 @@
+package com.codesoom.assignment.models;
+
+public class Task {
+    private int id;
+    private int title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+}
