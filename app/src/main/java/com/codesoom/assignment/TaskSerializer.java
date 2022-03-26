38df6664f45ef19a7382d9667eb918df47c77a6f
Jeon Gyedong <jeongyedong@gmail.com>
2022-03-26 13:18:35 +0900

    TaskSerializer 추가

---
+++ app/src/main/java/com/codesoom/assignment/TaskSerializer.java
@@ -0,0 +1,44 @@
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
+public class TaskSerializer {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public String tasksToJson(ArrayList<Task> tasks) {
+        String jsonString = null;
+        try {
+            jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(tasks);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("e: " + e);
+        }
+        return jsonString;
+    }
+
+    public String taskToJson(Task task) {
+        String jsonString = null;
+        try {
+            jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(task);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("e: " + e);
+        }
+        return jsonString;
+    }
+
+    public Task jsonToTask(String json) {
+        Task task = null;
+        try {
+            task = objectMapper.readValue(json, Task.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("e: " + e);
+        }
+        return task;
+    }
+}
