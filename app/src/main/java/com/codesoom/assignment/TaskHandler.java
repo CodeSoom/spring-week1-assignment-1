e2908b087b26b465b94d3bb0410d19ac758eb5f1
Jeon Gyedong <jeongyedong@gmail.com>
2022-03-26 13:04:48 +0900

    TaskHandler POST 구현

--- app/src/main/java/com/codesoom/assignment/TaskHandler.java
+++ app/src/main/java/com/codesoom/assignment/TaskHandler.java
@@ -1,16 +1,47 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class TaskHandler extends CommonHandler {
+    private ArrayList<Task> tasks = new ArrayList<Task>();
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private String taskToJson(Task task) {
+        String jsonString = null;
+        try {
+            jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(task);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("e: " + e);
+        }
+        System.out.println("JSON string: " + jsonString);
+        return jsonString;
+    }
+
+    private Task jsonToTask(String json) {
+        Task task = null;
+        try {
+            task = objectMapper.readValue(json, Task.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("e: " + e);
+        }
+        System.out.println("Task: " + task);
+        return task;
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
+        System.out.println(method);
         if(Objects.equals(method, "GET")){
             handleGetMethod(exchange);
         }else if(Objects.equals(method, "POST")){
@@ -29,7 +60,12 @@ public class TaskHandler extends CommonHandler {
     }
 
     private void handlePostMethod(HttpExchange exchange) throws IOException {
-        String content = "Task: POST";
+        InputStream inputStream = exchange.getRequestBody();
+        String requestBody = new BufferedReader(new InputStreamReader(inputStream, "UTF-8")).lines().collect(Collectors.joining());
+        Task task = jsonToTask(requestBody);
+        task.setId(tasks.size() + 1);
+        tasks.add(task);
+        String content = taskToJson(task);
         exchange.sendResponseHeaders(201, content.getBytes().length);
         outputResponse(exchange, content);
     }
