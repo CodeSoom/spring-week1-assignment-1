58808b0407c4e7f0271abd07d2f1acbba460bc50
n00nietzsche <n00nietzsche@gmail.com>
2022-03-26 12:54:48 +0900

    TASK에 일련번호 삭제

--- app/src/main/java/com/codesoom/assignment/domain/Task.java
+++ app/src/main/java/com/codesoom/assignment/domain/Task.java
@@ -5,22 +5,14 @@ package com.codesoom.assignment.domain;
  * ID 와 TITLE 을 통해 표현한다.
  */
 public class Task {
-    private static long ID_SERIAL_NUMBER = 1;
-
     private final Long id;
     private String title;
 
-    // ObjectMapper 의 readValue() 메서드를 이용하기 위해 빈 생성자를 추가
-    private Task() {
-        this.id = ID_SERIAL_NUMBER++;
-    }
-
-    public Task(String title) {
-        this.id = ID_SERIAL_NUMBER++;
+    public Task(Long id, String title) {
+        this.id = id;
         this.title = title;
     }
 
-    // getters
     public Long getId() {
         return id;
     }
@@ -32,13 +24,5 @@ public class Task {
     // setters
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                '}';
     }
 }
