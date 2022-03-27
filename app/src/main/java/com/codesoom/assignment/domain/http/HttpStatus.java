66842915df246c48c8cce7a5d03ed2a8db46e23b
jakeseo <n00nietzsche@gmail.com>
2022-03-27 15:35:46 +0900

    Status 추가

--- app/src/main/java/com/codesoom/assignment/domain/http/HttpStatus.java
+++ app/src/main/java/com/codesoom/assignment/domain/http/HttpStatus.java
@@ -1,6 +1,13 @@
 package com.codesoom.assignment.domain.http;
 
+/**
+ * https://datatracker.ietf.org/doc/html/rfc7231#section-6.1
+ */
 public interface HttpStatus {
     int SUCCESS = 200;
     int CREATED = 201;
+    int BAD_REQUEST = 400;
+    int NOT_FOUND = 404;
+    int METHOD_NOT_ALLOWED = 405;
+    int INTERNAL_SERVER_ERROR = 500;
 }
