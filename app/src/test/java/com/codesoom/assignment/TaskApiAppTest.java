bd96f56727ed6b5ba468481c904d73d9ce85f414
jakeseo <n00nietzsche@gmail.com>
2022-03-27 15:59:43 +0900

    Assertions 를 이용한 테스트로 변경

--- app/src/test/java/com/codesoom/assignment/TaskApiAppTest.java
+++ app/src/test/java/com/codesoom/assignment/TaskApiAppTest.java
@@ -7,10 +7,7 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,6 +22,12 @@ class TaskApiAppTest {
     @BeforeAll
     public static void startServer() throws IOException {
         TaskApiApp.startLocalHttpServer();
+    }
+
+    @BeforeEach
+    public void clear() throws IOException {
+        saveTask();
+        saveTask();
     }
 
     public String makeTitleJson(String title) {
@@ -42,51 +45,36 @@ class TaskApiAppTest {
     @Test
     @DisplayName("태스크를 등록하고, Task 목록 조회 API 를 호출하면 등록된 Task 들과 상태 200을 반환한다.")
     public void getTasks() throws IOException {
-        saveTask();
-        saveTask();
-        HttpResponse httpResponse = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS));
-        String content = httpResponse.getContent();
-        System.out.println("content = " + content);
+        String readTasks = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS)).getContent();
+        Assertions.assertEquals(readTasks, "[{\"id\":1,\"title\":\"아무것도 안하기\"},{\"id\":2,\"title\":\"아무것도 안하기\"}]");
 
-        HttpResponse httpResponse2 = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS + "/2"));
-        String content1 = httpResponse2.getContent();
-        System.out.println("content1 = " + content1);
+        String readTaskId2 = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS + "/2")).getContent();
+        Assertions.assertEquals(readTaskId2, "{\"id\":2,\"title\":\"아무것도 안하기\"}");
     }
 
     @Test
     @DisplayName("태스크를 등록하고, Task 삭제 API 를 호출하면 해당 Id의 Task 가 삭제된다.")
     public void deleteTask() throws IOException {
-        saveTask();
-        saveTask();
+        String deleteTask = getHttpResponse(new HttpDelete(LOCAL_HOST_TASKS + "/2")).getContent();
+        Assertions.assertEquals(deleteTask, "task id: 2가 삭제되었습니다.");
 
-        String content = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS)).getContent();
-        System.out.println("content = " + content);
-
-        String content1 = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS + "/2")).getContent();
-        System.out.println("content1 = " + content1);
-
-        String content2 = getHttpResponse(new HttpDelete(LOCAL_HOST_TASKS + "/2")).getContent();
-        System.out.println("content2 = " + content2);
-
-        String content3 = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS)).getContent();
-        System.out.println("content3 = " + content3);
+        String readTasks = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS)).getContent();
+        Assertions.assertEquals(readTasks, "[{\"id\":1,\"title\":\"아무것도 안하기\"}]");
     }
 
     @Test
     @DisplayName("태스크를 등록하고, Task 수정 API 를 호출하면 해당 Id의 Task 가 수정된다.")
     public void updateTask() throws IOException {
-        saveTask();
-        saveTask();
-
-        String content = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS)).getContent();
-        System.out.println("content = " + content);
+        String readTasks = getHttpResponse(new HttpGet(LOCAL_HOST_TASKS)).getContent();
+        Assertions.assertEquals(readTasks, "[{\"id\":1,\"title\":\"아무것도 안하기\"},{\"id\":2,\"title\":\"아무것도 안하기\"}]");
 
         HttpPut httpRequest = new HttpPut(LOCAL_HOST_TASKS + "/2");
-        StringEntity requestEntity = new StringEntity(makeTitleJson("아무것도 안하기 (수정 테스트)"), ContentType.APPLICATION_JSON);
+        String updatedTitle = "아무것도 안하기 (수정 테스트)";
+        StringEntity requestEntity = new StringEntity(makeTitleJson(updatedTitle), ContentType.APPLICATION_JSON);
         httpRequest.setEntity(requestEntity);
 
-        String content1 = getHttpResponse(httpRequest).getContent();
-        System.out.println("content1 = " + content1);
+        String updatedTask = getHttpResponse(httpRequest).getContent();
+        Assertions.assertEquals(updatedTask, "{\"id\":2,\"title\":\"아무것도 안하기 (수정 테스트)\"}");
     }
 
     @Test
