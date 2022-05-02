004296d83c7272f3a10fcee3ff2ee40cc4e2fa82
minjoon_lee <minjoon1995@naver.com>
2022-05-02 20:07:32 +0900

    feature : handler 기능 추가

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,27 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
-    public static void main(String[] args) {
-    }
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class App {
+	public String getGreeting() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new App().getGreeting());
+
+		try {
+			InetSocketAddress address = new InetSocketAddress(8080);
+			HttpServer httpServer = HttpServer.create(address, 0);
+			HttpHandler handler = new DemoHttpHandler();
+			httpServer.createContext("/", handler);
+			httpServer.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -0,0 +1,104 @@
+package com.codesoom.assignment;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpHandler;
+
+public class DemoHttpHandler implements HttpHandler {
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	private static int ID = 1;
+
+	private List<Task> tasks = new ArrayList<>();
+
+	public DemoHttpHandler() {
+	}
+
+	@Override
+	public void handle(com.sun.net.httpserver.HttpExchange exchange) throws IOException {
+		String method = exchange.getRequestMethod();
+		URI uri = exchange.getRequestURI();
+		String path = uri.getPath();
+		String idStr = path.substring(path.lastIndexOf('/') + 1);
+
+		InputStream inputStream = exchange.getRequestBody();
+		String body = new BufferedReader(new InputStreamReader((inputStream)))
+			.lines()
+			.collect(Collectors.joining("\n"));
+		String content = "Hello world" + " " + path;
+
+		if (method.equals("GET") && path.equals("/tasks") && !canConvertId(idStr)) {
+			content = tasksToJSON();
+		} else if (method.equals("GET") && canConvertId(idStr)) {
+			content = taskToJSON(new Long(idStr));
+		} else if (method.equals("POST") && path.equals("/tasks")) {
+			if (!body.isEmpty()) {
+				Task task = toTask(body);
+				tasks.add(task);
+			}
+			content = tasksToJSON();
+		} else if (method.equals("PUT") && canConvertId(idStr)) {
+			if (!body.isEmpty()) {
+				Long id = new Long(Integer.parseInt(idStr));
+				Task task = tasks.stream().filter(t -> t.getId().equals(id)).findAny().orElseThrow(
+					IllegalArgumentException::new);
+				Task changeTask = toTask(body);
+				task.setTitle(changeTask.getTitle());
+			}
+			content = tasksToJSON();
+		} else if (method.equals("DELETE") && canConvertId(idStr)) {
+			Long id = new Long(Integer.parseInt(idStr));
+			Task task = tasks.stream().filter(t -> t.getId().equals(id)).findAny().orElseThrow(
+				IllegalArgumentException::new);
+			tasks.remove(task);
+			content = tasksToJSON();
+		}
+
+		exchange.sendResponseHeaders(200, content.getBytes().length);
+		OutputStream outputStream = exchange.getResponseBody();
+		outputStream.write(content.getBytes());
+		outputStream.flush();
+		outputStream.close();
+	}
+
+	private boolean canConvertId(String idStr) {
+		try {
+			Integer.parseInt(idStr);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private Task toTask(String content) throws JsonProcessingException {
+		Task task = objectMapper.readValue(content, Task.class);
+		task.setId(new Long(ID));
+		ID += 1;
+		return task;
+	}
+
+	private String taskToJSON(Long id) throws IOException {
+		OutputStream outputStream = new ByteArrayOutputStream();
+		Task task = tasks.stream().filter(t -> t.getId().equals(id)).findAny().orElseThrow(IllegalAccessError::new);
+		objectMapper.writeValue(outputStream, task);
+		return outputStream.toString();
+	}
+
+	private String tasksToJSON() throws IOException {
+		OutputStream outputStream = new ByteArrayOutputStream();
+		objectMapper.writeValue(outputStream, tasks);
+		return outputStream.toString();
+	}
+}
