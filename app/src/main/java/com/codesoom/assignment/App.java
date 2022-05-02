fb4c9bedf1a128618a64e2c73361f4c7629b84c3
MJbae <bmj5208@gmail.com>
2022-05-02 17:27:23 +0900

    Define initial App

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,22 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
     public static void main(String[] args) {
+        try {
+            InetSocketAddress address = new InetSocketAddress(8000);
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new DemoHttpHandler();
+
+            WebServer server = new WebServer(httpServer, handler);
+            server.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
