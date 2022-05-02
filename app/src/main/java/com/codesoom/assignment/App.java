1440d8e9b6aea0ec0b84aa8be99645f40971341f
10M <endlesslhk@gmail.com>
2022-05-02 20:09:44 +0900

    feat(RequestHandler): Add `RequestHandler` and add `getTaskProcessor` function

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,20 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.handler.RequestHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create();
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("localhost", 8000);
+        httpServer.bind(inetSocketAddress,0);
+        httpServer.createContext("/", exchange -> new RequestHandler().handle(exchange));
+        httpServer.start();
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/handler/RequestHandler.java
@@ -0,0 +1,53 @@
+package com.codesoom.assignment.handler;
+
+import com.codesoom.assignment.models.MethodType;
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestHandler implements HttpHandler {
+    private final List<Task> tasks = new ArrayList<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        MethodType methodType = MethodType.valueOf(exchange.getRequestMethod());
+
+        switch (methodType) {
+            case GET:
+                getTasksProcessor(exchange);
+                break;
+            case POST:
+                break;
+            case PUT:
+                break;
+            case DELETE:
+                break;
+            default:
+                throw new IllegalArgumentException("지원되지 않는 METHOD 타입입니다.");
+        }
+
+    }
+
+    private void getTasksProcessor(HttpExchange exchange) throws IOException {
+        URI uri = exchange.getRequestURI();
+        if (uri.getPath().equals("/tasks")) {
+            String content = objectMapper.writeValueAsString(tasks);
+            printOutputStream(exchange, content);
+        }
+    }
+
+    private void printOutputStream(HttpExchange exchange, String content) throws IOException {
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+}
