4b82fe1da647b9e3e18e579285f7ce4cef603335
choyumin01 <choyumin01@gmail.com>
2022-05-03 16:32:14 +0900

    How to make RestAPI ...

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,23 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.out.println(new App().getGreeting());
+
+        InetSocketAddress address = new InetSocketAddress(8000);
+        HttpServer httpServer = HttpServer.create(address, 0);
+        HttpHandler handler = new AssignmentHttpHandler();
+        httpServer.createContext("/", handler);
+        httpServer.start();
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/AssignmentHttpHandler.java
@@ -0,0 +1,74 @@
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class AssignmentHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+
+//    public AssignmentHttpHandler() {
+//        Task task = new Task();
+//        task.setId(1L);
+//        task.setTitle("Do nothing...");
+//
+//        tasks.add(task);
+//    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                        .lines()
+                        .collect(Collectors.joining("\n"));
+
+        System.out.println(method + " " + path);
+        System.out.println(body);
+
+        String content = "Hello, world!";
+
+        if (method.equals("GET") && path.equals("/tasks")) {
+            content = tasksToJson();
+        }
+
+        if (method.equals("POST") && path.equals("/tasks")) {
+            content = tasksToJson();
+        }
+
+        if (method.equals("PUT") && path.equals("/tasks")) {
+            content = tasksToJson();
+        }
+
+        if (method.equals("DELETE") && path.equals("/tasks")) {
+            content = tasksToJson();
+        }
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private String tasksToJson() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+
+        return outputStream.toString();
+    }
+}
