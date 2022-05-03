f97ec1ffb25940ccc462d0f6e90b05c5398b3dca
hy43 <hyde0401@gmail.com>
2022-05-04 08:12:05 +0900

    feat : taskId 존재 여부에 따라 분기 처리 추가

--- app/src/main/java/com/codesoom/assignment/CustomHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/CustomHttpHandler.java
@@ -1,13 +1,61 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomHttpHandler implements HttpHandler {
+
+    private List<Task> tasks = new ArrayList<>();
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        InputStream inputStream = exchange.getRequestBody();
+
+        String requestBody = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        Task task = null;
+        if (!requestBody.isBlank()) {
+            task = jsonToTask(requestBody);
+        }
+
+        System.out.println(task);
+
+        if(hasTaskId(path)) {
+            if ("PUT".equals(method) || "PACTH".equals(method)) {
+                System.out.println("put/patch");
+            } else if ("DELETE".equals(method)) {
+                System.out.println("delete");
+            } else if ("GET".equals(method)) {
+                System.out.println("get");
+            }
+        } else {
+            if ("GET".equals(method)) {
+                System.out.println("get");
+            } else if ("POST".equals(method)) {
+                System.out.println("post");
+            }
+        }
+    }
+
+    private Task jsonToTask(String requestBody) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(requestBody, Task.class);
+    }
+
+    private boolean hasTaskId(String path) {
+        return path.split("/")[2].isBlank();
     }
 }
