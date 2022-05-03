62008ccd1baced1ce9c336e753d30e4597ebff54
hy43 <hyde0401@gmail.com>
2022-05-04 07:22:40 +0900

    feat : HttpServer 틀 구현

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,22 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
     public static void main(String[] args) {
+        InetSocketAddress address = new InetSocketAddress("localhost", 8000);
+        HttpServer httpServer = null;
+        try {
+            httpServer = HttpServer.create(address, 0);
+            httpServer.start();
+            CustomHttpHandler customHttpHandler = new CustomHttpHandler();
+            httpServer.createContext("/", customHttpHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/CustomHttpHandler.java
@@ -0,0 +1,13 @@
+package com.codesoom.assignment;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class CustomHttpHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
