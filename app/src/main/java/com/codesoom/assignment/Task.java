b16dcc2eae6df39c241ab2a423c018c06555363a
MJbae <bmj5208@gmail.com>
2022-05-04 16:12:22 +0900

    feat: 요구사항의 Task 데이터를 다루기 위해 Task 모델 정의
    what: Task 인스턴스 객체 생성 시 Task 클래스 멤버 변수인 maxId를 기준으로 1을 증가신킨 값을 Id로 부여

---
+++ app/src/main/java/com/codesoom/assignment/Task.java
@@ -0,0 +1,34 @@
+package com.codesoom.assignment;
+
+public class Task {
+    static private Long maxId = 1L;
+    private final Long id;
+    private final String title;
+
+    Task(String title) {
+        this.id = generateId();
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    private Long generateId() {
+        Long generatedId = maxId;
+        maxId += 1;
+        return generatedId;
+    }
+}
