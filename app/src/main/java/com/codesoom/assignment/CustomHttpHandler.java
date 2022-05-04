fc1c97fb6d1c6a4757f78b329cfb7312064cc9fd
hy43 <hyde0401@gmail.com>
2022-05-04 13:36:56 +0900

    feat : findbyId 추가, 반영 중

--- app/src/main/java/com/codesoom/assignment/CustomHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/CustomHttpHandler.java
@@ -7,6 +7,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 public class CustomHttpHandler implements HttpHandler {
 
     private List<Task> tasks = new ArrayList<>();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -31,27 +33,53 @@ public class CustomHttpHandler implements HttpHandler {
             task = jsonToTask(requestBody);
         }
 
-        System.out.println(task);
-
+        String content = "";
+        List<Task> result = new ArrayList<>();
         if(hasTaskId(path)) {
             if ("PUT".equals(method) || "PACTH".equals(method)) {
-                System.out.println("put/patch");
+                int index = findById(task.getId());
+                tasks.get(index).setTitle(task.getTitle());
+                result.add(tasks.get(index));
+                content = taskToJson(result);
             } else if ("DELETE".equals(method)) {
-                System.out.println("delete");
+                tasks.remove(task);
+                content = taskToJson(tasks);
             } else if ("GET".equals(method)) {
-                System.out.println("get");
+                content = taskToJson(result);
             }
         } else {
             if ("GET".equals(method)) {
-                System.out.println("get");
+                content = taskToJson(tasks);
             } else if ("POST".equals(method)) {
-                System.out.println("post");
+                task.setId((long) tasks.size());
+                tasks.add(task);
+                content = taskToJson(tasks);
             }
         }
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+        OutputStream responseBody = exchange.getResponseBody();
+        responseBody.write(content.getBytes());
+        responseBody.flush();
+    }
+
+    private int findById(Long id) {
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private String taskToJson(List<Task> taskList) throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, taskList);
+
+        return outputStream.toString();
     }
 
     private Task jsonToTask(String requestBody) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(requestBody, Task.class);
     }
 
