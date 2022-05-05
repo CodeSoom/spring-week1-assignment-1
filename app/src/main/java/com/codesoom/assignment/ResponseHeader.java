44590835fd35bf71b82da11f9215210d9f119498
MJbae <bmj5208@gmail.com>
2022-05-05 21:01:07 +0900

    feat: content와 http status code를 효과적으로 관리하기 위해 ResponseHeader 객체 생성

---
+++ app/src/main/java/com/codesoom/assignment/ResponseHeader.java
@@ -0,0 +1,38 @@
+package com.codesoom.assignment;
+
+public class ResponseHeader {
+    private int statusCode;
+    private String content;
+
+    ResponseHeader() {
+    }
+
+    ResponseHeader(int statusCode, String content) {
+        this.statusCode = statusCode;
+        this.content = content;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseHeader{" +
+                "statusCode=" + statusCode +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
