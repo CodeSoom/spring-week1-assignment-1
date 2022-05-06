57efb7eb1dcedeaa856e627a8481d7002d908866
choyumin01 <choyumin01@gmail.com>
2022-05-06 20:30:22 +0900

    feature(TaskRepository): save, findById, findAll, delete 기능 추가

---
+++ app/src/main/java/com/codesoom/assignment/repository/TaskRepository.java
@@ -0,0 +1,42 @@
+package com.codesoom.assignment.repository;
+
+import com.codesoom.assignment.models.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TaskRepository {
+    private static Map<Long, Task> store = new HashMap<>();
+    private static long sequence = 1L;
+
+    private static final TaskRepository instance = new TaskRepository();
+
+    public static TaskRepository getInstance() {
+        return instance;
+    }
+
+    private TaskRepository() {}
+
+    public Task save(Task task) {
+        task.setId(sequence);
+        store.put(task.getId(), task);
+
+        sequence++;
+        return task;
+    }
+
+    public Task findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Task> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void delete(Long id) {
+        store.remove(id);
+    }
+
+}
