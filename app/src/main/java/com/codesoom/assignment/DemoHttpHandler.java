f6f3702fdf2c820837cc64a7a3056b29a7e0e154
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-05-07 13:52:14 +0900

    refactoring : handle method에서 isStartwith("task")를 판단해주기 때문에 isStartWithTask method를 삭제

--- app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -1,15 +1,5 @@
 package com.codesoom.assignment;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.codesoom.assignment.models.Task;
 import com.codesoom.assignment.response.ResponseCreated;
 import com.codesoom.assignment.response.ResponseNoContent;
@@ -19,141 +9,136 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DemoHttpHandler implements HttpHandler {
-	private static final int OK = 200;
-	private static final int NO_CONTENT = 204;
-	private static final int CREATED = 201;
-	private final ObjectMapper objectMapper = new ObjectMapper();
-	private final List<Task> tasks = new ArrayList<>();
-	private int id = 1;
+    private static final int OK = 200;
+    private static final int NO_CONTENT = 204;
+    private static final int CREATED = 201;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final List<Task> tasks = new ArrayList<>();
+    private int id = 1;
 
-	protected DemoHttpHandler() {
-	}
+    protected DemoHttpHandler() {
+    }
 
-	@Override
-	public void handle(HttpExchange exchange) throws IOException {
-		String path = exchange.getRequestURI().getPath();
-		if (path.equals("/tasks")) {
-			handleCollection(exchange);
-		} else if (path.startsWith("/tasks/")){
-			handleItem(exchange);
-		}
-	}
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath();
+        if (path.equals("/tasks")) {
+            handleCollection(exchange);
+        } else if (path.startsWith("/tasks/")) {
+            handleItem(exchange);
+        }
+    }
 
-	private void handleCollection(HttpExchange exchange) throws IOException {
-		String method = exchange.getRequestMethod();
-		if (method.equals("GET")) {
-			getAllTasks(exchange);
-		} else if (method.equals("POST")) {
-			createTask(exchange);
-		}
-	}
+    private void handleCollection(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        if (method.equals("GET")) {
+            getAllTasks(exchange);
+        } else if (method.equals("POST")) {
+            createTask(exchange);
+        }
+    }
 
-	private void handleItem(HttpExchange exchange) throws IOException {
-		String method = exchange.getRequestMethod();
-		if (method.equals("GET")) {
-			getOneTask(exchange);
-		} else if (method.equals("PUT")) {
-			updateTask(exchange);
-		} else if (method.equals("DELETE")) {
-			deleteTask(exchange);
-		}
-	}
+    private void handleItem(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        if (method.equals("GET")) {
+            getOneTask(exchange);
+        } else if (method.equals("PUT")) {
+            updateTask(exchange);
+        } else if (method.equals("DELETE")) {
+            deleteTask(exchange);
+        }
+    }
 
-	private void deleteTask(HttpExchange exchange) throws IOException {
-		if (isStartWithTasks(exchange)) {
-			Long id = getIdFromPath(exchange);
-			Task task = tasks.stream()
-				.filter(t -> t.getId().equals(id))
-				.findAny()
-				.orElseThrow(IllegalArgumentException::new);
-			tasks.remove(task);
-			String content = tasksToJSON();
-			exchange.sendResponseHeaders(NO_CONTENT, -1);
-			new ResponseNoContent(exchange).send(content);
-		}
-	}
+    private void deleteTask(HttpExchange exchange) throws IOException {
+        Long id = getIdFromPath(exchange);
+        Task task = tasks.stream()
+                .filter(t -> t.getId().equals(id))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+        tasks.remove(task);
+        String content = tasksToJSON();
+        exchange.sendResponseHeaders(NO_CONTENT, -1);
+        new ResponseNoContent(exchange).send(content);
+    }
 
-	private void updateTask(HttpExchange exchange) throws IOException {
-		if (isStartWithTasks(exchange)) {
-			InputStream inputStream = exchange.getRequestBody();
-			String body = new BufferedReader(new InputStreamReader((inputStream))).lines()
-				.collect(Collectors.joining("\n"));
-			if (!body.isEmpty()) {
-				Long id = getIdFromPath(exchange);
-				Task task = tasks.stream()
-					.filter(t -> t.getId().equals(id))
-					.findAny()
-					.orElseThrow(IllegalArgumentException::new);
-				Task changeTask = contentToTask(body, false);
-				task.setTitle(changeTask.getTitle());
-			}
-			String content = tasksToJSON();
-			new ResponseCreated(exchange).send(content);
-		}
-	}
+    private void updateTask(HttpExchange exchange) throws IOException {
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader((inputStream))).lines()
+                .collect(Collectors.joining("\n"));
+        if (!body.isEmpty()) {
+            Long id = getIdFromPath(exchange);
+            Task task = tasks.stream()
+                    .filter(t -> t.getId().equals(id))
+                    .findAny()
+                    .orElseThrow(IllegalArgumentException::new);
+            Task changeTask = contentToTask(body, false);
+            task.setTitle(changeTask.getTitle());
+        }
+        String content = tasksToJSON();
+        new ResponseCreated(exchange).send(content);
+    }
 
-	private void createTask(HttpExchange exchange) throws IOException {
-		if (isStartWithTasks(exchange)) {
-			InputStream inputStream = exchange.getRequestBody();
-			String body = new BufferedReader(new InputStreamReader((inputStream))).lines()
-				.collect(Collectors.joining("\n"));
-			if (!body.isEmpty()) {
-				Task task = contentToTask(body, true);
-				tasks.add(task);
-			}
-			String content = tasksToJSON();
-			new ResponseCreated(exchange).send(content);
-		}
-	}
+    private void createTask(HttpExchange exchange) throws IOException {
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader((inputStream))).lines()
+                .collect(Collectors.joining("\n"));
+        if (!body.isEmpty()) {
+            Task task = contentToTask(body, true);
+            tasks.add(task);
+        }
+        String content = tasksToJSON();
+        new ResponseCreated(exchange).send(content);
+    }
 
-	private void getAllTasks(HttpExchange exchange) throws IOException {
-		String content = tasksToJSON();
-		new ResponseOK(exchange).send(content);
-	}
+    private void getAllTasks(HttpExchange exchange) throws IOException {
+        String content = tasksToJSON();
+        new ResponseOK(exchange).send(content);
+    }
 
-	private void getOneTask(HttpExchange exchange) throws IOException {
-		String content = taskToJSON(getIdFromPath(exchange));
-		new ResponseOK(exchange).send(content);
-	}
+    private void getOneTask(HttpExchange exchange) throws IOException {
+        String content = taskToJSON(getIdFromPath(exchange));
+        new ResponseOK(exchange).send(content);
+    }
 
-	private Long getIdFromPath(HttpExchange exchange) {
-		String idString = exchange.getRequestURI().getPath().substring("/tasks/".length());
-		canConvertId(idString);
-		return Long.valueOf(idString);
-	}
+    private Long getIdFromPath(HttpExchange exchange) {
+        String idString = exchange.getRequestURI().getPath().substring("/tasks/".length());
+        canConvertId(idString);
+        return Long.valueOf(idString);
+    }
 
-	private void canConvertId(String idString) {
-		try {
-			Long.valueOf(idString);
-		} catch (Exception e) {
-			throw new IllegalArgumentException();
-		}
-	}
+    private void canConvertId(String idString) {
+        try {
+            Long.valueOf(idString);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
 
-	private Task contentToTask(String content, boolean isPostMethod) throws JsonProcessingException {
-		Task task = objectMapper.readValue(content, Task.class);
-		task.setId(new Long(id));
-		if (isPostMethod) {
-			id += 1;
-		}
-		return task;
-	}
+    private Task contentToTask(String content, boolean isPostMethod) throws JsonProcessingException {
+        Task task = objectMapper.readValue(content, Task.class);
+        task.setId(new Long(id));
+        if (isPostMethod) {
+            id += 1;
+        }
+        return task;
+    }
 
-	private String taskToJSON(Long id) throws IOException {
-		OutputStream outputStream = new ByteArrayOutputStream();
-		Task task = tasks.stream().filter(t -> t.getId().equals(id)).findAny().orElseThrow(IllegalAccessError::new);
-		objectMapper.writeValue(outputStream, task);
-		return outputStream.toString();
-	}
+    private String taskToJSON(Long id) throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        Task task = tasks.stream().filter(t -> t.getId().equals(id)).findAny().orElseThrow(IllegalAccessError::new);
+        objectMapper.writeValue(outputStream, task);
+        return outputStream.toString();
+    }
 
-	private String tasksToJSON() throws IOException {
-		OutputStream outputStream = new ByteArrayOutputStream();
-		objectMapper.writeValue(outputStream, tasks);
-		return outputStream.toString();
-	}
-
-	private boolean isStartWithTasks(HttpExchange exchange) {
-		return exchange.getRequestURI().getPath().startsWith("/tasks/");
-	}
+    private String tasksToJSON() throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+        return outputStream.toString();
+    }
 }
