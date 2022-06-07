a460475a1038ef73a25052f9a37e9772f5f7ac71
akaster99 <idavid29@naver.com>
2022-06-07 23:34:42 +0900

    Define Task

---
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -0,0 +1,22 @@
+package com.codesoom.assignment.models;
+
+public class Task {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private Long id;
+    private String title;
+}
