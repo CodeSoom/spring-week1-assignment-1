b05b84bbf3d6cada39de0ae877dfc6dca4e93eab
Jinu Noh <yes.jinu@gmail.com>
2022-06-07 19:47:22 +0900

    chore: remove unused function & adjust spacing

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -7,15 +7,13 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
         try {
             InetSocketAddress address = new InetSocketAddress("localhost", 8080);
             HttpServer httpServer = HttpServer.create(address, 0);
-            HttpHandler handler = new TaskHttpHandler();
+
+            HttpHandler handler = new TaskController();
+
             httpServer.createContext("/", handler);
             httpServer.start();
         } catch (IOException e) {
