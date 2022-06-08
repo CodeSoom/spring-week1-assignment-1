f54b45e0e89377acba1fd9c8247f942b9fcdb544
akaster99 <idavid29@naver.com>
2022-06-08 10:00:40 +0900

    Define MemoryTaskRepository

---
+++ app/src/main/java/com/codesoom/assignment/repository/MemoryTaskRepository.java
@@ -0,0 +1,39 @@
+package com.codesoom.assignment.repository;
+
+import com.codesoom.assignment.models.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MemoryTaskRepository {
+    private List<Task> tasks = new ArrayList<>();
+    private Long sequence = 0L;
+
+    public Task save(Task task){
+        task.setId(sequence++);
+        tasks.add(task);
+        return task;
+    }
+
+    public Optional<Task> findById(Long taskId){
+        return tasks.stream().filter(task-> task.getId().equals(taskId)).findAny();
+
+    }
+
+    public List<Task> findAll(){
+        return new ArrayList<>(tasks);
+    }
+    public Optional<Task> update(Long taskId, String newTitle){
+        Optional<Task> changeTask = tasks.stream().filter(task-> task.getId().equals(taskId)).findAny();
+        if(!changeTask.isPresent()){
+            return changeTask;
+        }
+        changeTask.get().setTitle(newTitle);
+        return changeTask;
+    }
+    public void delete(Task deleteTask){
+        tasks.remove(deleteTask);
+    }
+
+}
