4a660a330e5e4d7eb5bfb99f34632162b7583a8e
unajun <bjyoon513@gmail.com>
2022-06-10 02:40:09 +0900

    fix: add statment for exception

--- app/src/main/java/com/codesoom/todo/controllers/ToDoController.java
+++ app/src/main/java/com/codesoom/todo/controllers/ToDoController.java
@@ -137,7 +137,7 @@ public class ToDoController {
             responseBody.flush();
             responseBody.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error occurred: Sending response");
         }
 
 
