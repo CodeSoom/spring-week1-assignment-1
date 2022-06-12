d07e845ffcd523fda8220abf60daf6683e29fcc2
Noh Jinu <yes.jinu@gmail.com>
2022-06-12 18:18:43 +0900

    feat: add database seeding code for test convenience

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -3,7 +3,16 @@ package com.codesoom.assignment;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import com.codesoom.assignment.services.TaskService;
+import org.junit.jupiter.api.BeforeAll;
 
 class AppTest {
+    private TaskService taskService = new TaskService();
+
+    @BeforeAll
+    void setup() {
+        this.taskService.register("task 1");
+        this.taskService.register("task 2");
+    }
 
 }
