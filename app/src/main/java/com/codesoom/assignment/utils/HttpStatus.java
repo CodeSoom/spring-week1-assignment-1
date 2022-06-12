7ad000f6f26122c7dd2e69cb538098f3343bd1f8
Noh Jinu <yes.jinu@gmail.com>
2022-06-12 17:43:46 +0900

    style: change one line expression for method

--- app/src/main/java/com/codesoom/assignment/utils/HttpStatus.java
+++ app/src/main/java/com/codesoom/assignment/utils/HttpStatus.java
@@ -16,5 +16,7 @@ public enum HttpStatus {
         this.statusCode = statusCode;
     }
 
-    public Integer statusCode() { return statusCode; }
+    public Integer statusCode() {
+        return statusCode;
+    }
 }
