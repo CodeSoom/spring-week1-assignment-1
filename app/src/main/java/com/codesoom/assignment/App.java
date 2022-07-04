43215da6f3d0ce31966f452a8140fce748eda800
박범진 <qjawlsqjacks@naver.com>
2022-07-04 20:41:18 +0900

    클라이언트와 통신할 HttpServer 생성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,25 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        try {
+            InetSocketAddress address = new InetSocketAddress(8080);
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new DemoHttpHandler();
+            httpServer.createContext("/", handler);
+            httpServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
