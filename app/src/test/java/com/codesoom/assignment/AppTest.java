4f5e16bcc89865a225333b6085881efac7cdd26f
박범진 <qjawlsqjacks@fasoo.com>
2022-07-04 14:09:58 +0900

    Git pull request 세팅용 커밋

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -8,6 +8,6 @@ class AppTest {
     @Test
     void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertNotNull(classUnderTest.getGreeting(), "이러면 Git Setting이 된건가?");
     }
 }
