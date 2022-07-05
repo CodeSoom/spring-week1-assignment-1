a3e50a3504c25f98f7bca07f90cd57c35fb6f524
eunice0035 <eunice7357@gmail.com>
2022-07-05 20:13:56 +0900

    Task 모델 및 HttpHandler 생성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,24 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
     public static void main(String[] args) {
+
+        InetSocketAddress address = new InetSocketAddress("localhost", 8000);
+
+        try {
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new TaskHttpHandler();
+            httpServer.createContext("/", handler);
+            httpServer.start();
+
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 }
