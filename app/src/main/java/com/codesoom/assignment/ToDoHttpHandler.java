d1216c7dea6fe063817b670c771d9aa4a4b9b969
Ernest Hong <hsh3592@gmail.com>
2022-07-05 09:45:41 +0900

    refactor: 예외처리추가

--- app/src/main/java/com/codesoom/assignment/ToDoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/ToDoHttpHandler.java
@@ -10,6 +10,7 @@ import java.io.*;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ToDoHttpHandler implements HttpHandler {
@@ -20,16 +21,21 @@ public class ToDoHttpHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
+        if (method == null) {
+            exchange.sendResponseHeaders(400, 0);
+            return;
+        }
+
         URI uri = exchange.getRequestURI();
         String path = uri.getPath();
-
-        InputStream inputStream = exchange.getRequestBody();
-        String body = new BufferedReader(new InputStreamReader(inputStream))
-                .lines()
-                .collect(Collectors.joining("\n"));
+        if (path == null) {
+            exchange.sendResponseHeaders(400, 0);
+            return;
+        }
 
         System.out.println(method + " " + path);
 
+        String body = getRequestBody(exchange).orElse("");
         if (!body.isBlank()) {
             System.out.println(body);
         }
@@ -38,13 +44,26 @@ public class ToDoHttpHandler implements HttpHandler {
         int responseCode = 404;
 
         if (method.equals("GET") && path.equals("/tasks")) {
-            content = tasksToJSON();
-            responseCode = 200;
-        } else if (method.equals("POST") && path.equals("/tasks")) {
-            Task task = toTask(body);
-            tasks.add(task);
-            content = toString(task);
-            responseCode = 201;
+            try {
+                content = tasksToJSON();
+                responseCode = 200;
+            } catch (IOException e) {
+                content = "Failed to convert tasks to JSON";
+                responseCode = 500;
+            }
+        } else if (method.equals("POST") && path.equals("/tasks") && !body.isBlank()) {
+            try {
+                Task task = toTask(body);
+                tasks.add(task);
+                content = toString(task);
+                responseCode = 201;
+            } catch (JsonProcessingException e) {
+                content = "Failed to convert request body to Task";
+                responseCode = 400;
+            } catch (IOException e) {
+                content = "Failed to convert Task to string";
+                responseCode = 500;
+            }
         }
 
         exchange.sendResponseHeaders(responseCode, content.getBytes().length);
@@ -53,6 +72,14 @@ public class ToDoHttpHandler implements HttpHandler {
         outputStream.write(content.getBytes());
         outputStream.flush();
         outputStream.close();
+    }
+
+    private Optional<String> getRequestBody(HttpExchange exchange) {
+        final InputStream inputStream = exchange.getRequestBody();
+        final String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        return Optional.ofNullable(body);
     }
 
     private Task toTask(String content) throws JsonProcessingException {
