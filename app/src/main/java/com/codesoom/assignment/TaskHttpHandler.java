02841c22570a6b19e954acef3be7be2efc4ee34f
eunice0035 <eunice7357@gmail.com>
2022-07-06 00:10:59 +0900

    Task 찾는 부분 함수화

--- app/src/main/java/com/codesoom/assignment/TaskHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/TaskHttpHandler.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 public class TaskHttpHandler implements HttpHandler {
     private List<Task> taskList = new ArrayList<>();
     private ObjectMapper objectMapper = new ObjectMapper();
-
+    private Long curTaskID = 0L;
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
@@ -38,24 +38,21 @@ public class TaskHttpHandler implements HttpHandler {
         if(method.equals("GET") && path.startsWith("/tasks/")){
             String searchIDString = path.substring("/tasks/".length());
             System.out.println("ID String : " + searchIDString);
-            Long searchID = new Long(-1);
+            Long searchID;
+
             try{
                 searchID = Long.parseLong(searchIDString);
+
+                if(findTaskIdx(searchID) != -1){
+                    returnCode = 200;
+                }
+                else{
+                    returnCode = 404;
+                    content = "Not Valid ID";
+                }
+
             }catch (Exception e){
                 System.out.println("Not Valid ID");
-                returnCode = 404;
-                content = "Not Valid ID";
-            }
-
-            for(int i=0; i<taskList.size(); i++){
-                if(taskList.get(i).equals(searchID)){
-                    returnCode = 200;
-                    content = taskList.get(i).toString();
-                    break;
-                }
-            }
-
-            if(returnCode == -1){
                 returnCode = 404;
                 content = "Not Valid ID";
             }
@@ -64,6 +61,7 @@ public class TaskHttpHandler implements HttpHandler {
         if(method.equals("POST") && path.equals("/tasks")) {
             if(!body.isBlank()){
                 Task task = makeTask(body);
+                task.setId(++curTaskID);
                 taskList.add(task);
 
                 returnCode = 200;
@@ -77,6 +75,17 @@ public class TaskHttpHandler implements HttpHandler {
         outputStream.flush();
         outputStream.close();
     }
+
+    private int findTaskIdx(Long id){
+
+        for(int i=0; i<taskList.size(); i++){
+            if(taskList.get(i).equals(id)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 
     private Task makeTask(String body) throws JsonProcessingException {
         return objectMapper.readValue(body, Task.class);
