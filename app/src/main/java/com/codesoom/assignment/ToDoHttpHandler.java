7f3d22bd9f255aa208acc4b46f1f3c8412ab9606
Ernest Hong <hsh3592@gmail.com>
2022-07-05 21:55:59 +0900

    chore: OutputStream 주석 추가

--- app/src/main/java/com/codesoom/assignment/ToDoHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/ToDoHttpHandler.java
@@ -123,9 +123,9 @@ public class ToDoHttpHandler implements HttpHandler {
             exchange.sendResponseHeaders(responseCode, content.getBytes().length);
 
             final OutputStream outputStream = exchange.getResponseBody();
-            outputStream.write(content.getBytes());
-            outputStream.flush();
-            outputStream.close();
+            outputStream.write(content.getBytes()); // 전달받은 byte array로부터 output stream에 기록하기
+            outputStream.flush(); // 버퍼에 담겨있는 output byte들을 강제로 기록되게 한다. 버퍼에 효율적으로 담아두는 것을 추측해볼 수 있음. (Flushable Interface)
+            outputStream.close(); // 이 stream에 관련된 리소스들을 해제해준다. (Closeable Interface)
         }
     }
 
