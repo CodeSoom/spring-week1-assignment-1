fb4af41963fa9b8097d692d0d3c6357e3fdf2b15
eunice0035 <eunice7357@gmail.com>
2022-07-05 23:31:31 +0900

    error를 발생시키는 코드만 try-catch문 안에 작성

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -13,15 +13,16 @@ public class App {
 
     public static void main(String[] args) {
         InetSocketAddress address = new InetSocketAddress("localhost", 8000);
+        HttpServer httpServer;
 
         try {
-            HttpServer httpServer = HttpServer.create(address, 0);
-            HttpHandler handler = new TaskHttpHandler();
-            httpServer.createContext("/", handler);
-            httpServer.start();
-
-        } catch (IOException e){
+            httpServer = HttpServer.create(address, 0);
+        }catch (IOException e){
             throw new RuntimeException(e);
         }
+
+        HttpHandler handler = new TaskHttpHandler();
+        httpServer.createContext("/", handler);
+        httpServer.start();
     }
 }
