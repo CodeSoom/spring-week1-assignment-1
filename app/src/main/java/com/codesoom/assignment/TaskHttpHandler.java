0ced3404dae499e3a87abe04fc94416a3335d001
eunice0035 <eunice7357@gmail.com>
2022-07-05 23:35:58 +0900

    GET 요청관련 기능 구현

--- app/src/main/java/com/codesoom/assignment/TaskHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/TaskHttpHandler.java
@@ -21,26 +21,61 @@ public class TaskHttpHandler implements HttpHandler {
         String method = exchange.getRequestMethod();
         URI requestURI = exchange.getRequestURI();
         String path = requestURI.getPath();
+
+        int returnCode = -1;
         String content = "[]";
+
         InputStream inputStream = exchange.getRequestBody();
         String body = new BufferedReader(new InputStreamReader(inputStream))
                 .lines()
                 .collect(Collectors.joining("\n"));
 
-
-        if(!body.isBlank()){
-            Task task = makeTask(body);
+        if(method.equals("GET") && path.equals("/tasks")){
+            content = taskToJSON();
         }
 
-        exchange.sendResponseHeaders(200, content.getBytes().length );
+        if(method.equals("GET") && path.startsWith("/tasks/")){
+            String searchIDString = path.substring("/tasks/".length());
+            System.out.println("ID String : " + searchIDString);
+            Long searchID = new Long(-1);
+            try{
+                searchID = Long.parseLong(searchIDString);
+            }catch (Exception e){
+                System.out.println("Not Valid ID");
+                returnCode = 404;
+                content = "Not Valid ID";
+            }
+
+            for(int i=0; i<taskList.size(); i++){
+                if(taskList.get(i).equals(searchID)){
+                    returnCode = 200;
+                    content = taskList.get(i).toString();
+                    break;
+                }
+            }
+
+            if(returnCode == -1){
+                returnCode = 404;
+                content = "Not Valid ID";
+            }
+        }
+        
+        exchange.sendResponseHeaders(returnCode, content.getBytes().length );
         OutputStream outputStream = exchange.getResponseBody();
         outputStream.write(content.getBytes());
         outputStream.flush();
         outputStream.close();
-
     }
 
     private Task makeTask(String body) throws JsonProcessingException {
         return objectMapper.readValue(body, Task.class);
+    }
+
+    //taskList -> JSON으로 변환 후 return
+    private String taskToJSON() throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, taskList);
+
+        return outputStream.toString();
     }
 }
