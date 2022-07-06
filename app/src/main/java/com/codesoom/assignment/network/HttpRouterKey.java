c6ad654709b296b96de76ef0b38035490a779f8d
Ernest Hong <hsh3592@gmail.com>
2022-07-06 22:22:17 +0900

    docs: HttpRouterKey 주석 추가

--- app/src/main/java/com/codesoom/assignment/network/HttpRouterKey.java
+++ app/src/main/java/com/codesoom/assignment/network/HttpRouterKey.java
@@ -2,15 +2,20 @@ package com.codesoom.assignment.network;
 
 import java.util.Objects;
 
+/**
+ * HttpRouter에 HttpMethod와 path 정규식을 등록해두는 용도로 사용
+ * Request가 들어왔을 때 HttpRouterKey와 매칭되는지 여부를 파악해서 핸들러를 실행해 줄 수 있습니다.
+ * hashCode를 지원해서 HashMap의 Key로 사용될 수 있습니다.
+ */
 public class HttpRouterKey {
     private HttpMethod method;
     private String pathRegex;
     private int hashCode;
 
-    public HttpRouterKey(HttpMethod method, String path) {
+    public HttpRouterKey(HttpMethod method, String pathRegex) {
         this.method = method;
-        this.pathRegex = path;
-        this.hashCode = Objects.hash(method, path);
+        this.pathRegex = pathRegex;
+        this.hashCode = Objects.hash(method, pathRegex);
     }
 
     @Override
@@ -38,6 +43,10 @@ public class HttpRouterKey {
         return method;
     }
 
+    /**
+     * @param path request로 들어온 path
+     * @return 등록된 path 정규식과 매칭 여부 반환
+     */
     public boolean matchesPath(String path) {
         if (path == null || this.pathRegex == null) {
             return false;
@@ -45,6 +54,10 @@ public class HttpRouterKey {
         return path.matches(pathRegex);
     }
 
+    /**
+     * @param method request로 들어온 method
+     * @return 등록된 HTTPMethod와 일차 여부 확인
+     */
     public boolean equalsMethod(HttpMethod method) {
         if (method == null || this.method == null) {
             return false;
