1b9eab55dc197bb3483419189757ea0aced27e3d
Ernest Hong <hsh3592@gmail.com>
2022-07-06 21:38:18 +0900

    refactor: public interface 내부에서 public 지정해둔 것 제거
    public interface 내부에서는 access control 지정안해줘도 public으로 암시적으로 지정됨

--- app/src/main/java/com/codesoom/assignment/network/RouterExecutable.java
+++ app/src/main/java/com/codesoom/assignment/network/RouterExecutable.java
@@ -3,5 +3,5 @@ package com.codesoom.assignment.network;
 import java.io.IOException;
 
 public interface RouterExecutable {
-    public void execute(HttpRequest request, HttpResponse response) throws IOException;
+    void execute(HttpRequest request, HttpResponse response) throws IOException;
 }
