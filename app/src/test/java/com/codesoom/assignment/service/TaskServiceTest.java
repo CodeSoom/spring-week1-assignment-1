afbe87a8363a33cadf70397c5dacee41b5b75ef8
박범진 <qjawlsqjacks@fasoo.com>
2022-07-08 08:14:55 +0900

    테스트 코드 설명 구체적으로 변경
    어떤 상황일 때 무엇이 무엇을 리턴하는지 다른 사람이 봤을 때 쉽게 알 수
    있도록 변경했습니다.

--- app/src/test/java/com/codesoom/assignment/service/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/service/TaskServiceTest.java
@@ -11,20 +11,20 @@ class TaskServiceTest {
     final TaskService taskService = new TaskService();
 
     @Test
-    @DisplayName("Task를 생성하고 Task를 반환하는지 테스트한다.")
+    @DisplayName("Task를 생성하는 요청이 들어왔을 때, createTask 메서드가 Task를 생성하고 생성한 테스트를 리턴하는 것을 테스트한다.")
     void createTaskTest() {
         Task taskToEquals = new Task(0L, "BJP");
         assertEquals(taskToEquals, taskService.createTask("BJP"));
     }
 
     @Test
-    @DisplayName("가진 tasks를 반환하는지 테스트한다.")
+    @DisplayName("전체 Tasks를 조회하는 요청이 들어왔을 때, getTasks 메서드가 저장되어 있는 Tasks를 리턴하는 것을 테스트한다.")
     void getTasksTest() {
         assertEquals(0, taskService.getTasks().size());
     }
 
     @Test
-    @DisplayName("특정 id를 가진 객체를 리턴한다.")
+    @DisplayName("Task를 조회하는 요청이 들어왔고 숫자 형식의 id와 같은 Task가 존재할 때, getTask 메서드가 해당 Task를 리턴한다.")
     void getTaskTest() {
         taskService.createTask("BJP");
         assertEquals(new Task(0L, "BJP"), taskService.getTask(0L));
