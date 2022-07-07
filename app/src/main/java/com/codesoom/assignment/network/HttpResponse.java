4290d7906503fe864e6aa10637cf1a24853ba806
Ernest Hong <hsh3592@gmail.com>
2022-07-07 22:52:45 +0900

    refactor: OutputStream finally에서 close 해주도록 수정

--- app/src/main/java/com/codesoom/assignment/network/HttpResponse.java
+++ app/src/main/java/com/codesoom/assignment/network/HttpResponse.java
@@ -37,10 +37,19 @@ public class HttpResponse {
         } else {
             exchange.sendResponseHeaders(responseCode.getRawValue(), content.getBytes().length);
 
-            final OutputStream outputStream = exchange.getResponseBody();
-            outputStream.write(content.getBytes()); // 전달받은 byte array로부터 output stream에 기록하기
-            outputStream.flush(); // 버퍼에 담겨있는 output byte들을 강제로 기록되게 한다. 버퍼에 효율적으로 담아두는 것을 추측해볼 수 있음. (Flushable Interface)
-            outputStream.close(); // 이 stream에 관련된 리소스들을 해제해준다. (Closeable Interface)
+            OutputStream outputStream = null;
+
+            try {
+                outputStream = exchange.getResponseBody();
+                outputStream.write(content.getBytes()); // 전달받은 byte array로부터 output stream에 기록하기
+                outputStream.flush(); // 버퍼에 담겨있는 output byte들을 강제로 기록되게 한다. 버퍼에 효율적으로 담아두는 것을 추측해볼 수 있음. (Flushable Interface)
+            } catch (IOException e) {
+                throw e;
+            } finally {
+                if (outputStream != null) {
+                    outputStream.close(); // 이 stream에 관련된 리소스들을 해제해준다. (Closeable Interface)
+                }
+            }
         }
     }
 }
