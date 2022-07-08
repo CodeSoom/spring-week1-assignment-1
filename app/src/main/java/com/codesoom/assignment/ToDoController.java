ee09052b63c6246ab904f7388866f3682a4e56b8
Ernest Hong <hsh3592@gmail.com>
2022-07-08 22:56:27 +0900

    docs: ToDoController 주석 추가

--- app/src/main/java/com/codesoom/assignment/ToDoController.java
+++ app/src/main/java/com/codesoom/assignment/ToDoController.java
@@ -8,15 +8,27 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * 할일을 추가, 조회, 수정, 삭제 하는 역할을 합니다
+ */
 public class ToDoController {
     private final ToDoRepository repository;
     private final AtomicLong lastId = new AtomicLong(1L);
     private final TaskMapper mapper = new TaskMapper();
 
+    /**
+     * @param repository Task를 보관하는 저장소
+     */
     public ToDoController(ToDoRepository repository) {
         this.repository = repository;
     }
 
+    /**
+     * Id를 기반으로 Task를 조회합니다
+     * @param taskId 조회할 task id
+     * @return 조회된 Task
+     * @throws TaskNotFoundException 저장된 Task가 없을 때 예외가 발생됩니다
+     */
     public Task getTaskById(Long taskId) throws TaskNotFoundException {
         Optional<Task> task = repository.getTaskById(taskId);
         if (task.isEmpty()) {
@@ -25,10 +37,18 @@ public class ToDoController {
         return task.get();
     }
 
+    /**
+     * @return 저장된 Task 목록을 반환합니다
+     */
     public List<Task> getTasks() {
         return repository.getTasks();
     }
 
+    /**
+     * @param body JSON String 형태의 Task
+     * @return 새로 생성된 Task를 반환합니다.
+     * @throws JsonProcessingException body로 입력받은 JSON String을 매핑하지 못할 떄 예외가 발생됩니다
+     */
     public Task addTask(String body) throws JsonProcessingException {
         Task task = mapper.readTask(body);
         task.setId(lastId.getAndIncrement());
@@ -36,12 +56,22 @@ public class ToDoController {
         return task;
     }
 
+    /**
+     * @param taskId 변경할 Task의 Id
+     * @param body 변경할 Task의 내용
+     * @throws TaskNotFoundException 변경할 Task가 없을 때 예외가 발생합니다
+     * @throws JsonProcessingException 변경할 Task의 내용을 매핑하지 못할 때 예외가 발생됩니다
+     */
     public void updateTask(Long taskId, String body) throws TaskNotFoundException, JsonProcessingException {
         Task oldTask = getTaskById(taskId);
         Task newTask = mapper.readTask(body);
         oldTask.update(newTask);
     }
 
+    /**
+     * @param taskId 삭제할 Task의 Id
+     * @throws TaskNotFoundException 삭제할 Task를 찾지 못할 때 예외가 발생됩니다
+     */
     public void deleteTask(Long taskId) throws TaskNotFoundException {
         Optional<Task> task = repository.getTaskById(taskId);
         if (task.isPresent()) {
