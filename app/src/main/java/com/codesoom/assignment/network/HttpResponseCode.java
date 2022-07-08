b56d3c3aee972d0cb4892d16e4b8e9ca660e9643
Ernest Hong <hsh3592@gmail.com>
2022-07-08 20:31:37 +0900

    refactor: HttpResponseCode 주석 추가

--- app/src/main/java/com/codesoom/assignment/network/HttpResponseCode.java
+++ app/src/main/java/com/codesoom/assignment/network/HttpResponseCode.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.network;
 
+/**
+ * Http Response Code를 enum으로 추상화
+ */
 public enum HttpResponseCode {
     OK(200),
     Created(201),
@@ -8,12 +11,21 @@ public enum HttpResponseCode {
     NotFound(404),
     InternalServerError(500);
 
+    /**
+     * http code raw value
+     */
     private final int rawValue;
 
+    /**
+     * @param rawValue http code를 raw value로 전달합니다
+     */
     HttpResponseCode(int rawValue) {
         this.rawValue = rawValue;
     }
 
+    /**
+     * @return http code를 raw value로 전달합니다
+     */
     public int getRawValue() {
         return rawValue;
     }
