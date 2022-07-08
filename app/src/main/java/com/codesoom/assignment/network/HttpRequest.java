4134a40be3ce68d8cee4f19e143da0011a674c67
Ernest Hong <hsh3592@gmail.com>
2022-07-08 20:21:59 +0900

    refactor: HttpRequest 주석 추가

--- app/src/main/java/com/codesoom/assignment/network/HttpRequest.java
+++ app/src/main/java/com/codesoom/assignment/network/HttpRequest.java
@@ -12,32 +12,52 @@ import java.util.stream.Collectors;
  * Http 요청에 관련된 정보
  */
 public class HttpRequest {
-    private HttpMethod method;
-    private String path;
-    private String body;
+    private final HttpMethod method;
+    private final String path;
+    private final String body;
 
+    /**
+     * HttpExchange로 부터 Request 정보를 추출
+     * @param exchange HttpExchange 객체
+     */
     public HttpRequest(HttpExchange exchange) {
         this.method = HttpMethod.convert(exchange.getRequestMethod());
         this.path = exchange.getRequestURI().getPath();
         this.body = getRequestBody(exchange).orElse("");
     }
 
+    /**
+     * @return Http Request 정보가 정상적인지 여부 반환
+     */
     public boolean isNotValid() {
         return method == null || path == null;
     }
 
+    /**
+     * @return HttpMethod enum 반환
+     */
     public HttpMethod getMethod() {
         return method;
     }
 
+    /**
+     * @return request path 반환
+     */
     public String getPath() {
         return path;
     }
 
+    /**
+     * @return request body 내용 반환. 내용 없는 경우 공백 문자가 반환됩니다.
+     */
     public String getBody() {
         return body;
     }
 
+    /**
+     * @param exchange HttpExchange
+     * @return exchange로 부터 request body를 추출해서 반환
+     */
     private Optional<String> getRequestBody(HttpExchange exchange) {
         final InputStream inputStream = exchange.getRequestBody();
         if (inputStream == null) {
