3377aecfeae8dd7622488329659520acbda33b86
정현준 <jeongdalma@gmail.com>
2022-08-01 20:08:04 +0900

    feat: HttpServer를 사용하여 로컬 서버 테스트

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,33 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.handler.TaskHandler;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+
+        InetSocketAddress address = new InetSocketAddress(8000);
+
+        try {
+            HttpServer server = HttpServer.create(address , 0);
+            HttpHandler handler = new TaskHandler();
+            server.createContext("/" , handler);
+            
+
+
+            server.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/handler/TaskHandler.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class TaskHandler implements HttpHandler {
+
+    public final int SUCCESSFUL_OK = 200;
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        System.out.println(String.format("[method] : %s , [path] : %s" , method , path));
+
+        String content = "Hello Codesoom!!!";
+
+        exchange.sendResponseHeaders(SUCCESSFUL_OK , content.getBytes().length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+}
