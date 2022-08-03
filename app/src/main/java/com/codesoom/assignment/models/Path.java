151fffdd3ddf51601014a591a4531fb69cb07211
정현준 <jeongdalma@gmail.com>
2022-08-03 16:37:48 +0900

    feat: Path 클래스 추가

---
+++ app/src/main/java/com/codesoom/assignment/models/Path.java
@@ -0,0 +1,34 @@
+package com.codesoom.assignment.models;
+
+import com.codesoom.assignment.exceptions.ParameterNotFoundException;
+
+public class Path {
+    private final String resource;
+    private final String pathVariable;
+
+    public Path(String path){
+        String[] pathArr = path.split("/");
+        this.resource = pathArr[1];
+        this.pathVariable = pathArr.length >= 3 ? pathArr[2] : null;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public String getPathVariable() throws ParameterNotFoundException {
+        if(pathVariable == null){
+            throw new ParameterNotFoundException("not existing pathVariable");
+        }
+        return pathVariable;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Path{");
+        sb.append("resource='").append(resource).append('\'');
+        sb.append(", pathVariable='").append(pathVariable).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
