45637b54fef9b90b5c6060fc0276eb40681dc2ae
정현준 <jeongdalma@gmail.com>
2022-08-03 17:42:56 +0900

    feat: Path 경로변수 커스텀 예외 추가

---
+++ app/src/main/java/com/codesoom/assignment/exceptions/ParameterNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.exceptions;
+
+public class ParameterNotFoundException extends Exception{
+    public ParameterNotFoundException(String msg){
+        super(msg);
+    }
+}
