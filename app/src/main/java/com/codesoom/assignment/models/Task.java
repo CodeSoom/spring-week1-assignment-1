aa69360cb862daa1b48d7ffe9dea82bc987b82a7
정현준 <jeongdalma@gmail.com>
2022-08-03 17:32:53 +0900

    refactor: setter 제거 , 필드 별 final 키워드 추가

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,23 +1,20 @@
 package com.codesoom.assignment.models;
 
 public class Task {
-    private Long id;
-    private String title;
+    private final Long id;
+    private final String title;
+
+    public Task(Long id , String title){
+        this.id = id;
+        this.title = title;
+    }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     @Override
