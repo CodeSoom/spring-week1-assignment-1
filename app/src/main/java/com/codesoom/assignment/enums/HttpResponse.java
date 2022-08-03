a5966b10a71cb202a770fbe50952366bba01b595
정현준 <jeongdalma@gmail.com>
2022-08-03 17:16:03 +0900

    refactor: 응답 코드 추가

--- app/src/main/java/com/codesoom/assignment/enums/HttpResponse.java
+++ app/src/main/java/com/codesoom/assignment/enums/HttpResponse.java
@@ -2,7 +2,10 @@ package com.codesoom.assignment.enums;
 
 public enum HttpResponse {
     OK(200),
-    CREATED(201);
+    CREATED(201),
+    BAD_REQUEST(400),
+    NO_CONTENT(204),
+    NOT_FOUND(404);
 
     private final int code;
 
