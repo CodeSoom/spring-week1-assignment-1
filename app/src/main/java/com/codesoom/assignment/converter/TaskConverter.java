7747125e2b251247582ee3d45319b4716f9a5b38
정현준 <jeongdalma@gmail.com>
2022-08-04 18:05:31 +0900

    refactor: ObjectMapper 제거
    ObjectMapper 제거 후 JSON → Task를 진행해 보았습니다

--- app/src/main/java/com/codesoom/assignment/converter/TaskConverter.java
+++ app/src/main/java/com/codesoom/assignment/converter/TaskConverter.java
@@ -2,13 +2,16 @@ package com.codesoom.assignment.converter;
 
 import com.codesoom.assignment.models.Task;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskConverter {
 
@@ -20,21 +23,48 @@ public class TaskConverter {
         public static final TaskConverter INSTANCE = new TaskConverter();
     }
 
-    private ObjectMapper objectMapper = new ObjectMapper();
-
     public Task convert(String content) throws JsonProcessingException {
-        return objectMapper.readValue(content , Task.class);
+        Map<String , String> map = jsonToMap(content);
+        Task task = new Task();
+        Arrays.stream(getTaskDeclaredFields()).forEach(field -> {
+            field.setAccessible(true);
+            try {
+                field.set(map.get(field.getName()) , task);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(task);
+        return task;
+//        return objectMapper.readValue(content , Task.class);
     }
 
     public String convert(Task task) throws IOException {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream , task);
-        return outputStream.toString();
+//        OutputStream outputStream = new ByteArrayOutputStream();
+//        objectMapper.writeValue(outputStream , task);
+        return "";
     }
 
     public String convert(Map<Long , Task> tasks) throws IOException {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream , new ArrayList<>(tasks.values()));
-        return outputStream.toString();
+//        OutputStream outputStream = new ByteArrayOutputStream();
+//        objectMapper.writeValue(outputStream , new ArrayList<>(tasks.values()));
+        return "";
+    }
+
+    public Field[] getTaskDeclaredFields(){
+        return Task.class.getDeclaredFields();
+    }
+
+    public Map<String , String> jsonToMap(String content){
+        Map<String , String> map = new HashMap<>();
+        String[] contents = content.split(",");
+        Pattern pattern = Pattern.compile("\"(.*?)\"");
+        for(String test : contents){
+            Matcher matcher = pattern.matcher(test);
+            String key = matcher.find() ? matcher.group().replace("\"" , "") : "";
+            String value = matcher.find() ? matcher.group().replace("\"" , "") : "";
+            map.put(key , value);
+        }
+        return map;
     }
 }
