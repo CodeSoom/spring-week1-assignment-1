26b06731fe69d6809cd674eeba111ef5510aa0a6
정현준 <jeongdalma@gmail.com>
2022-08-05 11:19:56 +0900

    docs: HttpMethod JavaDoc 작성

--- app/src/main/java/com/codesoom/assignment/enums/HttpMethod.java
+++ app/src/main/java/com/codesoom/assignment/enums/HttpMethod.java
@@ -1,12 +1,17 @@
 package com.codesoom.assignment.enums;
 
+/**
+ * HTTP Method를 관리한다 <a href="https://datatracker.ietf.org/doc/html/rfc7231#section-4.3">RFC7231</a>
+ *
+ * @see HttpMethod#equals(String) HTTP Method 비교 함수
+ */
 public enum HttpMethod {
     GET,
     POST,
     PUT,
     PATCH,
     DELETED;
-
+    
     public boolean equals(String method){
         return this.name().equals(method);
     }
