548b1cebebc311b0ee438d83903f32f20c79f439
정현준 <jeongdalma@gmail.com>
2022-08-07 00:39:12 +0900

    docs: @DisplayName 표시 내용 수정

--- app/src/test/java/com/codesoom/assignment/converter/TaskConverterJsonToMapTest.java
+++ app/src/test/java/com/codesoom/assignment/converter/TaskConverterJsonToMapTest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class TaskConverterJsonToMapTest {
 
     @Test
-    @DisplayName(" JSON형식에 맞지 않아도 [\"]로 감싸준다면 가져온다")
+    @DisplayName(" JSON형식에 맞지 않아도 [\"]로 감싸준다면 Map에 키 또는 값으로 저장된다")
     void notJson1(){
         // given
         String json = "[\"title1\"=\"테스트1\" ,\"title2\"=\"테스트2\"]";
@@ -24,7 +24,7 @@ public class TaskConverterJsonToMapTest {
     }
 
     @Test
-    @DisplayName(" [,]로 각 객체가 구분되어 있지 않다면 첫 번째 키와 값만 가져온다")
+    @DisplayName(" [,]로 키:값 Entry가 구분되어 있지 않다면 첫 번째 키와 값만 가져온다")
     void notJson2(){
         // given
         String json = "{\"title1\":\"테스트1\" \"title2\":\"테스트2\"}";
@@ -38,7 +38,7 @@ public class TaskConverterJsonToMapTest {
     }
 
     @Test
-    @DisplayName("JSON문자열에 키가 중복되지 않는다면 잘 가져온다")
+    @DisplayName("JSON문자열에 키가 중복되지 않는다면 Map에 각각 저장된다")
     void jsonhasMultiKey(){
         // given
         String json = "{\"title1\": \"테스트1\",\"title2\": \"테스트2\"}";
