3721ca43b85ace8053c066de36f9747bb8d156eb
kckoh <kyungcheolkoh@gmail.com>
2022-09-13 16:50:17 -0700

    Implement GET /tasks
    코드 리뷰 부탁드립니다. npm run test 실행 했을때 한글이 인식이 안되는데 어떻게 하면 될까요? 다른 부분들도 리뷰 해주시면 감사하겠습니다.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,25 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.handler.DemoHttpHandler;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress address = new InetSocketAddress(8000);
+
+        HttpServer httpServer = HttpServer.create(address, 0);
+
+        HttpHandler httpHandler = new DemoHttpHandler();
+
+        httpServer.createContext("/", httpHandler);
+        httpServer.start();
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/handler/DemoHttpHandler.java
@@ -0,0 +1,114 @@
+package com.codesoom.assignment.handler;
+
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DemoHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private int status = 200;
+    String content = "Hello World!";
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader((inputStream))).lines().collect(Collectors.joining("\n"));
+
+        System.out.println(method + " " + path);
+
+        if (!body.isEmpty()) {
+
+            Task task1 = toTask(body);
+            tasks.add(task1);
+        }
+
+        getMappingTasks(method, path);
+
+
+        if (method.equals("POST") && path.equals("/tasks")) {
+            content = "Create a new task";
+        }
+
+        exchange.sendResponseHeaders(status, content.getBytes().length);
+
+        OutputStream responseBody = exchange.getResponseBody();
+        responseBody.write(content.getBytes());
+        responseBody.flush();
+        responseBody.close();
+    }
+
+    private void getMappingTasks(String method, String path) throws IOException {
+
+//        GET /tasks
+        if (method.equals("GET") && path.equals("/tasks")) {
+            content = tasksToJson();
+            return;
+        }
+//        GET /tasks/{id}
+        else if (method.equals("GET") && path.contains("/tasks/")) {
+            String[] split = path.split("/");
+
+            String id = split[2];
+            if (!isNumeric(id)) {
+                System.out.println("tasks id not a number");
+//                status = 404;
+                return;
+            }
+
+            for (Task task : tasks) {
+                if (task.getId() == Long.parseLong(id)) {
+                    content = taskToJson(task);
+                    status = 200;
+                    return;
+                }
+            }
+
+            content = "";
+        }
+
+
+    }
+
+    private Task toTask(String body) throws JsonProcessingException {
+        return objectMapper.readValue(body, Task.class);
+    }
+
+    private String tasksToJson() throws IOException {
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+        return outputStream.toString();
+    }
+
+    private String taskToJson(Task task) throws IOException {
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, task);
+        return outputStream.toString();
+    }
+
+
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
