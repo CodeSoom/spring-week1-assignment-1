b4e6551d1456967b2270adfa2901bcbb500bf9c3
Eunha Jeong <eunhaay@gmail.com>
2022-09-14 16:01:43 +0900

    HttpServer 실행 및 Task 모델 추가

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,25 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.handler.TaskHttpHandler;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final int SERVER_PORT = 8000;
 
     public static void main(String[] args) {
+        InetSocketAddress address = new InetSocketAddress(SERVER_PORT);
+        try {
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new TaskHttpHandler();
+            httpServer.createContext("/", handler);
+
+            httpServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
