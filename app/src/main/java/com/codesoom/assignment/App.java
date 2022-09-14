54a293e06bc5a91117280204c0277eaff5284624
hautblis <hautblis@gmail.com>
2022-09-14 20:43:50 +0900

    feat: 웹서버 구현

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,20 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
+import java.net.InetSocketAddress;
+
+public class App {
     public static void main(String[] args) {
+        try {
+            HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+            HttpHandler handler = new TaskHandler();
+            httpServer.createContext("/tasks", handler);
+
+            httpServer.start();
+        } catch (Throwable e) {
+            e.printStackTrace(System.out);
+        }
     }
 }
