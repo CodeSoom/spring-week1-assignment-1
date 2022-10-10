2d8e0bec566284a6acf770055833b71000f4d417
giibeom <kpmyung@gmail.com>
2022-10-10 19:07:36 +0900

    feat: Add ToDo 상세 조회 API

--- app/src/main/java/com/codesoom/assignment/AppHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/AppHttpHandler.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AppHttpHandler implements HttpHandler {
     List<Task> tasks = new ArrayList<>();
@@ -18,18 +19,35 @@ public class AppHttpHandler implements HttpHandler {
     public AppHttpHandler() {
         // Test용
         tasks.add(new Task(1L, "과제 제출하기1"));
-        tasks.add(new Task(2L, "과제 제출하기2"));
+        tasks.add(new Task(222L, "과제 제출하기2"));
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
+        String content = "No Content";
         String method = exchange.getRequestMethod();
         String path = exchange.getRequestURI().getPath();
-        String content = "No Content";
+        Long id;
 
-        if (method.equals("GET") && path.equals("/tasks")) {
-            content = tasksToJson();
+        if (path.length() > 6) {
+            id = Long.parseLong(path.substring(path.indexOf("/", 1) + 1));
+        } else {
+            id = 0L;
+        }
+
+        if (method.equals("GET") && path.contains("/tasks")) {
+            if (hasUserId(id)) {
+                content = tasksToJson(tasks);
+            } else {
+                Optional<Task> task = tasks.stream()
+                        .filter(s -> s.getId().equals(id))
+                        .findFirst();
+
+                if (task.isPresent()) {
+                    content = taskToJson(task);
+                }
+            }
         }
 
         exchange.sendResponseHeaders(200, content.getBytes().length);
@@ -40,9 +58,19 @@ public class AppHttpHandler implements HttpHandler {
         responseBody.close();
     }
 
-    private String tasksToJson() throws IOException {
+    private boolean hasUserId(Long id) {
+        return id == 0;
+    }
+
+    private String tasksToJson(List<Task> tasks) throws IOException {
         OutputStream outputStream = new ByteArrayOutputStream();
         objectMapper.writeValue(outputStream, tasks);
+        return outputStream.toString();
+    }
+
+    private String taskToJson(Optional<Task> task) throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, task.get());
         return outputStream.toString();
     }
 }
