b60c75aa60ee04e6eb68955e16258c0e2df8df4d
Jaemin Kim <jaeminkim90@gmail.com>
2022-10-11 20:50:55 +0900

    Http method에 따라 필요한 작업을 처리할 수 있도록 응답 처리할 곳을 분리한다

--- app/src/main/java/com/codesoom/assignment/ResponseHandler.java
+++ app/src/main/java/com/codesoom/assignment/ResponseHandler.java
@@ -2,56 +2,51 @@ package com.codesoom.assignment;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 
-import com.sun.net.httpserver.Headers;
+import com.codesoom.assignment.methodController.DeleteController;
+import com.codesoom.assignment.methodController.GetController;
+import com.codesoom.assignment.methodController.PatchController;
+import com.codesoom.assignment.methodController.PostController;
+import com.codesoom.assignment.methodController.PutController;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 /**
- * RootHandler: http 요청에 대한 응답 내용을 만든다
+ * RootHandler: http 요청 메서드에 따라 처리를 분류한다
  */
-public class ResponseHandler implements HttpHandler  {
+public class ResponseHandler implements HttpHandler {
+
+    GetController getController = new GetController();
+    PostController postController = new PostController();
+    PatchController patchController = new PatchController();
+    PutController putController = new PutController();
+    DeleteController deleteController = new DeleteController();
 
     @Override
+
     public void handle(HttpExchange exchange) throws IOException {
 
         // Initialize Response Body
         OutputStream responseBody = exchange.getResponseBody();
 
-        try {
-            // Write Response Body
-            StringBuilder jsonResponse = new StringBuilder();
-            jsonResponse.append("response data 한글"+ "\n");
-            jsonResponse.append("method= " + exchange.getRequestMethod()+ "\n");
+        // http method에 따라 controller를 분기한다
+        switch (exchange.getRequestMethod()) {
+            case "get":
+                getController.handler(exchange, responseBody);
+                break;
+            case "post":
+                postController.handler(exchange, responseBody);
+                break;
+            case "put":
+                putController.handler(exchange, responseBody);
+                break;
+            case "patch":
+                patchController.handler(exchange, responseBody);
+                break;
+            case "delete":
+                deleteController.handler(exchange, responseBody);
+                break;
 
-            // Encoding to UTF-8
-            ByteBuffer byteData = Charset.forName("UTF-8").encode(jsonResponse.toString());
-            int contentLength = byteData.limit();
-            byte[] content = new byte[contentLength];
-            byteData.get(content, 0, contentLength);
-
-            // Set Response Headers
-            Headers headers = exchange.getResponseHeaders();
-            headers.add("Content-Type", "text/html;charset=UTF-8");
-            headers.add("Content-Length", String.valueOf(contentLength));
-
-            // Send Response Headers
-            exchange.sendResponseHeaders(200, contentLength);
-
-            responseBody.write(content);
-
-            responseBody.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-
-            if (responseBody != null) {
-                responseBody.close();
-            }
-        } finally {
-            exchange.close();
         }
     }
 }
