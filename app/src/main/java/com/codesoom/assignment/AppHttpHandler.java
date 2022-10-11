9bc1bce3c3c10de9c9c1adc13978343e61deed1d
giibeom <kpmyung@gmail.com>
2022-10-11 16:54:34 +0900

    refactor: jackson 관련 리펙토링

--- app/src/main/java/com/codesoom/assignment/AppHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/AppHttpHandler.java
@@ -1,12 +1,15 @@
 package com.codesoom.assignment;
 
 import com.codesoom.assignment.models.Task;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.codesoom.assignment.utils.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +18,6 @@ import java.util.stream.Collectors;
 public class AppHttpHandler implements HttpHandler {
     static Long id = 0L;
     List<Task> tasks = new ArrayList<>();
-    ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -61,30 +63,30 @@ public class AppHttpHandler implements HttpHandler {
         switch (method) {
             case "GET":
                 if (isEmptyUserId(userId)) {
-                    content = tasksToJson(tasks);
+                    content = JsonUtil.writeValue(tasks);
                 } else {
                     Optional<Task> task = tasks.stream()
                             .filter(t -> t.getId().equals(userId))
                             .findFirst();
 
                     if (task.isPresent()) {
-                        content = taskToJson(task.get());
+                        content = JsonUtil.writeValue(task.get());
                     }
                 }
 
                 exchange.sendResponseHeaders(200, content.getBytes().length);
                 break;
             case "POST":
-                Task task = toTask(requestBody);
+                Task task = JsonUtil.readValue(requestBody, Task.class);
                 task.setId(++id);
                 tasks.add(task);
 
-                content = taskToJson(task);
+                content = JsonUtil.writeValue(task);
                 exchange.sendResponseHeaders(201, content.getBytes().length);
                 break;
             case "PUT":
             case "PATCH":
-                String newTitle = toTask(requestBody).getTitle();
+                String newTitle = JsonUtil.readValue(requestBody, Task.class).getTitle();
 
                 Optional<Task> findTask = tasks.stream()
                         .filter(t -> t.getId().equals(userId))
@@ -94,7 +96,7 @@ public class AppHttpHandler implements HttpHandler {
                     int indexOfOriginTask = tasks.indexOf(findTask.get());
                     Task originTask = tasks.get(indexOfOriginTask);
                     originTask.setTitle(newTitle);
-                    content = taskToJson(originTask);
+                    content = JsonUtil.writeValue(originTask);
                 }
 
                 exchange.sendResponseHeaders(200, content.getBytes().length);
@@ -125,23 +127,7 @@ public class AppHttpHandler implements HttpHandler {
         responseBody.close();
     }
 
-    private Task toTask(String requestBody) throws JsonProcessingException {
-        return objectMapper.readValue(requestBody, Task.class);
-    }
-
     private boolean isEmptyUserId(Long id) {
         return id.equals(0L);
-    }
-
-    private String tasksToJson(List<Task> tasks) throws IOException {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream, tasks);
-        return outputStream.toString();
-    }
-
-    private String taskToJson(Task task) throws IOException {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        objectMapper.writeValue(outputStream, task);
-        return outputStream.toString();
     }
 }
