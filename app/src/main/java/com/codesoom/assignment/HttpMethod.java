ef3b3f22f1d4c55590662c83680264873e89f60c
steve7867 <parkjunsu3@gmail.com>
2022-10-12 14:51:25 +0900

    Refactor: Add line breaking for readability

--- app/src/main/java/com/codesoom/assignment/HttpMethod.java
+++ app/src/main/java/com/codesoom/assignment/HttpMethod.java
@@ -4,7 +4,11 @@ package com.codesoom.assignment;
  * @see https://www.rfc-editor.org/rfc/rfc7231
  */
 public enum HttpMethod {
-    GET("GET"), POST("POST"), PUT("PUT"), PATCH("PATCH"), DELETE("DELETE");
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    PATCH("PATCH"),
+    DELETE("DELETE");
 
     final String name;
 
