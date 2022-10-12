7e4bd9da3499d95e5a0b24fe3188e142d0e6be0f
steve7867 <parkjunsu3@gmail.com>
2022-10-12 15:12:24 +0900

    Feat: Add TaskRepository class

---
+++ app/src/main/java/com/codesoom/assignment/TaskRepository.java
@@ -0,0 +1,38 @@
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.models.Task;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TaskRepository {
+
+    private static final TaskRepository instance = new TaskRepository();
+    private static final Map<Long, Task> taskMap = new ConcurrentHashMap<>();
+
+    private TaskRepository() {
+
+    }
+
+    public static TaskRepository getInstance() {
+        return instance;
+    }
+
+    public Task findById(Long id) {
+        return taskMap.get(id);
+    }
+
+    public void addNewTask(Task task) {
+        task.allocateId();
+        taskMap.put(task.getId(), task);
+    }
+
+    public Task deleteById(Long id) {
+        return taskMap.remove(id);
+    }
+
+    public Collection<Task> getAllTasks() {
+        return taskMap.values();
+    }
+}
