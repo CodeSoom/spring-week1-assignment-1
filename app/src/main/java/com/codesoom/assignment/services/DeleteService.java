f9e04b74fe158b50913ad90a9ea4d8fcf8b067f2
steve7867 <parkjunsu3@gmail.com>
2022-10-12 18:42:14 +0900

    Feat: Create DeleteService class

---
+++ app/src/main/java/com/codesoom/assignment/services/DeleteService.java
@@ -0,0 +1,35 @@
+package com.codesoom.assignment.services;
+
+import com.codesoom.assignment.HttpStatusCode;
+import com.codesoom.assignment.TaskRepository;
+import com.codesoom.assignment.models.Task;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+public class DeleteService {
+
+    private static final DeleteService instance = new DeleteService();
+    private static final TaskRepository taskRepository = TaskRepository.getInstance();
+
+    private DeleteService() {
+    }
+
+    public static DeleteService getInstance() {
+        return instance;
+    }
+
+    public String service(Long id, HttpExchange exchange) throws IOException {
+        final String content = "";
+
+        final Task removedTask = taskRepository.deleteById(id);
+
+        if (removedTask == null) {
+            exchange.sendResponseHeaders(HttpStatusCode.NOT_FOUND.code, content.getBytes().length);
+            return content;
+        }
+
+        exchange.sendResponseHeaders(HttpStatusCode.NO_CONTENT.code, -1);
+        return content;
+    }
+}
