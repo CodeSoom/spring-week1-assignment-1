0dc6cfb8af88f13fe1d9ca3935e93471e6ca8d95
steve7867 <parkjunsu3@gmail.com>
2022-10-12 20:32:07 +0900

    Fix: Change IdGenerator class singleton

--- app/src/main/java/com/codesoom/assignment/utils/IdGenerator.java
+++ app/src/main/java/com/codesoom/assignment/utils/IdGenerator.java
@@ -4,7 +4,15 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class IdGenerator {
 
+    private static final IdGenerator instance = new IdGenerator();
     private static final AtomicLong atomicLong = new AtomicLong(1L);
+
+    private IdGenerator() {
+    }
+
+    public static IdGenerator getInstance() {
+        return instance;
+    }
 
     public Long allocateId() {
         return atomicLong.getAndIncrement();
