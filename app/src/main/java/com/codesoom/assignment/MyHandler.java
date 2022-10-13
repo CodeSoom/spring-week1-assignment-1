01a5260e8ef2f4de17cc5055c1e44f12fd409d90
steve7867 <parkjunsu3@gmail.com>
2022-10-13 19:47:22 +0900

    MyHandler.java 리팩토링
    Http request 정보를 추출하는 책임을 HttpRequest에 위임.

--- app/src/main/java/com/codesoom/assignment/MyHandler.java
+++ app/src/main/java/com/codesoom/assignment/MyHandler.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.exceptions.IllegalHttpRequestException;
+import com.codesoom.assignment.models.HttpRequest;
 import com.codesoom.assignment.models.HttpResponse;
 import com.codesoom.assignment.services.DeleteService;
 import com.codesoom.assignment.services.EditService;
@@ -11,46 +13,48 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyHandler implements HttpHandler {
 
+    private final Map<HttpMethod, HttpRequestService> serviceMap = new ConcurrentHashMap<>();
+
+    public MyHandler() {
+        serviceMap.put(HttpMethod.GET, GetService.getInstance());
+        serviceMap.put(HttpMethod.POST, PostService.getInstance());
+        serviceMap.put(HttpMethod.PUT, EditService.getInstance());
+        serviceMap.put(HttpMethod.PATCH, EditService.getInstance());
+        serviceMap.put(HttpMethod.DELETE, DeleteService.getInstance());
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        final HttpMethod httpMethod = HttpMethod.valueOf(exchange.getRequestMethod());
-        final Path path = new Path(exchange.getRequestURI());
         final OutputStream outputStream = exchange.getResponseBody();
 
-        if (!path.isValid()) {
-            exchange.sendResponseHeaders(HttpStatusCode.BAD_REQUEST.code, 0);
-            writeAndFlushAndClose("", outputStream);
+        HttpRequest httpRequest;
+        try {
+            httpRequest = new HttpRequest(exchange);
+        } catch (IllegalHttpRequestException e) {
+            String message = e.getMessage();
+            exchange.sendResponseHeaders(HttpStatusCode.BAD_REQUEST.code, message.getBytes().length);
+            writeAndFlushAndClose(outputStream, message);
             return;
         }
 
-        final HttpRequestService requestService = resolveHttpRequestService(httpMethod);
-        final HttpResponse httpResponse = requestService.serviceRequest(path.getId(), exchange);
+        final HttpRequestService requestService = serviceMap.get(httpRequest.getHttpMethod());
+        final Long id = httpRequest.getPath().getId();
+        final String requestBody = httpRequest.getRequestBody();
+        final HttpResponse httpResponse = requestService.serviceRequest(id, requestBody);
+
         exchange.sendResponseHeaders(httpResponse.getHttpStatusCode().code, httpResponse.getContent().getBytes().length);
-        writeAndFlushAndClose(httpResponse.getContent(), outputStream);
+        writeAndFlushAndClose(outputStream, httpResponse.getContent());
     }
 
-    private void writeAndFlushAndClose(String content, OutputStream outputStream) throws IOException {
+    private void writeAndFlushAndClose(OutputStream outputStream, String content) throws IOException {
         outputStream.write(content.getBytes());
         outputStream.flush();
         outputStream.close();
     }
 
-    private HttpRequestService resolveHttpRequestService(HttpMethod httpMethod) {
-        switch (httpMethod) {
-            case GET:
-                return GetService.getInstance();
-            case POST:
-                return PostService.getInstance();
-            case PUT:
-            case PATCH:
-                return EditService.getInstance();
-            case DELETE:
-                return DeleteService.getInstance();
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
 }
