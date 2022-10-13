1d4e7f638a1fa9e363e8b96e70457ed77df3030d
steve7867 <parkjunsu3@gmail.com>
2022-10-13 18:22:19 +0900

    HttpRequest 관련 Exception 추가

---
+++ app/src/main/java/com/codesoom/assignment/exceptions/IllegalHttpRequestMethodException.java
@@ -0,0 +1,7 @@
+package com.codesoom.assignment.exceptions;
+
+public class IllegalHttpRequestMethodException extends IllegalHttpRequestException {
+    public IllegalHttpRequestMethodException(String message) {
+        super(message);
+    }
+}
