b4933ba570e4f1f352b86950b60fcc8a8d743a8f
steve7867 <parkjunsu3@gmail.com>
2022-10-14 20:39:18 +0900

    HttpRequestValidator 기존 메소드 변경 및 기능 추가
    기존 메소드: 파라미터의 valid 여부를 리턴하는 방식에서 Exception을 던지는 방식으로 변경.
    기능 추가: Http Request Method에 맞는 id와 request body가 존재하는지 검증.

--- app/src/main/java/com/codesoom/assignment/utils/HttpRequestValidator.java
+++ app/src/main/java/com/codesoom/assignment/utils/HttpRequestValidator.java
@@ -1,41 +1,76 @@
 package com.codesoom.assignment.utils;
 
+import com.codesoom.assignment.HttpMethod;
+import com.codesoom.assignment.exceptions.IllegalHttpRequestBodyException;
+import com.codesoom.assignment.exceptions.IllegalHttpRequestException;
+import com.codesoom.assignment.exceptions.IllegalHttpRequestMethodException;
+import com.codesoom.assignment.exceptions.IllegalHttpRequestPathException;
+import com.codesoom.assignment.models.HttpRequest;
+
 public final class HttpRequestValidator {
 
-    public static boolean isPathValid(String path) {
+    private static final String INVALID_PATH_MESSAGE = "잘못된 경로입니다.\n.../tasks/{Number} 형식으로 입력해주세요.";
+    private static final String INVALID_METHOD_NAME_MESSAGE = "옳지 않은 Http Method 입니다.\nGET, POST, PUT, PATCH, DELETE 중에서 선택해 주세요.";
+    private static final String MISSING_ID_MESSAGE = "Resource Id가 누락되었습니다.\n.../tasks/{Number} 형식으로 입력해주세요.";
+    private static final String MISSING_REQUEST_BODY_MESSAGE = "Request Body가 누락되었습니다.\nex) {\"title\":\"Lorem Ipsum\"}";
+
+    public static void checksPathValid(String path) throws IllegalHttpRequestPathException {
         if (path == null || !path.contains("/")) {
-            return false;
+            throw new IllegalHttpRequestPathException(INVALID_PATH_MESSAGE);
         }
 
         final String[] pathArr = path.split("/");
         if (!(pathArr.length == 2 || pathArr.length == 3)) {
-            return false;
+            throw new IllegalHttpRequestPathException(INVALID_PATH_MESSAGE);
         }
 
         final String resourceName = pathArr[1];
         if (!resourceName.equals("tasks")) {
-            return false;
+            throw new IllegalHttpRequestPathException(INVALID_PATH_MESSAGE);
         }
 
         if (pathArr.length == 2) {
-            return true;
+            throw new IllegalHttpRequestPathException(INVALID_PATH_MESSAGE);
         }
 
         final String idPart = pathArr[2];
-        return StringValidator.isNumber(idPart);
+        if (!StringValidator.isNumber(idPart)) {
+            throw new IllegalHttpRequestPathException(INVALID_PATH_MESSAGE);
+        }
     }
 
-    public static boolean isMethodNameValid(String methodName) {
+    public static void checksMethodNameValid(String methodName) throws IllegalHttpRequestMethodException {
         switch (methodName) {
             case "GET":
             case "POST":
             case "PUT":
             case "PATCH":
             case "DELETE":
-                return true;
+                return;
             default:
-                return false;
+                throw new IllegalHttpRequestMethodException(INVALID_METHOD_NAME_MESSAGE);
         }
     }
 
+    public static void checksMissingPartExists(HttpRequest httpRequest) throws IllegalHttpRequestException {
+        final HttpMethod method = httpRequest.getHttpMethod();
+        final Long id = httpRequest.getPath().getId();
+        final String requestBody = httpRequest.getRequestBody();
+
+        checksIdMissed(method, id);
+        checksRequestBodyMissed(method, requestBody);
+    }
+
+    private static void checksRequestBodyMissed(HttpMethod method, String requestBody) throws IllegalHttpRequestBodyException {
+        if ((HttpMethod.POST.equals(method) || HttpMethod.PUT.equals(method) || HttpMethod.PATCH.equals(method))
+                && requestBody.isEmpty()) {
+            throw new IllegalHttpRequestBodyException(MISSING_REQUEST_BODY_MESSAGE);
+        }
+    }
+
+    private static void checksIdMissed(HttpMethod method, Long id) throws IllegalHttpRequestPathException {
+        if ((HttpMethod.PUT.equals(method) || HttpMethod.DELETE.equals(method)) && id == null) {
+            throw new IllegalHttpRequestPathException(MISSING_ID_MESSAGE);
+        }
+    }
 }
