d17cd8ab3a6dcb19798f7e469068eb017206359c
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-10 01:15:13 +0900

    조건에 맞게 필터링된 Task를 가져오는 테스트를 만들어라
    Stream filter()를 활용하여 조건에 맞는 필터링을 하였는지 테스트함.

---
+++ app/src/test/java/com/codesoom/assignment/handler/TaskHttpHandlerTest.java
@@ -0,0 +1,43 @@
+package com.codesoom.assignment.handler;
+
+import com.codesoom.assignment.model.Task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+class TaskHttpHandlerTest {
+
+    static final List<Task> tasks = new ArrayList<>();
+
+    @BeforeEach
+    void before() {
+        Task task = new Task();
+        task.setTitle("task1");
+
+        Task task2 = new Task();
+        task2.setTitle("task2");
+
+        Task task3 = new Task();
+        task3.setTitle("task3");
+
+        tasks.add(task);
+        tasks.add(task2);
+        tasks.add(task3);
+    }
+    
+    @Test
+    void getFilteredTask() {
+        String taskId = "2";
+        int id = Integer.parseInt(taskId);
+
+        Optional<Task> task = tasks.stream()
+                .filter(t -> t.getId() == id)
+                .findFirst();
+
+        Assertions.assertEquals(task.get().getTitle(), "task2");
+    }
+}
