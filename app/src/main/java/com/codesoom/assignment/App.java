06ae57b3f5725e465c2584c23269645e13cacd37
GyeongHun Woo <gyeonghunwoo@GyeongHuns-MacBook-Air.local>
2023-01-13 20:39:41 +0900

    Make get list
    Rest api 중 get을 통한 목록 얻기 기능 만들기.

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,33 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/*
+* 클라이언트의 요청을 받아서 처리할
+* HttpServer 생성
+* */
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+
+        System.out.println(new App().getGreeting());
+
+        try {
+            InetSocketAddress address = new InetSocketAddress(8000);
+
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new DemoHttpHandler();
+            httpServer.createContext("/", handler);
+            httpServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHandler.java
@@ -0,0 +1,71 @@
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+ * Rest Api를 만들어 보자.
+ * 1. 목록 얻기 - GET /tasks
+ * 2. 상세 조회하기 - GET /tasks/{id}
+ * 3. 생성 하기 - POST /tasks
+ * 4. 제목 수정하기 - PUT/PATCH /tasks/{id}
+ * 5. 삭제하기하기 - DELETE /tasks/{id}
+ * */
+public class DemoHttpHandler implements HttpHandler {
+    private List<Task> tasks = new ArrayList<>();
+    ObjectMapper objectMapper = new ObjectMapper();
+    public DemoHttpHandler(){
+
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        URI uri = exchange.getRequestURI();
+        String path = uri.getPath();
+
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new BufferedReader(new InputStreamReader(inputStream))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        if (!body.isBlank()){
+            Task task = toTask(body);
+            tasks.add(task);
+        }
+
+        String content = "Hello, world!";
+
+        if (method.equals("GET") && path.equals("/tasks")){
+            System.out.println("Get list.");
+            content = tasksToJSON();
+        }
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(content.getBytes());
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private Task toTask(String content) throws JsonProcessingException {
+        return objectMapper.readValue(content, Task.class);
+    }
+
+    private String tasksToJSON() throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, tasks);
+        
+        return outputStream.toString();
+    }
+}
