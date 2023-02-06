72106643a459a2229159e627b55632219b30b5fc
Yunjaejo <yunjae.dev@gmail.com>
2023-02-06 20:22:17 +0900

    feat: http 서버 오픈

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,28 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
+    private final static int PORT = 8000;
+
     public static void main(String[] args) {
+        InetSocketAddress address = new InetSocketAddress(PORT);
+        try {
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new DemoHttpHandler();
+            httpServer.createContext("/", handler);
+            httpServer.start();
+            System.out.println("localhost:" + PORT + "/");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
