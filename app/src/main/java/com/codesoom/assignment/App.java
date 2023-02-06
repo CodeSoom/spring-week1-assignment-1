e54e2c4e10ee759099ac4548d058b68112b47b9c
KMGeon <pos04167@kakao.com>
2023-02-06 16:17:40 +0900

    get,post commit

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,28 @@
 package com.codesoom.assignment;
 
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+
+        InetSocketAddress address = new InetSocketAddress(8899);
+        try {
+            HttpServer httpServer = HttpServer.create(address, 0);
+            HttpHandler handler = new DemoHttpHadnler();
+            httpServer.createContext("/" , handler);
+            httpServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
