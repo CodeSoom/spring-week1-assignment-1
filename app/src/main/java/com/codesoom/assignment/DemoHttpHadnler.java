362ae4ea88d862c7677b8eab65e7f8a080b46657
KMGeon <pos04167@kakao.com>
2023-02-06 19:58:13 +0900

    id값 증가 , detail

--- app/src/main/java/com/codesoom/assignment/DemoHttpHadnler.java
+++ app/src/main/java/com/codesoom/assignment/DemoHttpHadnler.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment;
 import com.codesoom.assignment.models.Task;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -11,12 +10,9 @@ import java.io.*;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class DemoHttpHadnler implements HttpHandler {
-    private Long testId;
     private ObjectMapper objectMapper = new ObjectMapper();
     private List<Task> tasks = new ArrayList<>();
     private Long default_id = 0L;
@@ -27,9 +23,7 @@ public class DemoHttpHadnler implements HttpHandler {
         URI uri = exchange.getRequestURI();
         String path = uri.getPath();
         String content = "";
-        String body = new BufferedReader(new InputStreamReader(exchange.getRequestBody()))
-                .lines()
-                .collect(Collectors.joining("\n"));
+        String body = getBody(exchange);
 
         System.out.println("method::" + requestMethod + "\npath:: " + path);
         //단일 검색
@@ -38,13 +32,27 @@ public class DemoHttpHadnler implements HttpHandler {
         }
 
         //디테일 검색
-        if (requestMethod.equals("GET") && path.equals("/tasks/")) {
+        if (requestMethod.equals("GET") && path.startsWith("/tasks/")) {
+            Task task = new Task();
             Long getId = gerRequestId(path);
-            tasks.forEach(c->{
-                if(c.getId().equals(getId)){
-                    System.out.println("성공");//값을 비교하고 출력을 하려고 하는데??
+            task.setId(getId);
+
+//            tasks.forEach(c -> {
+//                if (c.getId().equals(getId)) {
+//                    try {
+//                        String toJSON = tasksToJSON(task);
+//                        content = toJSON;
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//            });
+
+            for (Task task1 : tasks) {
+                if(task1.getId().equals(getId)){
+                    content = tasksToJSON(task1);
                 }
-            });
+            }
         }
 
         //일 생성
@@ -53,15 +61,31 @@ public class DemoHttpHadnler implements HttpHandler {
             content = "Create a new task";
         }
 
-        exchange.sendResponseHeaders(200, content.getBytes().length);
-        OutputStream outputStream = exchange.getResponseBody();
+        if (requestMethod.equals("PATCH") && path.equals("/tasks")) {
 
+        }
+        if (requestMethod.equals("DELETE") && path.equals("/tasks")) {
+
+        }
+
+        exchange.sendResponseHeaders(200, content.getBytes().length);
+        outputStream(exchange, content);
+    }
+
+    private  void outputStream(HttpExchange exchange, String content) throws IOException {
+        OutputStream outputStream = exchange.getResponseBody();
         outputStream.write(content.getBytes());
         outputStream.flush();
         outputStream.close();
     }
 
-    private static Long gerRequestId(String path) {
+    private String getBody(HttpExchange exchange) {
+        return new BufferedReader(new InputStreamReader(exchange.getRequestBody()))
+                .lines()
+                .collect(Collectors.joining("\n"));
+    }
+
+    private Long gerRequestId(String path) {
         return Long.parseLong(path.split("/")[2]);
     }
 
@@ -82,6 +106,11 @@ public class DemoHttpHadnler implements HttpHandler {
         return outputStream.toString();
     }
 
+    private String tasksToJSON(Task task) throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, task);
+        return outputStream.toString();
+    }
 
 
 }
