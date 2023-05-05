74ed429e94079fdc8208fb68895bbe40c9a24290
EunjinJwa <ejjwa0563@gmail.com>
2023-05-05 19:05:19 +0900

    sendResponse 동작 버그 수정

--- app/src/main/java/com/codesoom/assignment/httphandler/TaskHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/httphandler/TaskHttpHandler.java
@@ -114,6 +114,7 @@ public class TaskHttpHandler implements HttpHandler {
 			httpExchange.getResponseBody().close();
 		} else {
 			httpExchange.sendResponseHeaders(httpCode, 0);
+			httpExchange.close();
 		}
 	}
 }
