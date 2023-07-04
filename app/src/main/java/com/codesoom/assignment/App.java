6196d280b38c6e3b9079a225ed3402e027d1da30
tmxhsk99 <tmxhsk99@naver.com>
2023-07-04 10:37:29 +0900

    todo REST API 기능 추가

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -1,10 +1,25 @@
 package com.codesoom.assignment;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import com.codesoom.assignment.handler.TaskHttpHandler;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
+    private static final int DEFAULT_PORT = 8000;
+    private static String ROOT_PATH = "/";
     public static void main(String[] args) {
+        try {
+            InetSocketAddress address = new InetSocketAddress(DEFAULT_PORT);
+            HttpServer httpServer = HttpServer.create(address,0);
+            HttpHandler handler = new TaskHttpHandler();
+            httpServer.createContext(ROOT_PATH,handler);
+            httpServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
