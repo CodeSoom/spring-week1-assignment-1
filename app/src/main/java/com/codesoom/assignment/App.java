8f6431dd62f8a479e5ccbe7df7a7e99192b72e4e
tmxhsk99 <tmxhsk99@naver.com>
2023-07-05 14:20:56 +0900

    fix : codeReformat 모드 적용 후 적용

--- app/src/main/java/com/codesoom/assignment/App.java
+++ app/src/main/java/com/codesoom/assignment/App.java
@@ -10,12 +10,13 @@ import java.net.InetSocketAddress;
 public class App {
     private static final int DEFAULT_PORT = 8000;
     private static String ROOT_PATH = "/";
+
     public static void main(String[] args) {
         try {
             InetSocketAddress address = new InetSocketAddress(DEFAULT_PORT);
-            HttpServer httpServer = HttpServer.create(address,0);
+            HttpServer httpServer = HttpServer.create(address, 0);
             HttpHandler handler = new TaskHttpHandler();
-            httpServer.createContext(ROOT_PATH,handler);
+            httpServer.createContext(ROOT_PATH, handler);
             httpServer.start();
         } catch (IOException e) {
             e.printStackTrace();
